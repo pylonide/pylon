029d2393922bad3fe91ff1fdceea7742ad5649d4
Ruben Daniels <ruben@ajax.org>
2008-08-23 13:36:29 +0000

    * Changed textbox.js to adhere to the new component module way of doing things
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@104 3c3c2983-eddd-4584-88e6-321645f70290

---
+++ components/textbox/autocomplete.js
@@ -0,0 +1,158 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
+
+// #ifdef __ENABLE_TEXTBOX_AUTOCOMPLETE && (__JTEXTBOX || __INC_ALL)
+
+/**
+ * @constructor
+ * @private
+ */
+jpf.textbox.autocomplete = function(){
+    /*
+        missing features:
+        - web service based autocomplete
+    */
+    var autocomplete = {};
+    
+    this.initAutocomplete = function(ac){
+        ac.parentNode.removeChild(ac);
+        autocomplete.nodeset   = ac.getAttribute("nodeset").split(":");
+        autocomplete.method    = ac.getAttribute("method");
+        autocomplete.value     = ac.getAttribute("value");
+        autocomplete.count     = parseInt(ac.getAttribute("count")) || 5;
+        autocomplete.sort      = ac.getAttribute("sort");
+        autocomplete.lastStart = -1;
+        
+        this.oContainer = jpf.XMLDatabase.htmlImport(this.__getLayoutNode("container"),
+            this.oExt.parentNode, this.oExt.nextSibling);	
+    }
+    
+    this.fillAutocomplete = function(keyCode){
+        if (keyCode) {
+            switch(keyCode){
+                case 9:
+                case 27: 
+                case 13:  
+                    return this.oContainer.style.display = "none";
+                case 40: //DOWN
+                    if(autocomplete.suggestData 
+                      && autocomplete.lastStart < autocomplete.suggestData.length){
+                        this.clear();
+                        var value       = autocomplete.suggestData[autocomplete.lastStart++];
+                        this.oInt.value = value; //hack!
+                        this.change(value);
+                        //this.oInt.select(); this.oInt.focus();
+                        this.oContainer.style.display = "none";
+                        return;
+                    }
+                    break;
+                case 38: //UP
+                    if (autocomplete.lastStart > 0) {
+                        if(autocomplete.lastStart >= autocomplete.suggestData.length) 
+                            autocomplete.lastStart = autocomplete.suggestData.length - 1;
+
+                        this.clear();
+                        var value = autocomplete.suggestData[autocomplete.lastStart--];
+                        this.oInt.value = value; //hack!
+                        this.change(value);
+                        //this.oInt.select(); this.oInt.focus();
+                        this.oContainer.style.display = "none";
+                        return;
+                    }
+                    break;
+            }
+            
+            if (keyCode > 10 && keyCode < 20) return;
+        }
+        
+        if (autocomplete.method) {
+            var start = 0, suggestData = self[autocomplete.method]();
+            autocomplete.count = suggestData.length;
+        }
+        else {
+            if (this.oInt.value.length == 0){
+                this.oContainer.style.display = "none";
+                return;
+            }
+            if (!autocomplete.suggestData) {
+                //Get data from model
+                var nodes = self[autocomplete.nodeset[0]].data.selectNodes(autocomplete.nodeset[1]);
+                for(var value, suggestData = [], i = 0; i < nodes.length; i++) {
+                    value = jpf.getXmlValue(nodes[i], autocomplete.value);
+                    if (value)
+                        suggestData.push(value.toLowerCase());
+                }
+                if (autocomplete.sort)
+                    suggestData.sort();
+                autocomplete.suggestData = suggestData;
+            }
+            else {
+                suggestData = autocomplete.suggestData;
+            }
+            
+            //Find Startpoint in lookup list
+            var value = this.oInt.value.toUpperCase();
+            for(var start = suggestData.length - autocomplete.count, i = 0; i < suggestData.length; i++) {
+                if (value <= suggestData[i].toUpperCase()) {
+                    start = i;
+                    break;
+                }
+            }
+            autocomplete.lastStart = start;
+        }
+        
+        //Create html items
+        this.oContainer.innerHTML = "";
+        
+        for (var arr = [], j = start; j < Math.min(start + autocomplete.count, suggestData.length); j++) {
+            this.__getNewContext("item")
+            var oItem = this.__getLayoutNode("item");
+            jpf.XMLDatabase.setNodeValue(this.__getLayoutNode("item", "caption"), suggestData[j]);
+            
+            oItem.setAttribute("onmouseover", 'this.className = "hover"');
+            oItem.setAttribute("onmouseout",  'this.className = ""');
+            oItem.setAttribute("onmousedown", 'event.cancelBubble = true');
+            oItem.setAttribute("onclick",
+               "var o = jpf.lookup(" + this.uniqueId + ");\
+                o.oInt.value = this.innerHTML;\
+                o.change(this.innerHTML);\
+                o.oInt.select();\
+                o.oInt.focus();\
+                o.oContainer.style.display = 'none';");
+            
+            arr.push(this.__getLayoutNode("item"));
+        }
+        jpf.XMLDatabase.htmlImport(arr, this.oContainer);
+        
+        this.oContainer.style.display = "block";
+    }
+    
+    this.setAutocomplete = function(model, traverse, value){
+        autocomplete.lastStart   = -1;
+        autocomplete.suggestData = null;
+        
+        autocomplete.nodeset = [model, traverse];
+        autocomplete.value = value;
+        this.oContainer.style.display = "none";
+    }
+}
+
+// #endif
---
+++ components/textbox/masking.js
@@ -0,0 +1,353 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
+
+// #ifdef __ENABLE_TEXTBOX_MASKING && (__JTEXTBOX || __INC_ALL)
+
+/**
+ * @constructor
+ * @private
+ */
+jpf.textbox.masking = function(){
+    /*
+        Special Masking Values:
+        - PASSWORD
+        
+        <j:Textbox name="custref" mask="CS20999999" maskmsg="" validation="/CS200[3-5]\d{4}/" invalidmsg="" bind="custref/text()" />
+    */
+    
+    var _FALSE_ = 9128748732;
+
+    var _REF = {
+        "0" : "\\d",
+        "1" : "[12]",
+        "9" : "[\\d ]",
+        "#" : "[\\d +-]",
+        "L" : "[A-Za-z]",
+        "?" : "[A-Za-z ]",
+        "A" : "[A-Za-z0-9]",
+        "a" : "[A-Za-z0-9 ]",
+        "X" : "[0-9A-Fa-f]",
+        "V" : "[0-9A-Fa-fV]", //Vonage virtual mac address
+        "x" : "[0-9A-Fa-f ]",
+        "&" : "[^\s]",
+        "C" : "."
+    };
+
+    var lastPos = -1;
+    var masking = false;
+    var oExt    = this.oExt
+    var initial, pos, myvalue, format, fcase, replaceChar;
+
+    this.setPosition = function(setpos){
+        setPosition(setpos || lastPos || 0);
+    }
+
+    this.__clear = function(){
+        this.value = "";
+        if (this.mask) 
+            return this.setValue("");
+    }
+    
+    this.__supportedProperties = ["value"];
+    this.__handlePropSet = function(prop, value){
+        switch (prop) {
+            case "value":
+                var data = "";
+                if (this.includeNonTypedChars) {
+                    for (var i = 0; i < initial.length; i++) {
+                        if (initial.substr(i, 1) != value.substr(i, 1))
+                            data += value.substr(i, 1);//initial.substr(i,1) == replaceChar
+                    }
+                }
+                
+                this.__insertData(data || value);
+            break;
+        }
+    }
+    
+    /* ***********************
+            Keyboard Support
+    ************************/
+    
+    this.keyHandler = function(key, ctrlKey, shiftKey, altKey, e){
+        if (this.dispatchEvent("onkeydown", {
+            keyCode   : key,
+            ctrlKey   : ctrlKey,
+            shiftKey  : shiftKey,
+            altKey    : altKey,
+            htmlEvent : e}) === false)
+                return false;
+
+        switch (key) {
+            case 39:	
+                //RIGHT
+                setPosition(lastPos+1);
+                break;
+            case 37:
+                //LEFT
+                setPosition(lastPos-1);
+                break;
+            case 35:
+            case 34:
+                setPosition(myvalue.length);
+                break;
+            case 33:
+            case 36:
+                setPosition(0);
+                break;
+            case 8:
+                //BACKSPACE
+                deletePosition(lastPos-1);
+                setPosition(lastPos-1);
+                break;
+            case 46:
+                //DEL
+                deletePosition(lastPos);
+                setPosition(lastPos);
+                break;
+            default:
+                if (key == 67 && ctrlKey)
+                    window.clipboardData.setData("Text", this.getValue());  
+                /*
+                else if ((key == 86 && ctrlKey) || (shiftKey && key == 45)) {
+                    this.setValue(window.clipboardData.getData("Text"));
+                    setPosition(lastPos);
+                }
+                */
+                else
+                    return;
+            break;
+        }
+            
+        return false;
+    }
+    
+    /* ***********************
+            Init
+    ************************/
+    
+    this.__initMasking = function(){
+        ///this.keyHandler = this._keyHandler;
+        this.keyHandlerWA = this._keyHandler; //temp solution
+        masking = true;
+
+        this.oInt.onkeypress = function(){
+            var chr = String.fromCharCode(self.event.keyCode);
+            chr = (self.event.shiftKey ? chr.toUpperCase() : chr.toLowerCase());
+
+            if (setCharacter(chr))
+                setPosition(lastPos + 1);
+
+            return false;
+        }
+        
+        this.oInt.onmouseup = function(){
+            var pos = Math.min(calcPosFromCursor(), myvalue.length);
+            setPosition(pos);
+            return false;
+        }
+        
+        this.oInt.onpaste = function(){
+            event.returnValue = false;
+            this.host.setValue(window.clipboardData.getData("Text") || "");
+            //setPosition(lastPos);
+            setTimeout(function(){
+                setPosition(lastPos);
+            }, 1); //HACK good enough for now...
+        }
+        
+        this.getValue = function(){
+            if (this.includeNonTypedChars)
+                return initial == this.oInt.value 
+                    ? "" 
+                    : this.oInt.value.replace(new RegExp(replaceChar, "g"), "");
+            else
+                return myvalue.join("");
+        }
+        
+        this.setValue = function(value){
+            if (this.includeNonTypedChars) {
+                for (var data = "", i = 0; i < initial.length; i++) {
+                    if (initial.substr(i,1) != value.substr(i,1))
+                        data += value.substr(i,1);//initial.substr(i,1) == replaceChar
+                }
+            }
+            this.__insertData(data);
+        }
+    }
+    
+    this.setMask = function(m){
+        if(!masking) this.__initMasking();
+        
+        var m = m.split(";");
+        replaceChar = m.pop();
+        this.includeNonTypedChars = parseInt(m.pop()) !== 0;
+        var mask = m.join(""); //why a join here???
+        var validation = "", visual="", mode_case = "-",
+            strmode = false, startRight = false, chr;
+        var pos = [], format = "", fcase = "";
+        
+        for (var looppos = -1, i = 0; i < mask.length; i++) {
+            chr = mask.substr(i,1);
+            
+            if (!chr.match(/[\!\'\"\>\<\\]/))
+                looppos++;
+            else {
+                if (chr == "!")
+                    startRight = true;
+                else if (chr == "<" || chr == ">")
+                    mode_case = chr;
+                else if (chr == "'" || chr == "\"")
+                    strmode = !strmode;
+                continue;
+            }
+            
+            if (!strmode && _REF[chr]) {
+                pos.push(looppos);
+                visual     += replaceChar;
+                format     += chr;
+                fcase      += mode_case;
+                validation += _REF[chr];
+            }
+            else
+                visual += chr;
+        }
+
+        this.oInt.value = visual;
+        initial         = visual;
+        //pos = pos;
+        myvalue = [];
+        //format = format;
+        //fcase = fcase;
+        replaceChar = replaceChar;
+        
+        //setPosition(0);//startRight ? pos.length-1 : 0);
+        
+        //validation..
+        //forgot \ escaping...
+    }
+    
+    function checkChar(chr, p){
+        var f = format.substr(p, 1);
+        var c = fcase.substr(p, 1);
+    
+        if (chr.match(new RegExp(_REF[f])) == null)
+            return _FALSE_;
+        if (c == ">")
+            return chr.toUpperCase();
+        if (c == "<")
+            return chr.toLowerCase();
+        return chr;
+    }
+    
+    function setPosition(p){
+        if (p < 0)
+            p = 0;
+
+        var range = oExt.createTextRange();
+        range.expand("textedit");
+        range.select();
+        
+        if (pos[p] == null) {
+            range.collapse(false);
+            range.select();
+            lastPos = pos.length;
+            return false;
+        }
+        
+        range.collapse();
+        range.moveStart("character", pos[p]);
+        range.moveEnd("character", 1);
+        range.select();
+
+        lastPos = p;
+    }
+    
+    function setCharacter(chr){
+        if (pos[lastPos] == null) return false;
+        
+        var chr = checkChar(chr, lastPos);
+        if (chr == _FALSE_) return false;
+
+        var range = oExt.createTextRange();
+        range.expand("textedit");
+        range.collapse();
+        range.moveStart("character", pos[lastPos]);
+        range.moveEnd("character", 1);
+        range.text = chr;
+        if (jpf.window.getFocussedObject == this)
+            range.select();
+        
+        myvalue[lastPos] = chr;
+        
+        return true;
+    }
+    
+    function deletePosition(p){
+        if(pos[p] == null) return false;
+        
+        var range = oExt.createTextRange();
+        range.expand("textedit");
+        range.collapse();
+        range.moveStart("character", pos[p]);
+        range.moveEnd("character", 1);
+        range.text = replaceChar;
+        range.select();
+        
+        //ipv lastPos
+        myvalue[p] = " ";
+    }
+    
+    this.__insertData = function(str){
+        if (str == this.getValue()) return;
+        str = this.dispatchEvent("oninsert", { data : str }) || str;
+        
+        if (!str) {
+            if (!this.getValue()) return; //maybe not so good fix... might still flicker when content is cleared
+            for (var i = this.getValue().length - 1; i >= 0; i--)
+                deletePosition(i);
+            setPosition(0);	
+            return;
+        }
+        
+        for (var i = 0; i < str.length; i++) {
+            lastPos = i;
+            setCharacter(str.substr(i,1));
+        }
+        if (str.length)
+            lastPos++;
+    }
+    
+    function calcPosFromCursor(){
+        var range = document.selection.createRange();
+        r2 = range.duplicate();
+        r2.expand("textedit");
+        r2.setEndPoint("EndToStart", range);
+        var lt = r2.text.length;
+    
+        for (var i = 0; i < pos.length; i++)
+            if (pos[i] > lt)
+                return (i == 0) ? 0 : i - 1;
+    }
+}
+
+
+// #endif
