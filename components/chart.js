5d94f5ef2a6c6e51409da6558fd63bd6c6a433af
Rik Arends <rik@ajax.org>
2008-09-09 18:50:47 +0000

    git-svn-id: svn://svn.ajax.org/platform/source/trunk@266 3c3c2983-eddd-4584-88e6-321645f70290

--- components/chart.js
+++ components/chart.js
@@ -62,7 +62,7 @@ jpf.chart = jpf.component(jpf.GUI_NODE, function(){
 	this.__supportedProperties = ['formula', 'a','b','c','d'];
 	this.__handlePropSet = function(prop, value){
 	    if (prop == "formula") {
-	        this.addFormula(value, {color:"red"}, [[-4,-2],[4,2]]);
+	        this.addFormula('FXY2D',value, {color:"red"}, [[-1,-1],[1,1]]);
 	    }
 	    else if ("a|b|c|d".indexOf(prop) > -1)
 			this.drawChart();
@@ -105,6 +105,17 @@ jpf.chart = jpf.component(jpf.GUI_NODE, function(){
             vw : space.w,
             tx : space.x + space.w,
             ty : space.y + space.h,
+			rvx : -1.2,
+			rvy : 0,
+			rvz : 0.3,
+			tvx : 0,
+			tvy : 0,
+			tvz : -3,
+			stepx : 30,
+			stepy : 30,
+			perspx : 400,
+			perspy : 400,
+			scalez : 0.1,
             sw : (this.oExt.offsetWidth - 45) / space.w,
             sh : (this.oExt.offsetHeight - 30) / space.h
         };
@@ -153,7 +164,20 @@ jpf.chart = jpf.component(jpf.GUI_NODE, function(){
   		}, 100);   	
 	}
     
-	this.addFormula = function(formula, style, window){
+	this.addFormula = function(type,formula, style, window){
+		//this.drawChart();
+		var fobj = engine['compileFormula'+type](formula);
+		if(fobj){
+			calcSpace2D(window, space);	
+			// compile math function to a draw function
+			if (formulaId)
+	            series.removeIndex(formulaId - 1);
+			
+			formulaId = series.push({type:'formula', style:style, data:fobj});
+		}
+	}	
+	
+	this.addFormulaFXY = function(formula, style, window){
 		//this.drawChart();
 		var fobj = engine.compileFormula(formula);
 		if(fobj){
@@ -163,9 +187,9 @@ jpf.chart = jpf.component(jpf.GUI_NODE, function(){
 			if (formulaId)
 	            series.removeIndex(formulaId - 1);
 			
-			formulaId = series.push({type:'formula', style:style, data:fobj});
+			formulaId = series.push({type:'formulaFXY', style:style, data:fobj});
 		}
-	}	
+	}		
 	
     this.draw = function(){
         //Build Main Skin
@@ -337,7 +361,7 @@ jpf.chart.canvasDraw = {
 		c.restore();
 	},
 
-	compileFormula : function(eq){
+	compileFormulaFX2D : function(eq){
 		var fstr = eq.toLowerCase().replace(/([a-z][a-z]+)/g,"Math.$1").replace(/([0-9])([a-z)])/g,"$1*$2");
 		var c;
 		try{
@@ -353,23 +377,199 @@ jpf.chart.canvasDraw = {
 			ctx.lineWidth = 1;\
 	        ctx.strokeStyle = (style.color || defaultStyle.color);\
 			ctx.translate(-vx*sw,vy*sh);\
-			ctx.moveTo(x * sw, dh - ( "+fstr+" )*sh); x +=lx;\
-			for(;x<=tx; x+=lx)\
-				ctx.lineTo(x * sw, dh - ( "+fstr+")*sh);\
-	        ctx.stroke();\
+			try{\
+				ctx.moveTo(x * sw, dh - ( "+fstr+" )*sh); x +=lx;\
+				for(;x<=tx; x+=lx)\
+					ctx.lineTo(x * sw, dh - ( "+fstr+")*sh);\
+		        ctx.stroke();\
+			} catch(x){}\
 			ctx.restore();";
 			var fobj = new Function('o','style', 'persist','pthis',c);
 		}catch(x){
 			//alert("Error in: "+c);
-			return this.compileFormula("1");
+			return this.compileFormulaFX2D("1");
 		}
 		return fobj;
 	},
-	
+	compileFormulaFXY3D : function(eq){
+		var fstr = eq.toLowerCase().replace(/([0-9])([a-z)])/g,"$1*$2").replace(/([a-z][a-z]+)/g,"Math.$1");
+		var c;
+		try{
+			c="\
+			var dh2 = o.dh/2,dw2 = o.dw/2, vx = o.vx, vy = o.vy, vh = o.vh, vw = o.vw, \
+				ctx = persist.ctx, tx = o.tx,ty = o.ty, \
+				a=pthis.a, b=pthis.b, c=pthis.c,d=pthis.d,\
+				n=(new Date()).getTime() / 1000, e=Math.E, p=Math.PI,\
+				ax=o.perspx, ay=o.perspy, sx=o.stepx, sy=o.stepy,\
+				dx = vw/sx, dy = vh/sy, \
+				ma = Math.cos(o.rvx),mb = Math.sin(o.rvx),\
+				mc = Math.cos(o.rvy),md = Math.sin(o.rvy),\
+				me = Math.cos(o.rvz),mf = Math.sin(o.rvz),\
+				m00=mc*me,m01=-mf*mc,m02=md,m03=o.tvx,\
+				m40=(me*mb*md+mf*ma),m41=(-mb*md*mf+ma*me),m42=-mb*mc,m43=o.tvy,\
+				m80=(-ma*md*me+mb*mf),m81=(ma*md*mf+me*mb),m82=ma*mc,m83=o.tvz,\
+				gx=Array(sx),gy=Array(sy),sz = o.scalez,\
+				d, lx, ly, nx, ny, vx,vy, x, y, z, zt; \
+			ctx.save();\
+			ctx.beginPath();\
+			ctx.lineWidth = 1;\
+	        ctx.strokeStyle = (style.color || defaultStyle.color);\
+			ctx.translate(dw2,dh2);\
+			try{\
+				for(y = vy,j=0; y<=ty; y += dy, j++){\
+					for(x = vx,i=0; x<=tx; x += dx, i++){\
+						z = ("+fstr+")*sz;\
+						zt = m80*x+m81*y+m82*z+m83;\
+						nx=(m00*x+m01*y+m02*z+m03)*ax/zt;\
+						ny=(m40*x+m41*y+m42*z+m43)*ay/zt;\
+						if(j){\
+							if(i){\
+								ctx.moveTo(gx[i],gy[i]);\
+								ctx.lineTo(nx,ny);\
+								ctx.lineTo(lx,ly);\
+							}else{\
+								ctx.moveTo(gx[0],gy[0]);\
+								ctx.lineTo(nx,ny);\
+							}\
+						}else{\
+							if(!i)ctx.moveTo(nx,ny);\
+							else ctx.lineTo(nx,ny);\
+						}\
+						gx[i]=lx=nx, gy[i]=ly = ny;\
+					}\
+				}\
+				ctx.stroke();\
+			}catch(x){}\
+			ctx.restore();";
+			var fobj = new Function('o','style', 'persist','pthis',c);
+		}catch(x){
+			return this.compileFormulaFXY3D("0");
+			//return this.compileFormulaFXY("1");
+			//return 0;
+		}
+		return fobj;
+	},
+	compileFormulaFT2D : function(eq){
+		var fstr = eq.toLowerCase().replace(/([0-9])([a-z)])/g,"$1*$2").replace(/([a-z][a-z]+)/g,"Math.$1").split(";");
+		var fx = fstr[0], fy = (fstr.length>1)?fstr[1]:fx;
+		var c;
+		try{
+			c="\
+			var dh = o.dh,dw = o.dw, vx = o.vx, vy = o.vy, vh = o.vh, vw = o.vw,\
+				ts = o.ts, te = o.te, t = ts,lt = (te-ts)/(style.steps||100),\
+				sw = o.sw, sh = o.sh, ctx = persist.ctx, tx = o.tx,ty = o.ty, \
+				density = style.density || 3,\
+				a=pthis.a||0, b=pthis.b||0, c=pthis.c||0,d=pthis.d||0,\
+				n=(new Date()).getTime() / 1000, e=Math.E, p=Math.PI,\
+			ctx.save();\
+			ctx.beginPath();\
+			ctx.lineWidth = 1;\
+	        ctx.strokeStyle = (style.color || defaultStyle.color);\
+			ctx.translate(-vx*sw,vy*sh);\
+			try{\
+				ctx.moveTo(( "+fx+" ) * sw, dh - ( "+fy+" )*sh); x +=lx;\
+				for(;t<=te; t+=lt)\
+					ctx.lineTo(( "+fx+" ) * sw, dh - ( "+fy+")*sh);\
+		        ctx.stroke();\
+			} catch(x){}\
+			ctx.restore();";
+			var fobj = new Function('o','style', 'persist','pthis',c);
+		}catch(x){
+			//alert("Error in: "+c);
+			return this.compileFormulaFT2D("1");
+		}
+		return fobj;
+	},	
+	compileFormulaFT3D : function(eq){
+		var fstr = eq.toLowerCase().replace(/([0-9])([a-z)])/g,"$1*$2").replace(/([a-z][a-z]+)/g,"Math.$1").split(";");
+		var fx = fstr[0], fy = (fstr.length>1)?fstr[1]:fx, fz = (fstr.length>2)?fstr[1]:fx;
+		var c;
+		try{
+			c="\
+			var dh2 = o.dh/2,dw2 = o.dw/2, \
+				ts = o.ts, te = o.te, t = ts,lt = (te-ts)/(style.steps||100),\
+				ctx = persist.ctx, \
+				a=pthis.a, b=pthis.b, c=pthis.c,d=pthis.d,\
+				n=(new Date()).getTime() / 1000, e=Math.E, p=Math.PI,\
+				ax=o.perspx, ay=o.perspy,\
+				ma = Math.cos(o.rvx),mb = Math.sin(o.rvx),\
+				mc = Math.cos(o.rvy),md = Math.sin(o.rvy),\
+				me = Math.cos(o.rvz),mf = Math.sin(o.rvz),\
+				m00=mc*me,m01=-mf*mc,m02=md,m03=o.tvx,\
+				m40=(me*mb*md+mf*ma),m41=(-mb*md*mf+ma*me),m42=-mb*mc,m43=o.tvy,\
+				m80=(-ma*md*me+mb*mf),m81=(ma*md*mf+me*mb),m82=ma*mc,m83=o.tvz,\
+				d, x, y, z, zt; \
+			ctx.save();\
+			ctx.beginPath();\
+			ctx.lineWidth = 1;\
+	        ctx.strokeStyle = (style.color || defaultStyle.color);\
+			ctx.translate(dw2,dh2);\
+			try{\
+				x = ("+fx+"), y = ("+fy+"), z = ("+fz+");\
+				zt = m80*x+m81*y+m82*z+m83; t+=lt;\
+				ctx.moveTo( (m00*x+m01*y+m02*z+m03)*ax/zt, \
+							(m40*x+m41*y+m42*z+m43)*ay/zt;)\
+				for(;t<=te; t+=lt){\
+					x = ("+fx+"), y = ("+fy+"), z = ("+fz+");\
+					ctx.lineTo( (m00*x+m01*y+m02*z+m03)*ax/zt, \
+							(m40*x+m41*y+m42*z+m43)*ay/zt;)\
+				}\
+			   ctx.stroke();\
+			} catch(x){}\
+			ctx.restore();";
+			var fobj = new Function('o','style', 'persist','pthis',c);
+		}catch(x){
+			return this.compileFormulaFXY("0");
+			//return this.compileFormulaFXY("1");
+			//return 0;
+		}
+		return fobj;
+	},
+	compileFormulaFXY2D : function(eq){
+		var fstr = eq.toLowerCase().replace(/([a-z][a-z]+)/g,"Math.$1").replace(/([0-9])([a-z)])/g,"$1*$2");
+		var c;
+		try{
+			c="\
+			var vx = o.vx, vy = o.vy, vh = o.vh, vw = o.vw, \
+				ctx = persist.ctx, tx = o.tx,ty = o.ty, \
+				a=pthis.a, b=pthis.b, c=pthis.c,d=pthis.d,\
+				n=(new Date()).getTime() / 1000, e=Math.E, p=Math.PI,\
+				sx=o.stepx, sy=o.stepy,\
+				dx = vw/sx, dy = vh/sy, rdx = o.dw/sx, rdy = o.dh/sy,\
+				hrdx = rdx/2, hrdy = rdy/2, x, y, rx, ry, z;\
+			var pal=Array(255);\
+			for(var i = 0;i<256;i++)pal[i]='rgb('+i+','+i+','+i+')';\
+			ctx.save();\
+			ctx.fillStyle = (style.color || defaultStyle.color);\
+			try{\
+				if(style.block=='1'){\
+					for(y = vy, ry = hrdy; y<=ty; y += dy, ry += rdy){\
+						for(x = vx, rx = hrdx; x<=tx; x += dx, rx += rdx){\
+							z = ("+fstr+");z=z<0?0:(z>1?1:z);\
+							ctx.fillRect(rx-hrdx*z,ry-hrdy*z,rdx*z,rdy*z);\
+						}\
+					}\
+				}else{\
+					for(y = vy, ry = 0; y<=ty; y += dy, ry += rdy){\
+						for(x = vx, rx = 0; x<=tx; x += dx, rx += rdx){\
+							z=parseInt(("+fstr+")*255);\
+							ctx.fillStyle = pal[(z<0)?0:(z>255?255:z)];\
+							ctx.fillRect(rx,ry,rdx,rdy);\
+						}\
+					}\
+				}\
+			}catch(x){}\
+			ctx.restore();";
+			var fobj = new Function('o','style', 'persist','pthis',c);
+		}catch(x){
+			//alert("Error in: "+c);
+			return this.compileFormulaFXY2D("1");
+		}
+		return fobj;
+	},	
 	formula : function(o, eq, style, persist,pthis){
 		eq(o,style,persist,pthis);
     },
-	
 	callback : function(o,callback,style,persist){
 		var dh = o.dh,dw = o.dw, vx = o.vx, vy = o.vy, vh = o.vh, vw = o.vw, 
             sw = o.sw, sh = o.sh, c = persist.ctx, tx = o.tx,ty = o.ty,
