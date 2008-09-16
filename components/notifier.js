454933f0b4aedbb9bf3b414d78bf5975d1b09b24
Łukasz Lipiński <uzza17@gmail.com>
2008-09-16 16:48:47 +0000

    * positioning ([top, center, bottom]-[left, center, right]) and arrange [vertical, horizontal] has been added
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@351 3c3c2983-eddd-4584-88e6-321645f70290

--- components/notifier.js
+++ components/notifier.js
@@ -34,16 +34,19 @@
  */
 jpf.notifier = jpf.component(jpf.GUI_NODE, function() {   
     this.pHtmlNode = document.body;
-    this.timeout   = 2000;//in milliseconds
+    this.timeout   = 4000;//in milliseconds    
     
     this.__supportedProperties.push("margin", "position", "width", "timeout");
 
     var lastPos = null;
     var showing = 0;
     var _self   = this;
+	var sign = 1;	
 
     this.popupEvent = function(ev){       
-        var oNoti = this.pHtmlNode.appendChild(this.oExt.cloneNode(true));        
+        var oNoti = this.pHtmlNode.appendChild(this.oExt.cloneNode(true)); 
+		var ww = jpf.isIE ? document.documentElement.offsetWidth : window.innerWidth;
+		var wh = jpf.isIE ? document.documentElement.offsetHeight : window.innerHeight;			       
         
         this.__getLayoutNode("notification", "message", oNoti).innerHTML = ev.message;
         var oIcon = this.__getLayoutNode("notification", "icon", this.oExt);
@@ -54,41 +57,62 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
                 oIcon.nodeType = this.iconPath + ev.icon;
         }
         oNoti.style.display = "block";
-        
-        /*
-            I have added an example here which does it from left-top, you 
-            should expand this to all positions
-        */
+                
         var margin = jpf.getBox(this.margin || "0");
-        if (!lastPos) {
-            //if (this.position == "left-top")
-            lastPos = [margin[3], margin[0]];
-        }
+		var nh = oNoti.offsetHeight;
+		var nw = oNoti.offsetWidth;
+		var x = this.position.split("-");
         
-        //if (this.position == "left-top") {
-        
-        oNoti.style.left = lastPos[0] + "px";
-        lastPos[1] += oNoti.offsetHeight + 20; //this margin will later be read from css
-        oNoti.style.top  = lastPos[1] + "px";
-        //}
+		/* start positions */
+		if (!lastPos) {            
+			var ver = (x[0] == "top" ? margin[0] : (x[0] == "bottom" ? wh - nh - margin[2] : wh/2 - nh/2));
+			var hor = (x[1] == "left" ? margin[3] : (x[1] == "right" ? ww - nw - margin[1] : ww/2 - nw/2));
+			
+			lastPos = [ver, hor];  
+        }				
+		
+		/* reset to next line, first for vertical, second horizontal */
+		if(lastPos[0] > wh-nh || lastPos[0] < 0){			
+			lastPos[1] += (x[1] == "left" ? nw + margin[3] : (x[1] == "right" ? -nw - margin[3] : 0));
+			sign *=-1;
+			lastPos[0] += sign*(x[0] == "top" ? margin[0] + nh : (x[0] == "bottom" ? - margin[2] - nh : 0));		
+		}
+		else if(lastPos[1] > ww-nw || lastPos[1] < 0){
+			lastPos[0] += (x[0] == "top" ? nh + margin[0] : (x[0] == "bottom" ? - nh - margin[0] : 0));
+			sign *=-1;
+			lastPos[1] += x[0] == "center" ? 0 : sign*(x[1] == "left" ? margin[3] + nw : (x[1] == "right" ? - margin[1] - nw : 0));
+		}
+		
+        oNoti.style.left = lastPos[1] + "px";
+        oNoti.style.top  = lastPos[0] + "px";
+		
+		if(this.arrange == "vertical"){
+			lastPos[0] += x[1] == "center" ? 0 : sign*(x[0] == "top" ? margin[0] + nh : (x[0] == "bottom" ? - margin[2] - nh : 0));	
+		}
+		else{
+			lastPos[1] += x[0] == "center" ? 0 : sign*(x[1] == "left" ? margin[3] + nw : (x[1] == "right" ? - margin[1] - nw : 0)); 
+		}		
         
         showing++;
         
         jpf.tween.single(oNoti, {
             type    : 'fade', 
             from    : 0, 
-            to      : 1, 
+            to      : 0.8, 
             anim    : jpf.tween.NORMAL, 
-            steps   : 10,
+            steps   : 100,
             interval: 10,
-            onfinish: function(container){
+            onmouseover : function(){
+				to : 1
+			},
+			onfinish: function(container){
                 setTimeout(function(){
                     jpf.tween.single(oNoti, {
                         type    : 'fade', 
-                        from    : 1, 
+                        from    : 0.8, 
                         to      : 0, 
                         anim    : jpf.tween.NORMAL, 
-                        steps   : 10,
+                        steps   : 100,
                         interval: 10,
                         onfinish: function(){
                             showing--;
@@ -100,7 +124,26 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
                 }, _self.timeout);
             }
         });
-    }
+    
+		/* Events */	
+		oNoti.onmouseover = function(e){
+			if (jpf.hasStyleFilters) 
+	            oNoti.style.filter  = "alpha(opacity=100)";        
+	        else
+	            oNoti.style.opacity = 1;
+		}
+		
+		oNoti.onmouseout = function(e){
+			if (jpf.hasStyleFilters) 
+	            oNoti.style.filter  = "alpha(opacity=80)";        
+	        else
+	            oNoti.style.opacity = 0.8;
+		}
+	
+	}
+	this.popup = function(message, icon){
+		
+	}
 
     this.draw = function() {
         //Build Main Skin
@@ -109,18 +152,20 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
     }
     
     this.__loadJML = function(x) {
-        var i, l, nodes = x.childNodes;
-        for (i = 0, l = nodes.length; i < l; i++) {
-            node = nodes[i];
+        var nodes = x.childNodes;        		
+			
+		for (var l = nodes.length-1, i = 0; i < l; i++) {           
+		    node = nodes[i];
             if (node.nodeType != 1)
                 continue;
             
             if (node[jpf.TAGNAME] == "event") {
                 var ev = new jpf.notifier.event(this.pHtmlNode, "event", this);
                 
-                ev.handlePropSet("when", node.getAttribute("when"), false);
-                ev.loadJML(node);
-                this.childNodes.push(ev);                
+                ev.loadJML(node);               
+				ev.handlePropSet("when", node.getAttribute("when"), false);
+								
+                this.childNodes.push(ev);
             }
         }
     }
@@ -128,13 +173,16 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
 
 //@todo You might want to add icons to the event as well
 jpf.notifier.event = jpf.subnode(jpf.NOGUI_NODE, function(){
-    this.__supportedProperties = ["when", "message", "icon"];
+    var first = true;
+	this.__supportedProperties = ["when", "message", "icon"];
     
-    this.handlePropSet = function(prop, value, force){
-        this[prop] = value;
+    this.handlePropSet = function(prop, value, force){        
+		this[prop] = value;
         
-        if (prop == "when" && this.parentNode && this.parentNode.popupEvent)
-            this.parentNode.popupEvent(this);
+        if (first && prop == "when" && this.parentNode && this.parentNode.popupEvent){
+			this.parentNode.popupEvent(this);
+		}	
+		first = true;			
     }
     
     this.loadJML = function(x){
