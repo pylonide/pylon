ca1f13298052c9c5ea48d7574aad77947fb82381
Łukasz Lipiński <uzza17@gmail.com>
2008-09-18 16:38:56 +0000

    * skin has been added * bugs are fixed
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@369 3c3c2983-eddd-4584-88e6-321645f70290

--- components/notifier.js
+++ components/notifier.js
@@ -36,66 +36,93 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
     this.pHtmlNode = document.body;
     this.timeout   = 2000;//in milliseconds
     this.position  = "top-right";
-    
-    this.__supportedProperties.push("margin", "position", "width", "timeout");
+
+    this.__supportedProperties.push("margin", "position", "width", "timeout", "columnsize");
 
     var lastPos = null;
     var showing = 0;
     var _self   = this;
-	var sign    = 1;
+    var sign    = 1;
+	var howmany = 0;
 
-    this.popup = function(message, icon){       
-        var oNoti = this.pHtmlNode.appendChild(this.oExt.cloneNode(true)); 
-		var ww = jpf.isIE ? document.documentElement.offsetWidth : window.innerWidth;
-		var wh = jpf.isIE ? document.documentElement.offsetHeight : window.innerHeight;			       
+    this.popup = function(message, icon) {        
+		this.oExt.style.width = this.columnsize + "px";
+		var oNoti = this.pHtmlNode.appendChild(this.oExt.cloneNode(true)); 
+        var ww = jpf.isIE ? document.documentElement.offsetWidth : window.innerWidth;
+        var wh = jpf.isIE ? document.documentElement.offsetHeight : window.innerHeight;
+		howmany++;		
+		
+		var oIcon = this.__getLayoutNode("notification", "icon", oNoti);   
         
-        this.__getLayoutNode("notification", "message", oNoti).innerHTML = message;
-        var oIcon = this.__getLayoutNode("notification", "icon", this.oExt);
-        if (oIcon) {
-            if (oIcon.nodeType == 1)
-                oIcon.style.backgroundImage = "url(" + this.iconPath + icon + ")";
-            else
-                oIcon.nodeType = this.iconPath + icon;
-        }
-        oNoti.style.display = "block";
-                
+		if(icon){
+			if (oIcon) {
+	            if (oIcon.nodeType == 1){
+					oIcon.style.backgroundImage = "url(" + this.iconPath + icon + ")";
+				}
+	            else
+	                oIcon.nodeType = this.iconPath + icon;
+	        }  
+		this.__getLayoutNode("notification", "body", oNoti).insertAdjacentHTML("beforeend", message);
+		}	
+		else{			
+			this.__getLayoutNode("notification", "body", oNoti).innerHTML = message;
+		}    
+
+		
+		
+		oNoti.style.display = "block";
+
         var margin = jpf.getBox(this.margin || "0");
-		var nh = oNoti.offsetHeight;
-		var nw = oNoti.offsetWidth;
-		var x = this.position.split("-");
         
-		/* start positions */
-		if (!lastPos) {            
-			var ver = (x[0] == "top" ? margin[0] : (x[0] == "bottom" ? wh - nh - margin[2] : wh/2 - nh/2));
-			var hor = (x[1] == "left" ? margin[3] : (x[1] == "right" ? ww - nw - margin[1] : ww/2 - nw/2));
+		var nh = oNoti.offsetHeight;
+        var nw = oNoti.offsetWidth;
+        
+		var x = this.position.split("-");
+
+        /* start positions */
+        if (!lastPos) {
+            var ver = (x[0] == "top" ? margin[0] : (x[0] == "bottom" ? wh - nh - margin[2] : wh/2 - nh/2));
+            var hor = (x[1] == "left" ? margin[3] : (x[1] == "right" ? ww - nw - margin[1] : ww/2 - nw/2));
+			sign = 1;
 			
-			lastPos = [ver, hor];  
-        }				
+            lastPos = [ver, hor];
+        }
 		
-		/* reset to next line, first for vertical, second horizontal */
-		if(lastPos[0] > wh-nh || lastPos[0] < 0){			
-			lastPos[1] += (x[1] == "left" ? nw + margin[3] : (x[1] == "right" ? -nw - margin[3] : 0));
-			sign *=-1;
-			lastPos[0] += sign*(x[0] == "top" ? margin[0] + nh : (x[0] == "bottom" ? - margin[2] - nh : 0));		
-		}
-		else if(lastPos[1] > ww-nw || lastPos[1] < 0){
-			lastPos[0] += (x[0] == "top" ? nh + margin[0] : (x[0] == "bottom" ? - nh - margin[0] : 0));
-			sign *=-1;
-			lastPos[1] += x[0] == "center" ? 0 : sign*(x[1] == "left" ? margin[3] + nw : (x[1] == "right" ? - margin[1] - nw : 0));
-		}
+		if((howmany !==1 && x[0] == "bottom" && sign == 1) || (x[0] == "top" && sign == -1)){
+			if(this.arrange == "vertical"){
+	            lastPos[0] += x[1] == "center" ? 0 : sign*(x[0] == "top" ? margin[0] + nh : (x[0] == "bottom" ? - margin[2] - nh : 0));
+	        }
+	        else{
+	            lastPos[1] += x[0] == "center" ? 0 : sign*(x[1] == "left" ? margin[3] + nw : (x[1] == "right" ? - margin[1] - nw : 0));
+	        }
+		}		
 		
+        /* reset to next line, first for vertical, second horizontal */
+        if(lastPos[0] > wh - nh || lastPos[0] < 0) {            
+			lastPos[1] += (x[1] == "left" ? nw + margin[3] : (x[1] == "right" ? - nw - margin[3] : 0));
+            sign *= -1;
+            lastPos[0] += sign*(x[0] == "top" ? margin[0] + nh : (x[0] == "bottom" ? - margin[2] - nh : 0));
+        }
+        else if(lastPos[1] > ww - nw || lastPos[1] < 0) {
+            lastPos[0] += (x[0] == "top" ? nh + margin[0] : (x[0] == "bottom" ? - nh - margin[0] : 0));
+            sign *= -1;
+            lastPos[1] += x[0] == "center" ? 0 : sign*(x[1] == "left" ? margin[3] + nw : (x[1] == "right" ? - margin[1] - nw : 0));
+        }
+
         oNoti.style.left = lastPos[1] + "px";
         oNoti.style.top  = lastPos[0] + "px";
 		
-		if(this.arrange == "vertical"){
-			lastPos[0] += x[1] == "center" ? 0 : sign*(x[0] == "top" ? margin[0] + nh : (x[0] == "bottom" ? - margin[2] - nh : 0));	
+        if((x[0] == "top" && sign == 1) || (x[0] == "bottom" && sign == -1)){
+			if(this.arrange == "vertical"){
+	            lastPos[0] += x[1] == "center" ? 0 : sign*(x[0] == "top" ? margin[0] + nh : (x[0] == "bottom" ? - margin[2] - nh : 0));
+	        }
+	        else{
+	            lastPos[1] += x[0] == "center" ? 0 : sign*(x[1] == "left" ? margin[3] + nw : (x[1] == "right" ? - margin[1] - nw : 0));
+	        }
 		}
-		else{
-			lastPos[1] += x[0] == "center" ? 0 : sign*(x[1] == "left" ? margin[3] + nw : (x[1] == "right" ? - margin[1] - nw : 0)); 
-		}		
-        
+
         showing++;
-        
+
         var isMouseOver = false;
         jpf.tween.single(oNoti, {
             type    : 'fade', 
@@ -104,15 +131,15 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
             anim    : jpf.tween.NORMAL, 
             steps   : 10,
             interval: 10,
-			onfinish: function(container){
+            onfinish: function(container){
                 setTimeout(hideWindow, _self.timeout);
             }
         });
-        
+
         function hideWindow(){
             if (isMouseOver)
                 return;
-            
+
             jpf.tween.single(oNoti, {
                 type    : 'fade', 
                 from    : 0.8, 
@@ -121,26 +148,30 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
                 steps   : 10,
                 interval: 10,
                 onfinish: function(){
-                    showing--;
-                    oNoti.parentNode.removeChild(oNoti);
-                    if (!showing)
-                        lastPos = null;
+                    showing--;                    
+					howmany--;					
+                    if (!showing){
+						lastPos = null;
+					}                        
+					if(oNoti.parentNode){
+						oNoti.parentNode.removeChild(oNoti);
+					}					
                 }
             });
         }
-    
-		/* Events */	
-		oNoti.onmouseover = function(e){
-		    isMouseOver = true;
-		    jpf.tween.fade(oNoti, 1);
-		}
-		
-		oNoti.onmouseout = function(e){
-		    jpf.tween.fade(oNoti, 0.8);
-		    isMouseOver = false;
-		    hideWindow();
-		}
-	}
+
+        /* Events */    
+        oNoti.onmouseover = function(e){
+            isMouseOver = true;
+            jpf.tween.fade(oNoti, 1);
+        }
+        
+        oNoti.onmouseout = function(e){
+            jpf.tween.fade(oNoti, 0.8);
+            isMouseOver = false;
+            setTimeout(hideWindow, _self.timeout);
+        }
+    }
 
     this.draw = function() {
         //Build Main Skin
@@ -148,12 +179,12 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
         this.oExt.style.display = "none";
         this.oExt.style.position = "absolute";
     }
-    
+
     this.__loadJML = function(x) {
-        var nodes = x.childNodes;        		
-			
-		for (var l = nodes.length-1, i = 0; i < l; i++) {           
-		    node = nodes[i];
+        var nodes = x.childNodes;
+            
+        for (var l = nodes.length-1, i = 0; i < l; i++) {
+            node = nodes[i];
             if (node.nodeType != 1)
                 continue;
             
@@ -172,30 +203,32 @@ jpf.notifier = jpf.component(jpf.GUI_NODE, function() {
                     });
                 }
                 else {
-				    ev.setDynamicProperty("when", node.getAttribute("when"));
-				}
+                    ev.setDynamicProperty("when", node.getAttribute("when"));
+                }
             }
         }
     }
 }).implement(jpf.Presentation);
 
 jpf.notifier.event = jpf.subnode(jpf.NOGUI_NODE, function(){
-	this.__supportedProperties = ["when", "message", "icon"];
+    this.__supportedProperties = ["when", "message", "icon", "columnsize"];
     
     var hasInitedWhen = false;
-    this.handlePropSet = function(prop, value, force){        
-		this[prop] = value;
-
+    this.handlePropSet = function(prop, value, force){
+        this[prop] = value;
+		
         if (hasInitedWhen && prop == "when" && value 
           && this.parentNode && this.parentNode.popup){
-			this.parentNode.popup(this.message, this.icon);
-		}
-		
-		hasInitedWhen = true;	
+            this.parentNode.popup(this.message, this.icon);
+        }
+
+        hasInitedWhen = true;
     }
-    
+
     this.loadJML = function(x){
         this.jml     = x;
         this.message = x.getAttribute("message") || "[Empty]";
+		this.icon = x.getAttribute("icon");
+		this.columnsize = x.getAttribute("columnsize");
     }
 });
