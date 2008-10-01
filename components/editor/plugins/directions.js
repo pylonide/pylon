3c5f8fad8b52df820fb9241523c37df87f1f5961
Mike de Boer <mike@ajax.org>
2008-10-01 09:11:25 +0000

    * added frames for rtl, ltr, help and media plugins
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@555 3c3c2983-eddd-4584-88e6-321645f70290

---
+++ components/editor/plugins/directions.js
@@ -0,0 +1,44 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *
+ */
+
+// #ifdef __EDITOR || __INC_ALL
+
+jpf.editor.directionPlugin = function(sName) {
+    this.name        = sName;
+    this.icon        = sName;
+    this.type        = jpf.editor.TOOLBARITEM;
+    this.subType     = jpf.editor.TOOLBARBUTTON;
+    this.hook        = 'ontoolbar';
+    this.state       = jpf.editor.OFF;
+
+    this.execute = function(editor) {
+        // @todo: implement this baby
+    };
+    
+    this.queryState = function(editor) {
+        return this.state;
+    };
+};
+
+jpf.editor.Plugin('ltr', jpf.editor.directionPlugin);
+jpf.editor.Plugin('rtl', jpf.editor.directionPlugin);
+
+// #endif
