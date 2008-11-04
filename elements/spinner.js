5701b0f36f656459abb853c7694e7739934e5205
Łukasz Lipiński <uzza17@gmail.com>
2008-11-04 16:18:58 +0000

    * optimalizations * fixes in skin file
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@797 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/spinner.js
+++ elements/spinner.js
@@ -32,40 +32,28 @@
  * @version     %I%, %G% 
  */
 
-jpf.spinner = function(pHtmlNode, tagName) {
-    jpf.register(this, tagName || "spinner", jpf.NODE_VISIBLE);/** @inherits jpf.Class */
-    this.pHtmlNode = pHtmlNode || document.body;
+jpf.spinner = jpf.component(jpf.NODE_VISIBLE, function() {
+    this.pHtmlNode  = document.body;
     this.pHtmlDoc  = this.pHtmlNode.ownerDocument;
 
-    /* ***********************
-     Inheritance
-     ************************/
-    /**
-     * @inherits jpf.Presentation
-     * @inherits jpf.DataBinding
-     */
-    this.inherit(jpf.Presentation, jpf.DataBinding);
+    this.$supportedProperties.push("width", "value");
 
-    /* ********************************************************************
-     PROPERTIES
-     *********************************************************************/
-    //Options
-    this.$focussable     = true; // This object can get the focus
-    this.inherit(jpf.XForms); /** @inherits jpf.XForms */
-    var focusSelect      = false;
-    var masking          = false;
-    this.maximum         = 64000;
-    this.minimum         = -64000;
-    this.startValue      = 0;
+    this.maximum    = 64000;
+    this.minimum    = -64000;
+    this.width      = 200;
+    this.value      = 0;
 
-    this.counter = {
-        cy      : 0,
-        interval: 200,
-        start   : false,
-        type    : "plus"
+    var _self      = this;
+
+    this.$propHandlers["value"] = function(value) {
+        this.oInput.value = parseInt(value) || 0;
     };
-
-    var _self = this;
+    
+    this.$propHandlers["maximum"] = function(value) {
+        alert(value)
+        if(parseInt(value))
+            this.maximum = parseInt(value);
+    };
 
     /* ********************************************************************
      PUBLIC METHODS
@@ -74,50 +62,8 @@ jpf.spinner = function(pHtmlNode, tagName) {
         return this.setProperty("value", value);
     };
 
-    this.$clear = function() {
-        this.value = "";
-
-        if (this.oInput.tagName.toLowerCase().match(/input|textarea/i))
-            this.oInput.value = "";
-        else {
-            this.oInput.innerHTML = "";
-            //try{this.oInput.focus();}catch(e){}
-
-            if (!jpf.hasMsRangeObject)
-                return;
-
-            //will fail when object isn't visible
-            //N.B. why a select in a clear function.. isn't consistent...
-            try {
-                var range = document.selection.createRange();
-                range.moveStart("sentence", -1);
-                //range.text = "";
-                range.select();
-            } 
-            catch (e) {}
-        }
-    };
-
     this.getValue = function() {
         return this.isHTMLBox ? this.oInput.innerHTML : this.oInput.value;
-    };
-
-    this.insert = function(text) {
-        if (jpf.hasMsRangeObject) {
-            try {
-                this.oInput.focus();
-            } 
-            catch (e) {}
-            var range = document.selection.createRange();
-            if (this.oninsert)
-                text = this.oninsert(text);
-            range.pasteHTML(text);
-            range.collapse(true);
-            range.select();
-        }
-        else {
-            this.oInput.value += text;
-        }
     };
 
     this.$enable = function() {
@@ -126,14 +72,6 @@ jpf.spinner = function(pHtmlNode, tagName) {
 
     this.$disable = function() {
         this.oInput.disabled = true;
-    };
-
-    this.select = function() {
-        this.oInput.select();
-    };
-
-    this.deselect = function() {
-        this.oInput.deselect();
     };
 
     /* ********************************************************************
@@ -149,105 +87,17 @@ jpf.spinner = function(pHtmlNode, tagName) {
     //#ifdef __WITH_KEYBOARD
     this.addEventListener("keydown", function(e) {
         var key      = e.keyCode;
-        var ctrlKey  = e.ctrlKey;
-        var shiftKey = e.shiftKey;
-        var altKey   = e.altKey;
 
-        if (key < 8 || (key > 8 && key < 37) || (key > 40 && key < 46) 
-          || (key > 46 && key < 48) || (key > 57 && key < 109) || key > 109) 
-            return false;
         /* Allow: ARROWS, DEL, NUMBERS, MINUS, BACKSPACE */
-
-        //Mike - August 15, 2008: should we keep this in then? DEAD CODE
-        if (false && jpf.isIE && (key == 86 && ctrlKey || key == 45 && shiftKey)) {
-            var text = window.clipboardData.getData("Text");
-            if ((text = this.dispatchEvent("keydown", {
-                text: this.onpaste(text)
-            }) === false)) 
-                return false;
-            if (!text) 
-                text = window.clipboardData.getData("Text");
-
-            this.oInput.focus();
-            var range = document.selection.createRange();
-            range.text = "";
-            range.collapse();
-            range.pasteHTML(text.replace(/\n/g, "<br />").replace(/\t/g, "&nbsp;&nbsp;&nbsp;"));
-
+        if (key < 8 || (key > 8 && key < 37) || (key > 40 && key < 46)
+          || (key > 46 && key < 48) || (key > 57 && key < 109) || key > 109)
             return false;
-        }
     }, true);
     //#endif
-
-    /* ***********************
-     Focus
-     ************************/
-    this.$focus = function(){
-        if (!this.oInput || this.oInput.disabled)
-            return;
-        this.$setStyleClass(this.oInput, this.baseCSSname + "Focus");
-        
-        if (this.oExt)
-            this.$setStyleClass(this.oExt, this.baseCSSname + "Focus");
-
-        try {
-            this.oInput.focus();
-        } 
-        catch (e) {}
-        if (masking) 
-            this.setPosition();
-        
-        if (this.selectFocus) {
-            focusSelect = true;
-            this.select();
-        }
-    };
-
-    this.$blur = function(){
-        if (!this.oInput) 
-            return;
-        this.$setStyleClass(this.oInput, "", [this.baseCSSname + "Focus"]);
-        
-        if (this.oExt)
-            this.$setStyleClass(this.oExt, this.baseCSSname + "Focus");
-        
-        if (masking) {
-            var r = this.oInput.createTextRange();
-            r.collapse();
-            r.select();
-        }
-
-        try {
-            this.oInput.blur();
-            document.body.focus();
-        } 
-        catch (e) {}
-
-        if (this.changeTrigger == "enter") 
-            this.change(this.getValue());
-
-        focusSelect = false;
-        // check if we clicked on the oContainer. ifso dont hide it
-        if (this.oContainer) 
-            setTimeout('var o = jpf.lookup(' + this.uniqueId + ');o.oContainer.style.display = "none"', 100);
-    };
-
-    this.$supportedProperties.push("value");
-    this.$propHandlers["value"] = function(value) {
-        // Set Value
-        if (this.isHTMLBox) {
-            if (this.oInput.innerHTML != value)
-                this.oInput.innerHTML = value;
-        }
-        else if (this.oInput.value != value) {
-            this.oInput.value = value;
-        }
-    };
 
     /* *********
      INIT
      **********/
-    this.inherit(jpf.JmlElement); /** @inherits jpf.JmlElement */
     this.$draw = function() {
         //Build Main Skin
         this.oExt = this.$getExternal(null, null, function(oExt) {
@@ -255,13 +105,10 @@ jpf.spinner = function(pHtmlNode, tagName) {
             oExt.setAttribute("onmouseup",   'this.host.dispatchEvent("mouseup", {htmlEvent : event});');
             oExt.setAttribute("onclick",     'this.host.dispatchEvent("click", {htmlEvent : event});');
         });
+        this.oInt         = this.$getLayoutNode("main", "container", this.oExt);
         this.oInput       = this.$getLayoutNode("main", "input", this.oExt);
         this.oButtonPlus  = this.$getLayoutNode("main", "buttonplus", this.oExt);
         this.oButtonMinus = this.$getLayoutNode("main", "buttonminus", this.oExt);
-
-        this.oInput.value       = this.startValue;
-        this.oInput.style.width = this.oInput.parentNode.offsetWidth - 6 
-            - Math.max(this.oButtonPlus.offsetWidth, this.oButtonMinus.offsetWidth) + "px";
 
         var timer, z = 0;
 
@@ -310,7 +157,6 @@ jpf.spinner = function(pHtmlNode, tagName) {
             };
         };
 
-
         this.oButtonPlus.onmousedown = function(e) {
             e = e || window.event;
 
@@ -320,7 +166,11 @@ jpf.spinner = function(pHtmlNode, tagName) {
             timer = setInterval(function() {
                 z++;
                 value += Math.pow(Math.min(200, z) / 10, 2) / 10;
-                _self.oInput.value = Math.round(value);
+                value = Math.round(value);
+                
+                _self.oInput.value = value <= _self.maximum
+                    ? Math.round(value)
+                    : _self.maximum;
             }, 50);
         };
 
@@ -333,7 +183,12 @@ jpf.spinner = function(pHtmlNode, tagName) {
             timer = setInterval(function() {
                 z++;
                 value -= Math.pow(Math.min(200, z) / 10, 2) / 10;
-                _self.oInput.value = Math.round(value);
+                value = Math.round(value);
+
+                _self.oInput.value = value >= _self.minimum
+                    ? Math.round(value)
+                    : _self.minimum;
+
             }, 50);
         };
 
@@ -362,16 +217,6 @@ jpf.spinner = function(pHtmlNode, tagName) {
                 _self.change(_self.oInput.value);
         };
 
-
-        if (!jpf.hasContentEditable && !this.oInput.tagName.toLowerCase().match(/input|textarea/)) {
-            var node = this.oInput;
-            this.oInput = node.parentNode.insertBefore(document.createElement("textarea"), node);
-            node.parentNode.removeChild(node);
-            this.oInput.className = node.className;
-            if (this.oExt == node)
-                this.oExt = this.oInput;
-        }
-
         this.oInput.onselectstart = function(e) {
             if (!e)
                 e = event;
@@ -380,149 +225,30 @@ jpf.spinner = function(pHtmlNode, tagName) {
 
         this.oInput.host = this;
 
-        //temp fix
-        this.oInput.onkeydown = function(e) {
-            if (this.disabled)
-                return false;
-
-            e = e || window.event;
-
-            //Change
-            if (this.host.changeTrigger == "enter")
-                if (e.keyCode == 13)
-                    this.host.change(this.host.getValue());
-                else
-                    if (jpf.isSafari && this.host.xmlRoot) //safari issue (only old??)
-                        setTimeout('var o = jpf.lookup(' + this.host.uniqueId + ');o.change(o.getValue())');
-
-            if (e.ctrlKey && (e.keyCode == 66 || e.keyCode == 73 || e.keyCode == 85))
-                return false;
-        };
-
-        this.oInput.onkeyup = function(e) {
-            var keyCode = (e || event).keyCode, jmlNode = this.host;
-
-            if (this.host.changeTrigger != "enter") {
-                setTimeout(function() {
-                    if (!jmlNode.mask) 
-                        jmlNode.change(jmlNode.getValue()); //this is a hack
-                    jmlNode.dispatchEvent("keyup", {
-                        keyCode: keyCode
-                    });
-                });
-            }
-            else {
-                jmlNode.dispatchEvent("keyup", {
-                    keyCode: keyCode
-                });
-            }
-        };
-
-        this.oInput.onfocus = function() {
-            if (this.host.initial && this.value == this.host.initial) {
-                this.value = "";
-                this.host.$setStyleClass(this.host.oExt, "", [this.host.baseCSSname + "Initial"]);
-            }
-        };
-
-        this.oInput.onblur = function() {
-            if (this.host.initial && this.value == "") {
-                this.value = this.host.initial;
-                this.host.$setStyleClass(this.host.oExt, this.host.baseCSSname + "Initial");
-            }
-        };
-
-        if (!this.oInput.tagName.toLowerCase().match(/input|textarea/)) {
-            this.isHTMLBox = true;
-
-            this.oInput.unselectable    = "Off";
-            this.oInput.contentEditable = true;
-            this.oInput.style.width     = "1px";
-
-            this.oInput.select = function() {
-                var r = document.selection.createRange();
-                r.moveToElementText(this);
-                r.select();
-            };
-        }
-
-        this.oInput.deselect = function() {
-            if (!document.selection)
-                return;
-
-            var r = document.selection.createRange();
-            r.collapse();
-            r.select();
-        };
     };
 
     this.$loadJml = function(x) {
-        //Masking
-        if (jpf.hasMsRangeObject) {
-            this.mask = x.getAttribute("mask");
-            if (this.mask) {
-                masking = true;
-                this.inherit(jpf.TextboxMask); /** @inherits jpf.TextboxMask */
-                if (!this.mask.match(/PASSWORD/)) 
-                    this.setMask(this.mask);
-                this.maskmsg = x.getAttribute("maskmsg");
-            }
-        }
-
-        //Initial Message
-        this.initial = x.getAttribute("initial") || "";
-        if (this.initial) {
-            this.oInput.onblur();
-            this.setValue(this.initial);
-        }
-
-        //Triggering and Focus
-        this.changeTrigger = jpf.xmldb.getInheritedAttribute(x, "change") || "realtime";
-        this.selectFocus   = x.getAttribute("focusselect") == "true";
-        if (this.mask) {
-            this.selectFocus   = false;
-            this.changeTrigger = "enter";
-        }
-
-        if (this.selectFocus) {
-            this.oInput.onmouseup = function() {
-                if (focusSelect) {
-                    this.select();
-                    focusSelect = false;
-                }
-
-                this.host.dispatchEvent("mouseup");
-                return false;
-            };
-        }
-
-        //Special validation support using nativate max-length browser support
-        if (x.getAttribute("maxlength") && this.oInput.tagName.toLowerCase().match(/input|textarea/))
-            this.oInput.maxLength = parseInt(x.getAttribute("maxlength"));
-        
-        //Autocomplete
-        var ac = $xmlns(x, "autocomplete", jpf.ns.jpf)[0];
-        if (ac) {
-            this.inherit(jpf.TextboxAutocomplete); /** @inherits jpf.TextboxAutocomplete */
-            this.initAutocomplete(ac);
-        }
-
-        if (x.getAttribute("width")) {
-            this.oInput.style.width = this.oInput.parentNode.offsetWidth - 6 
-                - Math.max(this.oButtonPlus.offsetWidth, this.oButtonMinus.offsetWidth) + "px";
-        }
-        
         jpf.JmlParser.parseChildren(this.$jml, null, this);
+        this.oInput.style.width = parseInt(this.width )
+                                - this.oButtonPlus.offsetWidth
+                                - jpf.getDiff(this.oInput)[1]
+                                - jpf.getDiff(this.oInt)[1] + "px";
     };
 
     this.$destroy = function() {
-        this.oInput.onkeypress = this.oInput.onmousedown =
-        this.oInput.onkeydown = this.oInput.onkeyup =
-        this.oButtonPlus.onmouseover = this.oButtonPlus.onmouseout =
-        this.oButtonPlus.onmousedown = this.oButtonPlus.onmouseup =
-        this.oButtonMinus.onmouseover = this.oButtonMinus.onmouseout =
-        this.oButtonMinus.onmousedown = this.oButtonMinus.onmouseup =
-        this.oInput.onselectstart = null;
+        this.oInput.onkeypress =
+        this.oInput.onmousedown =
+        this.oInput.onkeydown =
+        this.oInput.onkeyup =
+        this.oInput.onselectstart =
+        this.oButtonPlus.onmouseover =
+        this.oButtonPlus.onmouseout =
+        this.oButtonPlus.onmousedown =
+        this.oButtonPlus.onmouseup =
+        this.oButtonMinus.onmouseover =
+        this.oButtonMinus.onmouseout =
+        this.oButtonMinus.onmousedown =
+        this.oButtonMinus.onmouseup = null;
     };
 
     /**
@@ -530,7 +256,6 @@ jpf.spinner = function(pHtmlNode, tagName) {
      * 
      * @param {Number} max  Maximal number who could be in spinner
      */
-
     this.setMaximum = function(max) {
         this.maximum = max;
     };
@@ -540,8 +265,7 @@ jpf.spinner = function(pHtmlNode, tagName) {
      * 
      * @param {Number} min  Minimal number who could be in spinner
      */
-
     this.setMinimum = function(min) {
         this.minimum = min;
     };
-};
+}).implement(jpf.Presentation, jpf.DataBinding, jpf.JmlElement);
