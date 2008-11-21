19b8d5b911ce01068a69e124157922e761404e75
Ruben Daniels <ruben@ajax.org>
2008-11-21 09:46:54 +0000

    * Fix for weird regexp problem
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@956 3c3c2983-eddd-4584-88e6-321645f70290

--- core/class.js
+++ core/class.js
@@ -201,8 +201,10 @@ jpf.Class = function(){
         else if (pStart == "{") { //One Way Dynamic Properties
             var o, node, bProp, p, matches = {};
             pValue = pValue.substr(1, pValue.length - 2);
-            pValue.replace(/["'](?:\\.|[^"']+)*["']|\/(?:\\.|[^\/\\]+)*\/|(?:\W|^)([a-z]\w*\.\w+(?:\.\w+)*)(?!\()(?:\W|$)/g,
+            //Not removing  % first crashes the regular expression parser of IE and of FF. Are they using the same implementation or am I doing something wrong?
+            pValue.replace('%', '/').replace(/["'](?:\\.|[^"']+)*["']|\/(?:\\.|[^\/\\]+)*\/|(?:\W|^)([a-z]\w*\.\w+(?:\.\w+)*)(?!\()(?:\W|$)/gi,
                 function(m, m1){
+                    debugger;
                     if(m1) matches[m1] = true;
                 });
 			
