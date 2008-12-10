43c12b40715263cc22b152a76bfff25a5c3666e0
Mike de Boer <mike@ajax.org>
2008-12-10 22:13:28 +0000

    * added restful="true/false" option. Not used right now
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@1214 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/teleport/rpc/cgi.js
+++ elements/teleport/rpc/cgi.js
@@ -24,22 +24,22 @@
 
 /**
  * Implementation of the Common Gateway Interface (CGI) as a module for the RPC
- * plugin of jpf.teleport. 
+ * plugin of jpf.teleport.
  * Example:
  * Javeline Markup Language
  * <code>
  *  <j:teleport>
  *      <j:rpc id="comm" protocol="cgi">
- *          <j:method 
- *            name    = "searchProduct" 
- *            url     = "http://example.com/search.php" 
+ *          <j:method
+ *            name    = "searchProduct"
+ *            url     = "http://example.com/search.php"
  *            receive = "processSearch">
  *              <j:variable name="search" />
  *              <j:variable name="page" />
  *              <j:variable name="textbanner" value="1" />
  *          </j:method>
- *          <j:method 
- *            name = "loadProduct" 
+ *          <j:method
+ *            name = "loadProduct"
  *            url  = "http://example.com/show-product.php">
  *              <j:variable name="id" />
  *              <j:variable name="search_id" />
@@ -75,97 +75,101 @@
 jpf.cgi = function(){
     this.supportMulticall = false;
     this.namedArguments   = true;
-    
+
     // Register Communication Module
     jpf.teleport.register(this);
-    
+
     // Stand Alone
     if (!this.uniqueId) {
         jpf.makeClass(this);
         this.inherit(jpf.BaseComm, jpf.http, jpf.rpc);
     }
-    
+
     this.unserialize = function(str){
         return str;
     }
-    
+
     this.getSingleCall = function(name, args, obj){
         obj.push(args);
     }
-    
+
     // Create message to send
     this.serialize = function(functionName, args){
-        var vars = [];
-        
+        var vars    = [];
+        var restful = this.useRestResources;
+
         function recur(o, stack){
             if (o && o.dataType == "array") {
-                for (var j = 0; j < o.length; j++) 
+                for (var j = 0; j < o.length; j++)
                     recur(o[j], stack + "%5B" + j + "%5D");//" + j + "
             }
-            else 
+            else
                 if (typeof o == "object") {
                     for (prop in o) {
                         //#ifdef __SUPPORT_SAFARI2
-                        if (jpf.isSafariOld && (!o[prop] || typeof p[prop] != "object")) 
+                        if (jpf.isSafariOld && (!o[prop] || typeof p[prop] != "object"))
                             continue;
                         //#endif
-                        
-                        if (typeof o[prop] == "function") 
+
+                        if (typeof o[prop] == "function")
                             continue;
                         recur(o[prop], stack + "%5B" + encodeURIComponent(prop) + "%5D");
                     }
                 }
-                else 
-                    vars.push(stack + "=" + encodeURIComponent(o));
+                else
+                    vars.push(stack + (restful ? "/" : "=") + encodeURIComponent(o));
         };
-        
+
         if (this.multicall) {
-            vars.push("func=" + this.mcallname);
-            for (var i = 0; i < args[0].length; i++) 
+            vars.push("func" + (restful ? "/" : "=") + this.mcallname);
+            for (var i = 0; i < args[0].length; i++)
                 recur(args[0][i], "f%5B" + i + "%5D");
         }
         else {
             for (prop in args) {
                 //#ifdef __SUPPORT_SAFARI2
-                if (jpf.isSafariOld && (!args[prop] || typeof args[prop] == "function")) 
+                if (jpf.isSafariOld && (!args[prop] || typeof args[prop] == "function"))
                     continue;
                 //#endif
-                
+
                 recur(args[prop], prop);
             }
         }
-        
+
         if (!this.baseUrl)
             this.baseUrl = this.url;
-        
+
         this.url = this.urls[functionName]
             ? this.urls[functionName]
             : this.baseUrl;
-        
+
         if (this.method != "GET")
-            return vars.join("&");
-        
-        this.url = this.url + (vars.length 
-            ? (this.url.indexOf("?") > -1 ? "&" : "?") + vars.join("&")
+            return restFul ? vars.join("/") : vars.join("&");
+
+        this.url = this.url + (vars.length
+            ? restful
+                ? (this.url.lastIndexOf("/") != this.url.length - 1 ? "/" : "") + vars.join("/")
+                : (this.url.indexOf("?") > -1 ? "&" : "?") + vars.join("&")
             : "");
-        
+
         return "";
     }
-    
+
     this.$load = function(x){
         this.method      = (x.getAttribute("http-method") || "GET").toUpperCase();
-        this.contentType = this.method == "GET" 
+        this.contentType = this.method == "GET"
             ? null
             : "application/x-www-form-urlencoded";
+        this.useRestResources = jpf.isTrue(x.getAttribute("restful"));
 
         if (x.getAttribute("method-name")) {
             var mName = x.getAttribute("method-name");
             var nodes = x.childNodes;
-            
+
             for (var v, i = 0; i < nodes.length; i++) {
-                if (nodes[i].nodeType != 1) 
+                if (nodes[i].nodeType != 1)
                     continue;
-                
+
                 v = nodes[i].insertBefore(x.ownerDocument.createElement("variable"),
                     nodes[i].firstChild);
                 v.setAttribute("name",  mName);
@@ -173,51 +177,55 @@ jpf.cgi = function(){
             }
         }
     }
-    
+
     /**
      * Submit a form with ajax (GET)
      *
      * @param {HTMLElement} form      the html form element to submit.
-     * @param {Function} 	  callback  called when the http call returns.
+     * @param {Function}       callback  called when the http call returns.
      */
     this.submitForm = function(form, callback){
-        if (!this['postform']) 
+        if (!this['postform'])
             this.addMethod('postform', callback);
-        
+
         var args = [];
         for (var i = 0; i < form.elements.length; i++) {
-            if (!form.elements[i].name) 
+            if (!form.elements[i].name)
                 continue;
             if (form.elements[i].tagname = 'input'
               && (form.elements[i].type == 'checkbox'
               || form.elements[i].type == 'radio')
-              && !form.elements[i].checked) 
+              && !form.elements[i].checked)
                 continue;
-            
+
             if (form.elements[i].tagname = 'select' && form.elements[i].multiple) {
                 for (j = 0; j < form.elements[i].options.length; j++) {
-                    if (form.elements[i].options[j].selected) 
-                        args.push(form.elements[i].name + "="
+                    if (form.elements[i].options[j].selected)
+                        args.push(form.elements[i].name
+                            + (this.useRestResources ? "/" : "=")
                             + encodeURIComponent(form.elements[i].options[j].value));
                 }
             }
             else {
-                args.push(form.elements[i].name + "="
+                args.push(form.elements[i].name
+                    + (this.useRestResources ? "/" : "=")
                     + encodeURIComponent(form.elements[i].value));
             }
         }
-        
+
         var loc               = (form.action || location.href);
-        this.urls['postform'] = loc + (loc.indexOf("?") > -1 ? "&" : "?") + args.join("&");
+        this.urls['postform'] = this.useRestResources
+            ? loc + (loc.lastIndexOf("/") != loc.length - 1 ? "/" : "") + args.join("/")
+            : loc + (loc.indexOf("?") > -1 ? "&" : "?") + args.join("&");
         this['postform'].call(this);
-        
+
         return false;
     }
 }
 
 // #ifdef __WITH_DATA_INSTRUCTIONS
 
-jpf.datainstr["url"]        = 
+jpf.datainstr["url"]        =
 jpf.datainstr["url.post"]   =
 jpf.datainstr["url.delete"] =
 jpf.datainstr["url.put"]    =
@@ -225,9 +233,9 @@ jpf.datainstr["url.get"]    = function(xmlContext, options, callback){
     if (!options.parsed) {
         var query = options.instrData.join(":").split("?");
         var url   = query.shift();
-    
+
         //@todo change this parser to support {} and []
-        query = query.join("?").replace(/\=(xpath|eval)\:([^\&]*)\&/g, 
+        query = query.join("?").replace(/\=(xpath|eval)\:([^\&]*)\&/g,
           function(m, type, content){
             if (type == "xpath") {
                 var o = xmlNode.selectSingleNode(RegExp.$1);
@@ -245,30 +253,30 @@ jpf.datainstr["url.get"]    = function(xmlContext, options, callback){
                         if(options)
                             for(var prop in options)
                                 eval("var " + prop + " = options[prop]");
-                        
+
                         return eval(content);//RegExp.$1);
                     })();
                 }
                 catch(e){
                     //#ifdef __DEBUG
-                    throw new Error(jpf.formatErrorString(0, null, 
+                    throw new Error(jpf.formatErrorString(0, null,
                         "Saving/Loading data", "Could not execute javascript \
-                        code in process instruction '" + content 
+                        code in process instruction '" + content
                         + "' with error " + e.message));
                     //#endif
                 }
             }
-            
+
             return "=" + retvalue + "&";
         });
-    
+
         var args     = options.args;
         var httpBody = (args && args.length)
-            ? (args[0].nodeType 
-                ? args[0].xml || args[0].serialize() 
+            ? (args[0].nodeType
+                ? args[0].xml || args[0].serialize()
                 : jpf.serialize(args[0]))
             : query;
-    
+
         if (options.preparse) {
             options.parsed = [query, httpBody];
             options.preparse = false;
@@ -279,10 +287,10 @@ jpf.datainstr["url.get"]    = function(xmlContext, options, callback){
         var query    = options.parsed[0];
         var httpBody = options.parsed[1];
     }
-    
+
     var oHttp = new jpf.http();
     oHttp.method = (options.instrType.replace(/url.?/, "") || "GET").toUpperCase();
-    oHttp.get(url + (oHttp.method == "GET" ? "?" + query : ""), callback, 
+    oHttp.get(url + (oHttp.method == "GET" ? "?" + query : ""), callback,
         jpf.extend({data : httpBody}, options));
 }
 
