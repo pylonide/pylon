cbf77e956f889f9155a3514557f468cb82ccd0a8
Łukasz Lipiński <uzza17@gmail.com>
2009-01-13 07:27:52 +0000

    * coma char after last table element
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@1309 3c3c2983-eddd-4584-88e6-321645f70290

--- core/lib/date.js
+++ core/lib/date.js
@@ -58,7 +58,7 @@ jpf.date = {
         ],
         monthNumbers : {
             "Jan" : 0, "Feb" : 1, "Mar" : 2, "Apr" : 3, "May" : 4, "Jun" : 5,
-            "Jul" : 6, "Aug" : 7, "Sep" : 8, "Oct" : 9, "Nov" : 10, "Dec" : 11,
+            "Jul" : 6, "Aug" : 7, "Sep" : 8, "Oct" : 9, "Nov" : 10, "Dec" : 11
         }
     }
 };
