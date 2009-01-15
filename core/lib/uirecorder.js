5040caff2ea7b19fc6103b9d958915c6c3424e11
Łukasz Lipiński <uzza17@gmail.com>
2009-01-15 16:59:51 +0000

    * renaming on dblclick support has been added * support for scrolling has been addded
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@1325 3c3c2983-eddd-4584-88e6-321645f70290

--- core/lib/uirecorder.js
+++ core/lib/uirecorder.js
@@ -12,10 +12,9 @@ jpf.uirecorder = {
         jpf.uirecorder.inited = true;
 
         var dEvents = [
-            "onclick", "onmousedown", "onmouseup", "onmousemove", "onmouseover", "onmouseout", 
+            "onclick", "onmousedown", "onmouseup", "onmousemove", "onmouseover", "onmouseout", "ondblclick", 
             "onkeydown", "onkeyup", "onkeypress",
-            "onselect", "onchange", "onsubmit", "onreset", "onfocus", "onblur",
-            "onscroll"];
+            "onselect", "onchange", "onsubmit", "onreset", "onfocus", "onblur"];
 
         var mEvents = ["DOMSubtreeModified", "DOMNodeInserted", "DOMNodeRemoved", "DOMNodeRemovedFromDocument",
             "DOMNodeInsertedIntoDocument", "DOMAttrModified", "DOMCharacterDataModified", "DOMActivate"];
@@ -24,15 +23,15 @@ jpf.uirecorder = {
             document.documentElement[dEvents[i]] = function(e) {
                 if (jpf.uirecorder.isPlaying || !jpf.uirecorder.isRecording)
                     return;
-        
+
                 e = e || event;
-        
+
                 jpf.uirecorder.actionStack.push([new Date().getTime(), dEvents[i],
                     e.srcElement || e.target, jpf.extend({}, e)]);
             }
         }
         
-        for (var i = 0; i < mEvents.length; i++) {
+        for (var i = 0, l = mEvents.length; i < l; i++) {
             document.addEventListener(mEvents[i], function(e) {
                 if (jpf.uirecorder.isPlaying || !jpf.uirecorder.isRecording)
                     return;
@@ -42,7 +41,36 @@ jpf.uirecorder = {
                 jpf.uirecorder.actionStack.push([new Date().getTime(), mEvents[i], e.srcElement || e.target, jpf.extend({}, e)]);
             }, false);
         }
-        
+
+        document.addEventListener("DOMMouseScroll", function(e) {
+            if (jpf.uirecorder.isPlaying || !jpf.uirecorder.isRecording)
+                return;
+
+            e = e || event;
+
+            jpf.uirecorder.actionStack.push([
+                new Date().getTime(),
+                "DOMMouseScroll",
+                e.srcElement || e.target,
+                jpf.extend({}, jpf.uirecorder.createMouseWheelEvent(e))
+            ]);
+        }, false);
+    },
+    
+    createMouseWheelEvent : function(e) {
+        var delta = null;
+        if (e.wheelDelta) {
+            delta = e.wheelDelta / 120;
+            if (jpf.isOpera)
+                delta *= -1;
+        }
+        else if (e.detail)
+            delta = -e.detail / 3;
+
+        return {
+            type : "DOMMouseScroll",
+            delta : delta
+        }
     }
 };
 
@@ -54,9 +82,9 @@ jpf.uirecorder.record = function() {
 jpf.uirecorder.play = function() {
     jpf.uirecorder.isRecording = false;
     jpf.uirecorder.isPlaying   = true;
-    jpf.uirecorder.playStack = jpf.uirecorder.actionStack.slice(0);
+    jpf.uirecorder.playStack   = jpf.uirecorder.actionStack.slice(0);
 
-    playItem();
+    playFrame();
 };
 
 jpf.uirecorder.stop = function() {
@@ -67,39 +95,41 @@ jpf.uirecorder.stop = function() {
 jpf.uirecorder.reset = function() {
     jpf.uirecorder.isRecording = false;
     jpf.uirecorder.isPlaying   = false;
-    jpf.uirecorder.playStack = [];
+    jpf.uirecorder.playStack   = [];
     jpf.uirecorder.actionStack = [];
 };
 
 var timeout;
-function playItem() {
-    var item = jpf.uirecorder.playStack.shift();
+function playFrame() {
+    var frame = jpf.uirecorder.playStack.shift();
 
-    if(!item)
+    if(!frame)
         return;
 
-    var lastTime = item[0];
+    var lastTime = frame[0];
+    var simulate = false;
 
     if (jpf.isIE) {
         var e = document.createEventObject();
         
-        for (prop in item[3]) {
-            if (item[3][prop])
-                e[prop] = item[3][prop];
+        for (prop in frame[3]) {
+            if (frame[3][prop])
+                e[prop] = frame[3][prop];
         }
 
-        e.srcElement = e.target = item[2];//orginalnie byl tylko e.target
+        e.srcElement = e.target = frame[2];//orginalnie byl tylko e.target
 
-        item[2].fireEvent(item[1], e);
+        frame[2].fireEvent(frame[1], e);
     }
     else {
-        var src = item[3], e;
+        var src = frame[3], e;
 
         switch(src.type) {
             case "mousemove":
             case "mouseup":
             case "mousedown":
             case "click":
+            case "dblclick":
             case "mouseover":
             case "mouseout":
                 e = document.createEvent("MouseEvents");
@@ -123,7 +153,6 @@ function playItem() {
             case "focus":
             case "blur":
             case "resize":
-            case "scroll":
                 e = document.createEvent("HTMLEvents");
                 e.initEvent(src.type, src.bubbles, src.cancelable);
                 break;
@@ -131,27 +160,34 @@ function playItem() {
                 e = document.createEvent("UIEvents");
                 e.initUIEvent(src.type, src.bubbles, src.cancelable, e.view, e.detail);
                 break;
-           case "DOMAttrModified":
-           case "DOMCharacterDataModified":
-           case "DOMNodeInsertedIntoDocument":
-           case "DOMNodeRemovedFromDocument":
-           case "DOMNodeRemoved":
-           case "DOMNodeInserted":
-           case "DOMSubtreeModified":
-               e = document.createEvent("MutationEvents");
-               e.initMutationEvent(src.type, src.bubbles, src.cancelable, src.relatedNode, src.prevValue, src.newValue, src.attrName, src.attrChange);
-               break;
-           default:
-               jpf.console.info("default: "+src.type);
-               break;
+            case "DOMAttrModified":
+            case "DOMCharacterDataModified":
+            case "DOMNodeInsertedIntoDocument":
+            case "DOMNodeRemovedFromDocument":
+            case "DOMNodeRemoved":
+            case "DOMNodeInserted":
+            case "DOMSubtreeModified":
+                e = document.createEvent("MutationEvents");
+                e.initMutationEvent(src.type, src.bubbles, src.cancelable, src.relatedNode, src.prevValue, src.newValue, src.attrName, src.attrChange);
+                break;
+            case "DOMMouseScroll": //mouse scroll
+                 fireScroll(frame);
+                 simulate = true;
+                break;
+            default:
+                jpf.console.info("default: " + src.type);
+                break;
         }
-
-        item[2].dispatchEvent(e);
+        
+        if (!simulate) {
+            frame[2].dispatchEvent(e);
+        }
+        
     }
 
     if (jpf.uirecorder.playStack.length) {
         timeout = setTimeout(function() {
-            playItem();
+            playFrame();
         }, jpf.uirecorder.playStack[0][0] - lastTime);
     }
     else {
@@ -159,6 +195,25 @@ function playItem() {
         clearInterval(timeout);
     }
 };
+
+/**
+ * Actually supports only vertical scrolling
+ * 
+ * @param {Object} frame
+ */
+function fireScroll(frame) {
+    var el = frame[2];
+
+    while (el != document.body && el.scrollTop == 0) {
+        el = el.parentNode || el.parentElement;
+    }
+
+    var max = el.scrollHeight;
+    var actual = el.scrollTop;
+    var src = frame[3];
+
+    el.scrollTop = actual + -1*20*src.delta;
+}
 
 jpf.uirecorder.saveState = function() {
     
