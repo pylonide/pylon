94f1ed5a3b6ca614da48423495c84e17fd6cbd42
Mike de Boer <mike@ajax.org>
2009-01-29 14:02:34 +0000

    * minor fixes HTML parser
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@1436 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/editor.js
+++ elements/editor.js
@@ -237,6 +237,8 @@ jpf.editor = jpf.component(jpf.NODE_VISIBLE, function() {
             .replace(/<BR[^>]*_jpf_placeholder="1"\/?>/gi, '')
             .replace(/(\<(\w+).*?\>)|(\<\/(\w+?)\s*\>)/gi, function(m, fullstart, tagstart, fullend, tagend){
                    if (fullstart){
+                       if (tagstart == "LI" || tagstart == "BR")
+                           return fullstart;
                        depth++;
                        if (fullstart.indexOf("_jpf_placeholder") > -1) {
                            stack.push([tagstart, true]);
