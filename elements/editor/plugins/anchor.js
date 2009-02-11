6eabe535efe74a5c163655c5ea2c4b8932b38684
Mike de Boer <mike@ajax.org>
2009-02-11 11:21:43 +0000

    * fixed JML statement
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@1554 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/editor/plugins/anchor.js
+++ elements/editor/plugins/anchor.js
@@ -96,7 +96,7 @@ jpf.editor.plugin('anchor', function() {
 
         this.appendJmlNode(
             '<j:toolbar xmlns:j="' + jpf.ns.jml + '"><j:bar>\
-             <j:button caption="Insert"\
+             <j:button caption="Insert" \
                onclick="jpf.lookup(' + this.uniqueId + ').submit(event)" />\
              </j:bar></j:toolbar>',
           document.getElementById(idButton));
