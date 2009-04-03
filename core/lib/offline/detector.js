b7f6009d37bdc9a4d68ccaf7722bf081dadcc82f
Mike de Boer <mike@ajax.org>
2009-04-03 18:43:26 +0000

    * finished implementation of jpf.appsettings.resourcePath
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@1890 3c3c2983-eddd-4584-88e6-321645f70290

--- core/lib/offline/detector.js
+++ core/lib/offline/detector.js
@@ -47,7 +47,7 @@
  */
 jpf.namespace("offline.detector", {
     //#ifndef __PACKED
-    detectUrl : (jpf.appsettings.resourcePath || jpf.basePath) + "core/lib/offline/network_check.txt",
+    detectUrl : jpf.basePath + "core/lib/offline/network_check.txt",
     /* #else
     detectUrl : "network_check.txt",
     #endif */
@@ -58,6 +58,11 @@ jpf.namespace("offline.detector", {
         if (jml.nodeType) {
             if (jml.getAttribute("detect-url"))
                 this.detectUrl = jml.getAttribute("detect-url");
+            /* #ifdef __PACKAGED
+            else
+                this.detectUrl = (jpf.appsettings.resourcePath || jpf.basePath)
+                    + "resources/network_check.txt";
+            #endif */
 
             this.detection = jpf.isTrue(jml.getAttribute("detection"))
                 ? "auto"
