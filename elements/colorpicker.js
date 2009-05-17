a2b1fa958f74e158b425ad8fc01a5ba174bda035
Mike de Boer <mike@ajax.org>
2009-05-17 11:57:42 +0000

    * refactoring of colorpicker element - ready for skinning
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@2173 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/colorpicker.js
+++ elements/colorpicker.js
@@ -66,17 +66,18 @@ jpf.colorpicker = jpf.component(jpf.NODE_VISIBLE, function(){
     this.setValue = function(value, type){
         //this.value = value;
         if (!type) type = "RGBHEX";
+        var a;
         switch (type) {
             case "HSL":
                 this.fill(value[0], value[1], value[2]);
                 break;
             case "RGB":
-                var a = this.RGBtoHLS(value[0], value[1], value[2]);
+                a = RGBtoHLS(value[0], value[1], value[2]);
                 this.fill(a[0], a[1], a[2]);
                 break;
             case "RGBHEX":
                 var RGB = arguments[0].match(/(..)(..)(..)/);
-                var a = this.RGBtoHLS(Math.hexToDec(RGB[0]),
+                a = RGBtoHLS(Math.hexToDec(RGB[0]),
                     Math.hexToDec(RGB[1]), Math.hexToDec(RGB[2]));
                 this.fill(a[0], a[1], a[2]);
                 break;
@@ -84,104 +85,105 @@ jpf.colorpicker = jpf.component(jpf.NODE_VISIBLE, function(){
     };
 
     this.getValue = function(type){
-        return this.HSLRangeToRGB(this.cH, this.cS, this.cL);
+        return HSLRangeToRGB(cH, cS, cL);
     };
 
     // PRIVATE METHODS
-    this.cL       = 120;
-    this.cS       = 239;
-    this.cH       = 0;
-    this.cHex     = "#FF0000";
-    this.HSLRange = 240;
+    var cL       = 120,
+        cS       = 239,
+        cH       = 0,
+        cHex     = "#FF0000",
+        HSLRange = 240,
+        _self    = this;
 
-    this.HSLRangeToRGB = function(H, S, L){
-        return this.HSLtoRGB (H / (this.HSLRange-1), S / this.HSLRange,
-            Math.min(L / this.HSLRange, 1))
+    function HSLRangeToRGB(H, S, L){
+        return HSLtoRGB(H / (HSLRange - 1), S / HSLRange,
+            Math.min(L / HSLRange, 1))
     };
 
-    this.RGBtoHLS = function(R,G,B){
-        var RGBMAX = 255;
-        var HLSMAX = this.HSLRange;
-        var UNDEF  = (HLSMAX*2/3);
+    function RGBtoHLS(R,G,B){
+        var RGBMAX = 255,
+            HLSMAX = HSLRange,
+            UNDEF  = (HLSMAX*2/3),
 
-       /* calculate lightness */
-       cMax = Math.max(Math.max(R,G), B);
-       cMin = Math.min(Math.min(R,G), B);
-       L    = (((cMax + cMin) * HLSMAX) + RGBMAX) / (2 * RGBMAX);
+        /* calculate lightness */
+             cMax = Math.max(Math.max(R,G), B),
+             cMin = Math.min(Math.min(R,G), B);
+        L = (((cMax + cMin) * HLSMAX) + RGBMAX) / (2 * RGBMAX);
 
-       if (cMax == cMin) {           /* r=g=b --> achromatic case */
-          S = 0;                     /* saturation */
-          H = UNDEF;                     /* hue */
-       }
-       /* chromatic case */
-       else {
-          /* saturation */
-          if (L <= (HLSMAX/2))
-              S = (((cMax - cMin) * HLSMAX) + ((cMax + cMin) / 2)) / (cMax + cMin);
-          else
-              S = (((cMax - cMin) * HLSMAX) + ((2 * RGBMAX - cMax - cMin) / 2))
+        if (cMax == cMin) {           /* r=g=b --> achromatic case */
+            S = 0;                    /* saturation */
+            H = UNDEF;                /* hue */
+        }
+        /* chromatic case */
+        else {
+            /* saturation */
+            if (L <= (HLSMAX/2))
+                S = (((cMax - cMin) * HLSMAX) + ((cMax + cMin) / 2)) / (cMax + cMin);
+            else
+                S = (((cMax - cMin) * HLSMAX) + ((2 * RGBMAX - cMax - cMin) / 2))
                   / (2 * RGBMAX - cMax - cMin);
 
-          /* hue */
-          Rdelta = (((cMax - R) * (HLSMAX / 6)) + ((cMax - cMin) / 2)) / (cMax - cMin);
-          Gdelta = (((cMax - G) * (HLSMAX / 6)) + ((cMax - cMin) / 2)) / (cMax - cMin);
-          Bdelta = (((cMax - B) * (HLSMAX / 6)) + ((cMax - cMin) / 2)) / (cMax - cMin);
+            /* hue */
+            Rdelta = (((cMax - R) * (HLSMAX / 6)) + ((cMax - cMin) / 2)) / (cMax - cMin);
+            Gdelta = (((cMax - G) * (HLSMAX / 6)) + ((cMax - cMin) / 2)) / (cMax - cMin);
+            Bdelta = (((cMax - B) * (HLSMAX / 6)) + ((cMax - cMin) / 2)) / (cMax - cMin);
 
-          if (R == cMax)
-              H = Bdelta - Gdelta;
-          else if (G == cMax)
-              H = (HLSMAX / 3) + Rdelta - Bdelta;
-          else
-              H = ((2 * HLSMAX) / 3) + Gdelta - Rdelta;
+            if (R == cMax)
+                H = Bdelta - Gdelta;
+            else if (G == cMax)
+                H = (HLSMAX / 3) + Rdelta - Bdelta;
+            else
+                H = ((2 * HLSMAX) / 3) + Gdelta - Rdelta;
 
-          if (H < 0)
-              H += HLSMAX;
-          if (H > HLSMAX)
-              H -= HLSMAX;
-       }
+            if (H < 0)
+                H += HLSMAX;
+            if (H > HLSMAX)
+                H -= HLSMAX;
+        }
 
-       return [H, S, L];
-    };
+        return [H, S, L];
+    }
 
-    this.HueToColorValue = function(Hue){
+    function hueToColorValue(hue){
         var V;
 
-        if (Hue < 0)
-            Hue = Hue + 1
-        else if (Hue > 1)
-            Hue = Hue - 1;
+        if (hue < 0)
+            hue = hue + 1
+        else if (hue > 1)
+            hue = hue - 1;
 
-        if (6 * Hue < 1)
-            V = M1 + (M2 - M1) * Hue * 6
-        else if (2 * Hue < 1)
+        if (6 * hue < 1)
+            V = M1 + (M2 - M1) * hue * 6
+        else if (2 * hue < 1)
             V = M2
-        else if (3 * Hue < 2)
-            V = M1 + (M2 - M1) * (2 / 3 - Hue) * 6
+        else if (3 * hue < 2)
+            V = M1 + (M2 - M1) * (2 / 3 - hue) * 6
         else
             V = M1;
 
         return Math.max(Math.floor(255 * V), 0);
     };
 
-    this.HSLtoRGB = function(H, S, L){
+    function HSLtoRGB(H, S, L){
         var R,  G,  B;
 
         if (S == 0)
             G = B = R = Math.round (255 * L);
-        else{
+        else {
             M2 = (L <= 0.5) ? (L * (1 + S)) : (L + S - L * S);
 
             M1 = 2 * L - M2;
-            R = this.HueToColorValue(H + 1 / 3);
-            G = this.HueToColorValue(H);
-            B = this.HueToColorValue(H - 1 / 3);
+            R = hueToColorValue(H + 1 / 3);
+            G = hueToColorValue(H);
+            B = hueToColorValue(H - 1 / 3);
         }
 
         return Math.decToHex(R) + "" + Math.decToHex(G) + "" + Math.decToHex(B);
     };
 
     this.fill = function(H, S, L){
-        var Hex    = this.HSLRangeToRGB(H,S,L);
+        var Hex    = HSLRangeToRGB(H,S,L);
         this.value = Hex;
 
         //RGB
@@ -202,76 +204,71 @@ jpf.colorpicker = jpf.component(jpf.NODE_VISIBLE, function(){
         this.shower.style.backgroundColor = Hex;
 
         //Luminance
-        var HSL120                        = this.HSLRangeToRGB(H, S, 120);
+        var HSL120                        = HSLRangeToRGB(H, S, 120);
         this.bar1.style.backgroundColor   = HSL120;
-        this.bgBar1.style.backgroundColor = this.HSLRangeToRGB(H, S, 240);
-        this.bar2.style.backgroundColor   = this.HSLRangeToRGB(H, S, 0);
+        this.bgBar1.style.backgroundColor = HSLRangeToRGB(H, S, 240);
+        this.bar2.style.backgroundColor   = HSLRangeToRGB(H, S, 0);
         this.bgBar2.style.backgroundColor = HSL120;
     };
 
     this.movePointer = function(e){
         e = e || window.event;
-        var cs = colorPicker;
 
-        var ty = cs.pHolder.ty;
+        var ty = _self.pHolder.ty;
         if ((e.clientY - ty >= 0) && (e.clientY - ty
-          <= cs.pHolder.offsetHeight - cs.pointer.offsetHeight + 22))
-            cs.pointer.style.top = e.clientY - ty;
+          <= _self.pHolder.offsetHeight - _self.pointer.offsetHeight + 22))
+            _self.pointer.style.top = e.clientY - ty;
         if (e.clientY - ty < 21)
-            cs.pointer.style.top = 21;
+            _self.pointer.style.top = 21;
         if (e.clientY - ty
-          > cs.pHolder.offsetHeight - cs.pointer.offsetHeight + 19)
-            cs.pointer.style.top = cs.pHolder.offsetHeight
-                - cs.pointer.offsetHeight + 19;
+          > _self.pHolder.offsetHeight - _self.pointer.offsetHeight + 19)
+            _self.pointer.style.top = _self.pHolder.offsetHeight
+                - _self.pointer.offsetHeight + 19;
 
-        var y = cs.pointer.offsetTop - 22;
-        cs.cL = (255-y) / 2.56 * 2.4;
-        cs.fill(cs.cH, cs.cS, cs.cL);
+        // 255 - posY:
+        cL = (255 - (_self.pointer.offsetTop - 22)) / 2.56 * 2.4;
+        _self.fill(cH, cS, cL);
 
         e.returnValue  = false;
         e.cancelBubble = true;
     };
 
     this.setLogic = function(){
-        this.pHolder.host         = this;
         this.pHolder.style.zIndex = 10;
         this.pHolder.onmousedown  = function(){
-            var colorPicker = this.host;
 
             this.ty = jpf.getAbsolutePosition(this)[1] - 20;
 
-            this.host.movePointer();
-            document.onmousemove = this.host.movePointer
+            _self.movePointer();
+            document.onmousemove = _self.movePointer
             document.onmouseup   = function(){ this.onmousemove = function(){}; };
         }
 
-        this.container.host        = this;
         this.container.onmousedown = function(e){
             e = e || window.event;
-            var colorPicker = this.host;
 
             this.active = true;
             if (e.srcElement == this) {
                 if (e.offsetX >= 0 && e.offsetX <= 256
                   && e.offsetY >= 0 && e.offsetY <= 256) {
-                    this.host.cS = (256 - e.offsetY) / 2.56 * 2.4
-                    this.host.cH = e.offsetX / 2.56 * 2.39
+                    cS = (256 - e.offsetY) / 2.56 * 2.4
+                    cH = e.offsetX / 2.56 * 2.39
                 }
-                this.host.fill(this.host.cH, this.host.cS, this.host.cL);
-                this.host.shower.style.backgroundColor = this.host.currentColor;
+                _self.fill(cH, cS, cL);
+                _self.shower.style.backgroundColor = _self.currentColor;
             }
-            this.host.point.style.display = "none";
+            _self.point.style.display = "none";
 
             e.cancelBubble = true;
         }
 
         this.container.onmouseup = function(e){
             this.active                   = false;
-            this.host.point.style.top     = event.offsetY - this.host.point.offsetHeight - 2;
-            this.host.point.style.left    = event.offsetX - this.host.point.offsetWidth - 2;
-            this.host.point.style.display = "block";
+            _self.point.style.top     = event.offsetY - _self.point.offsetHeight - 2;
+            _self.point.style.left    = event.offsetX - _self.point.offsetWidth - 2;
+            _self.point.style.display = "block";
 
-            this.host.change(this.host.tbHexColor.value);
+            _self.change(_self.tbHexColor.value);
         }
 
         this.container.onmousemove = function(e){
@@ -279,11 +276,11 @@ jpf.colorpicker = jpf.component(jpf.NODE_VISIBLE, function(){
             if (this.active) {
                 if (e.offsetX >= 0 && e.offsetX <= 256
                   && e.offsetY >= 0 && e.offsetY <= 256) {
-                    this.host.cS = (256 - e.offsetY) / 2.56 * 2.4
-                    this.host.cH = e.offsetX / 2.56 * 2.39
+                    cS = (256 - e.offsetY) / 2.56 * 2.4
+                    cH = e.offsetX / 2.56 * 2.39
                 }
-                this.host.fill(this.host.cH, this.host.cS, this.host.cL);
-                this.host.shower.style.backgroundColor = this.host.currentColor;
+                _self.fill(cH, cS, cL);
+                _self.shower.style.backgroundColor = _self.currentColor;
             }
         }
 
@@ -291,10 +288,10 @@ jpf.colorpicker = jpf.component(jpf.NODE_VISIBLE, function(){
         this.tbRed.host =
         this.tbGreen.host =
         this.tbBlue.host = this;
-        this.tbHexColor.onblur = function(){this.host.setValue("RGBHEX", this.value);}
-        this.tbRed.onblur = function(){this.host.setValue("RGB", this.value, this.host.tbGreen.value, this.host.tbBlue.value);}
-        this.tbGreen.onblur = function(){this.host.setValue("RGB", this.host.tbRed.value, this.value, this.host.tbBlue.value);}
-        this.tbBlue.onblur = function(){this.host.setValue("RGB", this.host.tbRed.value, this.host.tbGreen.value, this.value);}
+        this.tbHexColor.onblur = function(){_self.setValue("RGBHEX", this.value);}
+        this.tbRed.onblur = function(){_self.setValue("RGB", this.value, _self.tbGreen.value, _self.tbBlue.value);}
+        this.tbGreen.onblur = function(){_self.setValue("RGB", _self.tbRed.value, this.value, _self.tbBlue.value);}
+        this.tbBlue.onblur = function(){_self.setValue("RGB", _self.tbRed.value, _self.tbGreen.value, this.value);}
         */
     }
 
@@ -314,9 +311,8 @@ jpf.colorpicker = jpf.component(jpf.NODE_VISIBLE, function(){
         this.tbLuminance = this.$getLayoutNode("main", "luminance", this.oExt);
 
         this.tbHexColor          = this.$getLayoutNode("main", "hex", this.oExt);
-        this.tbHexColor.host     = this;
         this.tbHexColor.onchange = function(){
-            this.host.setValue(this.value, "RGBHEX");
+            _self.setValue(this.value, "RGBHEX");
         }
 
         this.shower = this.$getLayoutNode("main", "shower", this.oExt);
@@ -342,7 +338,7 @@ jpf.colorpicker = jpf.component(jpf.NODE_VISIBLE, function(){
         this.setLogic();
 
         this.setValue("ffffff");
-        //this.fill(this.cH, this.cS, this.cL);
+        //this.fill(cH, cS, cL);
     }
 
     this.$loadJml = function(x){
