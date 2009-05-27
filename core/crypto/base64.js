9c30d33bcd09aa84d94a554be2f308766c6c1477
Mike de Boer <mike@ajax.org>
2009-05-27 08:29:11 +0000

    * improved formatting/ codeflow
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@2224 3c3c2983-eddd-4584-88e6-321645f70290

--- core/crypto/base64.js
+++ core/crypto/base64.js
@@ -43,11 +43,10 @@ jpf.crypto.Base64 = (function() {
             enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
             enc4 = chr3 & 63;
 
-            if (isNaN(chr2)) {
+            if (isNaN(chr2))
                 enc3 = enc4 = 64;
-            } else if (isNaN(chr3)) {
+            else if (isNaN(chr3))
                 enc4 = 64;
-            }
 
             output = output +
               keyStr.charAt(enc1) + keyStr.charAt(enc2) +
@@ -109,10 +108,12 @@ jpf.crypto.UTF8 = {
 
             if (c < 128) {
                 utftext += String.fromCharCode(c);
-            } else if((c > 127) && (c < 2048)) {
+            }
+            else if((c > 127) && (c < 2048)) {
                 utftext += String.fromCharCode((c >> 6) | 192);
                 utftext += String.fromCharCode((c & 63) | 128);
-            } else {
+            }
+            else {
                 utftext += String.fromCharCode((c >> 12) | 224);
                 utftext += String.fromCharCode(((c >> 6) & 63) | 128);
                 utftext += String.fromCharCode((c & 63) | 128);
@@ -125,9 +126,9 @@ jpf.crypto.UTF8 = {
 
     // private method for UTF-8 decoding
     decode : function (utftext) {
-        var string = "";
-        var i = 0;
-        var c = c1 = c2 = 0;
+        var string = "",
+            c, c2, c3, i = 0;
+        c = c2 = c3 = 0;
 
         while (i < utftext.length) {
             c = utftext.charCodeAt(i);
@@ -135,11 +136,13 @@ jpf.crypto.UTF8 = {
             if (c < 128) {
                 string += String.fromCharCode(c);
                 i++;
-            } else if((c > 191) && (c < 224)) {
+            }
+            else if((c > 191) && (c < 224)) {
                 c2 = utftext.charCodeAt(i+1);
                 string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
                 i += 2;
-            } else {
+            }
+            else {
                 c2 = utftext.charCodeAt(i+1);
                 c3 = utftext.charCodeAt(i+2);
                 string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
