949600b06d6c721201dfd3198d7dfc2ddc30179c
Ruben Daniels <ruben@ajax.org>
2009-06-07 18:38:56 +0000

    * Updated esthetic things for the property editor
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@2266 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/datagrid.js
+++ elements/datagrid.js
@@ -1703,7 +1703,7 @@ jpf.datagrid    = jpf.component(jpf.NODE_VISIBLE, function(){
                 y       : (isMultiple ? mirrorNode.firstChild.firstChild.offsetHeight : mirrorNode.offsetHeight) - 1,
                 animate : true,
                 ref     : mirrorNode,
-                width   : mirrorNode.offsetWidth - widthdiff + 5, //Math.max(self[this.lookupjml].minwidth, 
+                width   : mirrorNode.offsetWidth - widthdiff + 4, //Math.max(self[this.lookupjml].minwidth, 
                 height  : height,
                 callback: function(){
                     oContainer.style.height = "auto";
