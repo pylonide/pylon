a0f73244b350e59ca4583548202bf66f66b9f90a
Giannis <giannis@ajax.org>
2009-07-15 13:04:28 +0000

    * space remove at ifdef in draw_canvas.js
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@2508 3c3c2983-eddd-4584-88e6-321645f70290

--- core/lib/draw_canvas.js
+++ core/lib/draw_canvas.js
@@ -18,7 +18,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  *
  */
-// #ifdef __WITH_DRAW
+//#ifdef __WITH_DRAW
 //#ifdef __ENABLE_DRAW_CANVAS
 jpf.namespace("draw.canvas",{
    
