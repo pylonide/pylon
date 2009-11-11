b27951ce585a51346a1d66014b0db64f9ff074c1
Łukasz Lipiński <uzza17@gmail.com>
2009-11-11 15:40:12 +0000

    * block renamed to item
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@3611 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/flowchart.js
+++ elements/flowchart.js
@@ -951,12 +951,12 @@ apf.flowchart = function(struct, tagName){
         this.$flowVars.lastBlockId++;
         //apf.flow.alert_r(xmlNode)
         apf.console.info("ADD");
-        this.$getNewContext("block");
-        var block            = this.$getLayoutNode("block"),
-            elSelect         = this.$getLayoutNode("block", "select"),
-            elImage          = this.$getLayoutNode("block", "image"),
-            elimageContainer = this.$getLayoutNode("block", "imagecontainer"),
-            elCaption        = this.$getLayoutNode("block", "caption");
+        this.$getNewContext("item");
+        var block            = this.$getLayoutNode("item"),
+            elSelect         = this.$getLayoutNode("item", "select"),
+            elImage          = this.$getLayoutNode("item", "image"),
+            elimageContainer = this.$getLayoutNode("item", "imagecontainer"),
+            elCaption        = this.$getLayoutNode("item", "caption");
 
         if (elCaption)
             elCaption.setAttribute("ondblclick", 'apf.lookup(' + this.$uniqueId
