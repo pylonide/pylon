050c3ab9f957104a0b1a15a24355ae0ea93d0015
Linh <linh@ajax.org>
2009-11-27 16:40:13 +0000

    update vectorflow
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@3730 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/vectorflow.js
+++ elements/vectorflow.js
@@ -51,51 +51,196 @@ apf.vectorflow = function(struct, tagName){
     this.bufferselect  = true;
     
     /**** Properties and Attributes ****/
-
+    this.nodeIdIndex = 1;
 
     /**** Public Methods ****/
     
 
     /**** Databinding Support ****/
 
-    this.blocks = [];
     //Here each xml node from the data is rendered
     this.$add = function(xmlNode, Lid, xmlParentNode, htmlParentNode, beforeNode, isLast){
-        this.blocks.push({
-            caption: this.$applyBindRule("caption", xmlNode),
-            width: this.$applyBindRule("width", xmlNode),
-            height: this.$applyBindRule("height", xmlNode)
-        });
-        //this.$int.innerHTML += this.$applyBindRule("caption", xmlNode) + "<br />";
-        //return parentNode;
+        var node = {
+            id              : this.$applyBindRule("id", xmlNode) || "n" + this.nodeIdIndex++,
+            caption         : this.$applyBindRule("caption", xmlNode) || "",
+            type            : this.$applyBindRule("type", xmlNode) || "rect",
+            x               : parseInt(this.$applyBindRule("left", xmlNode)) || 0,
+            y               : parseInt(this.$applyBindRule("top", xmlNode)) || 0,
+            top             : parseInt(this.$applyBindRule("top", xmlNode)) || 0,
+            left            : parseInt(this.$applyBindRule("left", xmlNode)) || 0,
+            width           : parseInt(this.$applyBindRule("width", xmlNode)) || 0,
+            height          : parseInt(this.$applyBindRule("height", xmlNode)) || 0,
+            fill            : xmlNode.getAttribute("fill")
+        };
+
+        // set default line attach positions
+        node.lines = {
+            top:    {x: node.x, y: node.y - node.height/2},
+            left:   {x: node.x - node.width/2, y: node.y},
+            right:  {x: node.x + node.width/2, y: node.y},
+            bottom: {x: node.x, y: node.y + node.height/2},
+        }
+        
+        if ("line".indexOf(node.type) > -1) {
+            node.from       = xmlNode.getAttribute("from");
+            node.to         = xmlNode.getAttribute("to");
+        }
+        else if ("circle|rect|decision".indexOf(node.type) > -1) {
+            node.top    -= node.height/2;
+            node.left   -= node.width/2;
+            node.bottom = node.y + node.height/2;
+            node.right  = node.x + node.width/2;
+        }
+        if ("circle".indexOf(node.type) > -1) {
+            node.width /= 2;
+            node.height /= 2;
+        }
+        
+        if (this.nodes[node.id]) {
+            apf.console.error("block with id " + node.id + " already exists");
+            return;
+        }
+        this.nodes[node.id] = node;
     };
     
     //This function is called to do the final render pass
     this.$fill = function(){
-        /*
-        for (var i = 0, l = this.blocks.length; i < l; i++) {
-            //draw block
-            this.$drawBlock(this.blocks[i]);
-            this.$int.innerHTML += this.blocks[i].caption + "<br />";
-        }
-        */
-        //this.$int.innerHTML += "Done"
+        var err = {};
+        this.$style = apf.draw.parseStyle(this.$node_style , "", err );
+
+        apf.draw.initLayer(this.$rootLayer, this);
+        this.$drawCode = this.$compile( this.$rootLayer, this.$style );    
+        this.$drawCode(this.$rootLayer, this);
+        apf.draw.resizeLayer(this.$rootLayer);
     };
 
-    // draw block
-    this.$drawBlock = function(l){
-        e = apf.draw.vml;
+    this.$compile = function(l,s){
+        var e = apf.draw;
+
         var c = e.optimize([
-            e.vars(),
             e.beginLayer(l),
+            e.vars(),
+            e.clear(),
+            e.beginShape(s.node),
+            "var type;",
+            "for (var name in this.nodes) {",
+                "type=this.nodes[name].type;",
+                "if (type === 'rect') {", 
+                    e.rect(
+                        "this.nodes[name].left",
+                        "this.nodes[name].top",
+                        "this.nodes[name].width",
+                        "this.nodes[name].height"
+                    ),
+//e.beginFont(s.label,  0, 0, 0, 0),
+//e.text(0, 0, "Label"), 
+                "}",
+                "else if (type === 'decision') {", 
+                    e.moveTo("this.nodes[name].lines.top.x", "this.nodes[name].lines.top.y"),
+                    e.lineTo("this.nodes[name].lines.right.x", "this.nodes[name].lines.right.y"),
+                    e.lineTo("this.nodes[name].lines.bottom.x", "this.nodes[name].lines.bottom.y"),
+                    e.lineTo("this.nodes[name].lines.left.x", "this.nodes[name].lines.left.y"),
+                    e.lineTo("this.nodes[name].lines.top.x", "this.nodes[name].lines.top.y"),
+                "}",
+                "else if (type === 'circle') {", e.ellipse("this.nodes[name].x","this.nodes[name].y","this.nodes[name].width","this.nodes[name].height"), "}",
+                "else if (type === 'line') {",
+                    "if (this.nodes[name].from && this.nodes[name].to) {",
+                        "var fromPos, toPos;",
+                        "if (this.nodes[this.nodes[name].from].y == this.nodes[this.nodes[name].to].y) {",
+                            "if (this.nodes[this.nodes[name].from].right < this.nodes[this.nodes[name].to].left) {",
+                                "fromPos = 'right';", 
+                                "toPos = 'left';",
+                            "}",
+                            "else {",
+                                "fromPos = 'left';", 
+                                "toPos = 'right';",
+                            "}",
+                        "}",
+                        "else if (this.nodes[this.nodes[name].from].x == this.nodes[this.nodes[name].to].x) {",
+                            "if (this.nodes[this.nodes[name].from].bottom > this.nodes[this.nodes[name].to].top) {",
+                                "fromPos = 'top';", 
+                                "toPos = 'bottom';",
+                            "}",
+                            "else {",
+                                "fromPos = 'bottom';", 
+                                "toPos = 'top';",
+                            "}",
+                        "}",
+
+                        "else if (this.nodes[this.nodes[name].from].right < this.nodes[this.nodes[name].to].left) {",
+                            "fromPos = 'right';", 
+                            "toPos = 'left';",
+                        "}",
+                        "else if (this.nodes[this.nodes[name].from].left > this.nodes[this.nodes[name].to].right) {",
+                            "fromPos = 'left';", 
+                            "toPos = 'right';",
+                        "}",
+                        "else {",
+                            "if (this.nodes[this.nodes[name].from].bottom > this.nodes[this.nodes[name].to].top) {",
+                                "fromPos = 'top';", 
+                                "toPos = 'bottom';",
+                            "}",
+                            "else {",
+                                "fromPos = 'bottom';", 
+                                "toPos = 'top';",
+                            "}",
+                        "}",
+                        e.moveTo(
+                            "this.nodes[this.nodes[name].from].lines[fromPos].x",
+                            "this.nodes[this.nodes[name].from].lines[fromPos].y"
+                        ),
+                        e.lineTo(
+                            "this.nodes[this.nodes[name].to].lines[toPos].x",
+                            "this.nodes[this.nodes[name].to].lines[toPos].y"
+                        ),
+                    "}",
+                "}",
+                e.close(),
+            "}",
             e.endLayer()
-            ].join(''));
+        ].join(''));
+
         try{
-           // c = apf.formatJS(c);
             return new Function('l','v','m',c);
         }catch(x){
             alert("Failed to compile:\n"+c);return 0;
         }
+    }
+    
+    this.$createShape = function(l, e) {
+        if (l.type == "line") {
+            return [
+                
+            ].join('');
+        }
+        else if (l.type == "rect") {
+            return [
+                e.rect(0,0,l.width,l.height)
+            ].join('');
+        }
+        else if ("decision|conditional".indexOf(l.type) > -1) {
+            var top = 0, left = 0, 
+            bottom = top + l.height, right = left + l.width, 
+            hCenter = left + l.width/2, vCenter = top + l.height/2;
+            return [
+                e.moveTo(hCenter, top),
+                e.lineTo(right, vCenter),
+                e.lineTo(hCenter, bottom),
+                e.lineTo(left, vCenter),
+                e.lineTo(hCenter, top),
+            ].join('');
+        }
+    }
+    
+    // draw block
+    this.$drawShape = function(l){
+        l.err = {};
+        l.style = apf.draw.parseStyle(this.$node_style , "", l.err );;
+        apf.draw.initLayer( l, this );
+        
+        this.$drawCode = this.$compile( l, l.style);    
+        this.$drawCode(l, this);
+        apf.draw.resizeLayer(l);
     }
     
     
@@ -410,34 +555,29 @@ apf.vectorflow = function(struct, tagName){
     this.$node_style = {
         node : {
             inherit:'shape',
-            fill:'red',
-            stroke:'green',
-        $:1}
+            fill:'white',
+            stroke:'black',
+        $:1},
+        label : {
+            inherit : 'font', 
+            width: 40,
+            top : 5,
+            left: 0,
+            side: 0, 
+            axis: 0, 
+            edgeclip : 2,
+            align:'center',
+        $:1},
     },
-    this.$layer1 = {
+    this.$rootLayer = {
+        type: "rect",
         left: 0,
         top: 0,
-        width:300,
-        height: 300
+        width: 800,
+        height: 800,
+        fill: "yellow"
     };
-    
-    this.$compile = function(l,s){
-        var e = apf.draw;
-        var c = e.optimize([
-            e.vars(),
-            e.beginLayer(l),
-            e.clear(),
-            e.beginShape(s.node),
-            e.rect(30,30,70,70),
-            e.endLayer()
-            ].join(''));
-        try{
-            return new Function('l','v','m',c);
-        }catch(x){
-            alert("Failed to compile:\n"+c);return 0;
-        }
-    }
-    
+
     this.addEventListener("$clear", function(){
         return false;
     })
@@ -446,20 +586,13 @@ apf.vectorflow = function(struct, tagName){
         //Build Main Skin
         this.$ext = this.$getExternal(); 
         this.$int = this.$getLayoutNode("main", "container", this.$ext);
+
         this.top = 0, this.left = 0,
-        this.width = 300, this.height = 300,
+        this.width = 800, this.height = 800;
         //Any initialization code comes here
         apf.draw.initDriver();
         apf.draw.initRoot(this);
-        apf.draw.initLayer(this.$layer1, this);
-       
         apf.draw.resizeRoot(this);
-        apf.draw.resizeLayer(this.$layer1);
-        
-        var err = {};
-        this.$style = apf.draw.parseStyle(this.$node_style , "", err );
-        this.$drawCode = this.$compile( this.$layer1, this.$style );    
-        this.$drawCode(this.$layer1,this);
     };
     
     this.$loadAml = function(){
@@ -472,9 +605,9 @@ apf.vectorflow = function(struct, tagName){
 // #ifdef __WITH_MULTISELECT
 }).call(apf.vectorflow.prototype = new apf.MultiSelect());
 /* #elseif __WITH_DATABINDING
-}).call(apf.tree.prototype = new apf.MultiselectBinding());
+}).call(apf.vectorflow.prototype = new apf.MultiselectBinding());
    #else
-}).call(apf.tree.prototype = new apf.Presentation());
+}).call(apf.vectorflow.prototype = new apf.Presentation());
 #endif*/
 
 apf.aml.setElement("vectorflow", apf.vectorflow);
