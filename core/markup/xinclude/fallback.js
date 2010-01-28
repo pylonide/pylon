8ad85ffc2232c1b9c5a89b56e869e710c46040ed
Linh <linh@ajax.org>
2010-01-28 13:50:20 +0000

    changed ifdefs
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@4058 3c3c2983-eddd-4584-88e6-321645f70290

--- core/markup/xinclude/fallback.js
+++ core/markup/xinclude/fallback.js
@@ -19,7 +19,7 @@
  *
  */
 
-//#ifdef __PARSER_XSD || __WITH_XFORMS || __WITH_MODEL_VALIDATION
+//#ifdef __WITH_XIFALLBACK
 /**
  * Defines a list of acceptable values
  */
--- core/markup/xinclude/include.js
+++ core/markup/xinclude/include.js
@@ -19,7 +19,7 @@
  *
  */
 
-//#ifdef __PARSER_XSD || __WITH_XFORMS || __WITH_MODEL_VALIDATION
+//#ifdef __WITH_XIINCLUDE
 /**
  * Defines a list of acceptable values
  */
