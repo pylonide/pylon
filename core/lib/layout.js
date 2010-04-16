7f81cacf592b51e03b28503c61b6f54ea585d5a9
Ruben Daniels <ruben@ajax.org>
2010-04-16 22:36:45 +0000

    * removed debug code
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@4694 3c3c2983-eddd-4584-88e6-321645f70290

--- core/lib/layout.js
+++ core/lib/layout.js
@@ -1268,11 +1268,7 @@ apf.layout = {
     getHtmlId : function(oHtml){
         //if(apf.hasSingleRszEvent) return 1;
         //else
-        try{
         return oHtml.getAttribute ? oHtml.getAttribute("id") : 1;
-    }catch(e){
-        debugger;
-    }
     },
 
     /**
