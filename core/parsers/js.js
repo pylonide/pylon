0d88dba1117dac6ec41d07da13fc7090afbd16de
Linh <linh@ajax.org>
2010-04-19 08:55:15 +0000

    * Updated the javascript parser
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@4707 3c3c2983-eddd-4584-88e6-321645f70290

--- core/parsers/js.js
+++ core/parsers/js.js
@@ -31,200 +31,201 @@
  * @version     %I%, %G%
  * @since       0.9
  */
-apf.JSImplementation = function(){
-    //------------------------------------------------------------------------------------
+apf.JsParser = new (function(){
+    var tok_lut = {
+            '"': 4, '\'': 4, '[': 2, ']': 3, '{': 2, '}': 3, '(': 2, ')': 3,
+            '\n': 6, '\r\n': 6, '//': 7, '/*': 7, '*/': 7, '/':8
+        },
+        tok_close = {'}': '{', ']': '[', ')': '('},
+        tokenizerx    = /(\r?[\n]|\/[\/*]|\*\/|["'{(\[\])}\]\/])|([ \t]+)|([\w\$._])+|(\\?[\w._?,:;!=+-\\\/^&|*"'[\]{}()%$#@~`<>])/g;    
     
-    var types = ['[', '{', '(', 'text', 'xpath', 'word', 'sep', 'ws', 'semi', 'sh', 'op', 'col', 'str', 'regex', 'comment'];
-    var closes = [']', '}', ')'];
-    var reParse = new RegExp();
-    reParse.compile("([\\w_\\.]+)|([\\s]*,[\\s]*)|([\\s]*;[\\s]*)|(\\/\\*)|(\\*\\/)|(\\/\\/)|([\\r\\n])|((?:[\\s]*)[\\$\\@\\#\\%\\^\\&\\*\\?\\!](?:[\\s]*))|([\\s]*[\\+\\-\\<\\>\\|\\=]+[\\s]*)|(\\s*\\:\\s*)|(\\s+)|(\\\\[\\\\\\{\\}\\[\\]\\\"\\'\\/])|(\\[)|(\\])|([\\s]*\\([\\s]*)|([\\s]*\\)[\\s]*)|([\\s]*\\{[\\s]*)|([\\s]*\\}[\\s]*)|(\\')|(\\\")|(\\/)", "g");
+    function errToString(){
+        return this.t + '[' + this.pos + ']';
+    }
     
-    //------------------------------------------------------------------------------------
+    function posToString(){
+        return 'Ln : ' + this.line + ', Col : ' + this.col;
+    }
     
-    this.dump_tree = function(n, s, w){
-        for (var i = 0; i < n.length; i++) {
-            var m = n[i], t = m[0];
-            if (t < 3) {
-                s.push(w + types[t]);
-                this.dump_tree(m[1], s, '  ' + w);
-                s.push(closes[t]);
-                s.push('\n');
-            }
-            else {
-                s.push(w + types[t] + ': ' + m[1] + '\n');
-            }
+    function argToString(){
+        return this.type + ' ' + this.name
+            + (this.def !== undefined ? '=' + this.def : '');
+    }
+    
+    function markerToString(){
+        return 'Marker:' + this.type + '  [' + this.pos + '] '+ (this.args ? '('
+            + this.args.join(',') + ')' : '');
+    }
+    
+    function funcToString(){
+        return this.type + ' ' + this.ret + ' ' + this.name + (this.args ? '('
+            + this.args.join(',') + ')' : '') + '  [' + this.pos + ']'
+            + (this.doc ? (' Doc: ' + this.doc) : '');
+    }
+    
+    function concat(t, b, e, m){
+        for (var s = [], sl = 0, i = b; i < e; i += 3) {
+            s[sl++] = (t[i] == 2)
+                ? concat(t[i + 2], 0, t[i + 2].length)
+                : t[i + 2];
+            if(m && i<e-3) s[sl++] = m;    
         }
-        return s;
+        return s.join('');
+    }    
+    
+    function formatPos(l,pos){
+        return JsParser.line(l, pos).toString();
+    }
+
+    var pre_regex = {
+        'throw': 1, 'return': 1, '(': 1, '[': 1, ',': 1, '=': 1, ":": 1
+    }        
+    
+    this.parse = function(str){
+        var t = [],     // parse tree
+            b = 0,      // block output
+            stack = [], // tree stack
+            type = 0,   // token type
+            mode_tok = 0, // parsemode, contains char of block we parse
+            n,          // tempvar
+            lines = [], // array of linepositions
+            err = [];   // tokenize array
+        var last_tok = null;
+        str.replace(tokenizerx, function(tok, rx_lut, rx_ws, rx_word, rx_misc, pos){
+            type = rx_lut ? tok_lut[rx_lut] : (rx_ws ? 9 : (rx_word ? 5 : 0)); //5 = word
+            //logw( type+" "+pos+"\n");
+            if (!mode_tok) {
+                switch (type) {
+                    case 8: // regex
+                        // previous is: throw return ( , [
+                        if (pre_regex[last_tok] || pre_regex[t[t.length-1]]) {
+                            if (t[t.length-3] == "[") {
+                                mode_tok = "objRgx";
+                                var openTags = 1;
+                            } else
+                                mode_tok = tok;
+                            
+                            t[n = t.length] = type,
+                            t[++n] = pos;
+                            
+                            b = [tok];
+                        } else {
+                            t[n = t.length] = 0;
+                            t[++n] = pos;
+                            t[++n] = tok; 
+                        }
+                        //debugger;
+                        break;                
+                    case 4: //String 
+                    case 7: //Comment
+                        t[n=t.length] = type,
+                        t[++n] = pos;
+                        mode_tok = tok;
+                        b = [tok];
+                        break;
+                    case 2: //[ ( {
+                        t[n = t.length] = type;
+                        t[++n] = pos;
+                        stack.push(t);
+                        t = t[++n] = [0, pos, tok];
+                        break;
+                    case 3: // } ) ]
+                        if (t[2] != tok_close[tok])  {                            
+                            err.push({t: "Error closing " + tok + " (opened with: " + t[2] + ")", pos: pos, toString: errToString});                                                                                                                            
+                        }
+                        else {
+                            t[n = t.length] = type;
+                            t[++n] = pos;
+                            t[++n] = tok;
+                            t      = stack.pop();
+                        }
+                        break;
+                    case 6: // \n
+                        lines[lines.length] = pos;
+                        break;
+                    case 9: // white space
+                        break;
+                    default: // word
+                        t[n = t.length] = type;
+                        t[++n] = pos;
+                        t[++n] = tok;
+                        break;
+                }
+            }
+            else { //In comment or string mode
+                b[b.length] = tok;
+                if (mode_tok == "objRgx") {
+                    if (tok == "[") { 
+                        openTags++;
+                    } else if (tok == "]") {
+                        openTags--;
+                    }
+                    if (openTags == 0) {
+                        mode_tok = 0;
+                        t[++n] = b.join('');
+                    }
+                    //debugger;
+                }
+                switch (type) {
+                    case 4: //String
+                        if (mode_tok == tok){
+                            mode_tok = 0;
+                            t[++n] = b.join('');
+                        }
+                        break;
+                    case 7: //Comment
+                        if (tok == '*/' && mode_tok == '/*') {
+                            mode_tok = 0;
+                            t[++n] = b.join('');
+                        } else if (tok == '*/' && mode_tok == '/') {
+                            mode_tok = 0;
+                            t[++n] = b.join('');
+                        }
+                        break;
+                    case 8: // regex
+                        if(mode_tok == '/'){
+                            mode_tok = 0;
+                            t[++n] = b.join('');
+                        }
+                        break;
+                    case 6: //New line
+                        lines[lines.length] = pos;
+                        if (mode_tok == '//'){
+                            mode_tok = 0;
+                            b.pop();
+                            t[++n] = b.join('');
+                        }
+                        break;
+                }
+            }
+            if(type<9)last_tok = tok;
+        });
+        while (stack.length)
+            err.push({t: "Not closed: " + (n = t[2], t = stack.pop(), n), pos: formatPos(lines,t[t.length - 2]), toString: errToString});
+        if (mode_tok)
+            err.push({t: "Blockmode not closed of " + b[0], pos: formatPos(lines,t[t.length - 1]), toString: errToString});
+                        
+        return {tree: t, lines: lines, err: err};
     };
     
-    this.parse = function(str, trim_startspace){
-        var err    = []; // error list
-        var tree   = []; // parse tree
-        var stack  = []; // scopestack
-        var node   = tree;
-        var blevel = 0, tpos = 0;
-        var istr   = 0, icc = 0;
-        var lm     = 0;
-        var count  = [0, 0, 0];
-        
-        //tokenize
-        //return str;
-        //str = str.replace(/\/\*[\s\S]*?\*\//gm,"");
-        str.replace(reParse, function(m, word, sep, semi, c1, c2, c3, nl, sh,
-          op, col, ws, bs1, bo, bc, po, pc, co, cc, q1, q2, re, pos){
-            // stack helper functions
-            function add_track(t){
-                var txt = trim_startspace
-                    ? str.substr(tpos, pos - tpos)
-                        .replace(/[\r\n]\s*/, '').replace(/^\s*[\r\n]/, '')
-                        .replace(/[\r\n\t]/g, '')
-                    : str.substr(tpos, pos - tpos).replace(/[\r\n\t]/g, '');
-                if (txt.length > 0) {
-                    node[node.length] = [t, txt, tpos, pos];
-                }
-            }
-            
-            function add_node(t, data){
-                node[node.length] = [t, data, pos];
-            }
-            
-            function add_sub(t){
-                count[t]++;
-                var n = [];
-                node[node.length] = [t, n, pos];
-                stack[stack.length] = node;
-                node = n;
-            }
-            
-            function pop_sub(t){
-                count[t]--;
-                if (stack.length == 0) {
-                    err[err.length] = ["extra " + closes[t], pos];
-                }
-                else {
-                    node = stack.pop();
-                    var ot = node[node.length - 1][0];
-                    if (ot != t) {
-                        err[err.length] = ["scope mismatch " + types[ot] + " with " + types[t], pos];
-                    }
-                }
-            }
-            
-            if (!istr) {
-                if (word) {
-                    add_node(5, m);
-                    if (m == 'macro') 
-                        lm = 1;
-                    else 
-                        if (lm) 
-                            macros[m] = 1, lm = 0;
-                }
-                if (sep) 
-                    add_node(6, ',');
-                if (ws) 
-                    add_node(7, m);
-                if (semi) 
-                    add_node(8, m);
-                if (sh) 
-                    add_node(9, m);
-                if (op) 
-                    add_node(10, m);
-                if (col) 
-                    add_node(11, m);
-                if (bo) {
-                    add_sub(0);
-                }
-                if (bc) {
-                    pop_sub(0);
-                }
-                if (co) 
-                    add_sub(1);
-                if (cc) 
-                    pop_sub(1);
-                if (po) 
-                    add_sub(2);
-                if (pc) 
-                    pop_sub(2);
-            }
-            if (c3) {
-                if (istr == 0) {
-                    istr = 5;
-                    tpos = pos + 2;
-                }
-            };
-            if (nl) {
-                if (istr == 5) {
-                    istr = 0;
-                    pos += 1;
-                    add_track(14);
-                }
-            }
-            if (q1) {
-                if (istr == 0) {
-                    istr = 1;
-                    tpos = pos;
-                } else 
-                    if (istr == 1) {
-                        istr = 0;
-                        pos += 1;
-                        add_track(12);
-                    }
-            }
-            if (q2) {
-                if (istr == 0) {
-                    istr = 2;
-                    tpos = pos;
-                } else 
-                    if (istr == 2) {
-                        istr = 0;
-                        pos += 1;
-                        add_track(12);
-                    }
-            }
-            if (c1) {
-                if (istr == 0) {
-                    istr = 4;
-                    tpos = pos + 2;
-                };
-                            }
-            if (c2) {
-                if (istr == 4) {
-                    istr = 0;
-                    add_track(14);
-                }
-            }
-            if (re) {
-                if (istr == 0) {
-                    // only allow regex mode if we have no previous siblings or we have a , before us
-                    if (node.length == 0 || node[node.length - 1][0] == 6) {
-                        istr = 3;
-                        tpos = pos;
-                    } else 
-                        add_node(10, m);
-                }
-                else 
-                    if (istr == 3) {
-                        istr = 0;
-                        pos += 1;
-                        add_track(13);
-                    }
-            }
-            return m;
-        });
-        //return parse tree
-        if (stack.length > 0) 
-            for (var i = stack.length - 1; i >= 0; i--) {
-                var j = stack[i][stack[i].length - 1];
-                err[err.length] = ["unclosed tag " + types[j[0]], j[2]];
-            }
-        return {
-            'tree' : tree,
-            'stack': stack,
-            'err'  : err,
-            'count': count
-        };
+    this.dump = function(s, t, level){
+        for (var i = 0; i < t.length; i += 3) {
+            var type = t[i];
+            if (type ==2)
+                this.dump(s, t[i + 2], level + 1);
+            else 
+                s.push(Array(level).join('----'), type, " ", t[i + 2], "\n");
+        }
     };
-};
-apf.JavascriptParser = new apf.JSImplementation();
+    
+    this.line = function(lines,pos){
+        for (var i = 0, j = lines.length; i < j && lines[i] < pos; i++);
+        return {line: i+1, col: pos - lines[i - 1], toString: posToString};
+    };
+    
+    this.scan = function(t, tag_prefix, classes, err, in_struct, scope, outer, base_class){
+        
+    }    
+})();
 
 //#endif
