31ada88955b8e8f1cb4a11f722ebd99f19c58e52
Rik Arends <rik@ajax.org>
2010-04-22 15:23:43 +0000

    * xhr test
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@4756 3c3c2983-eddd-4584-88e6-321645f70290

--- test/apf_stub.js
+++ test/apf_stub.js
@@ -37,28 +37,37 @@ var apf = {
 	profile_end:function(id){
 		return (new Date()).getTime() - apf.$profiler[id];
 	},
-	profile_log : function(id,txt){
-		apf.logw("Profiling run "+(txt?txt:"")+" "+((new Date()).getTime() - apf.$profiler[id])+" ms");
+	profile_log : function(id,txt,delta){
+		apf.logw("Profiling run "+(txt?txt:"")+" "+((new Date()).getTime() - apf.$profiler[id] - (delta||0))+" ms");
 	},
 	profile_loop : function(times,func){
+        function test(){};
+        var delta = (new Date()).getTime();
+        for(var i = 0;i<times;i++) test();
+        delta = (new Date()).getTime() - delta;
 		apf.profile_start("loop");
 		for(var i = 0;i<times;i++)
 			func();
-		apf.profile_log("loop");
+		apf.profile_log("loop",null,delta);
 	},
 	logw:function( txt ){
 		// lets find our log div
 		var e = document.getElementById('_logw');
 		if(!e){
 			// lets inject it
-			document.body.insertAdjacentHTML("beforeend", 
-				"<div id='_logw' style='width:100%;height:500px;overflow-y:scroll;overflow-x:scroll;white-space:break-word;background-color:black;color:gray;font-family:courier;font-size:8pt;'></div>"
-			);
+            var t = "<div id='_logw' style='width:100%;height:500px;overflow-y:scroll;overflow-x:scroll;white-space:break-word;background-color:black;color:gray;font-family:courier;font-size:8pt;'></div>";
+            if(!document.body.insertAdjacentHTML)
+                document.body.innerHTML += t;
+            else
+                document.body.insertAdjacentHTML("beforeend",t); 
 			e = document.getElementById('_logw');
 		}
 		if(e){
-			var t = txt+'';
-			e.insertAdjacentHTML("beforeend", t.replace(/\</g, "&lt;").replace(/\</g, "&gt;").replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp;&nbsp;&nbsp;")+"<br/>");
+			var t = (txt+'').replace(/\</g, "&lt;").replace(/\</g, "&gt;").replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp;&nbsp;&nbsp;")+"<br/>";
+            if(!e.insertAdjacentHTML)
+                e.innerHTML += t;
+            else
+                e.insertAdjacentHTML("beforeend", t);
 			e.scrollTop = e.scrollHeight;
 		}
 		return txt;
