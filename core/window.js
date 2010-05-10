24a88e57b53ed979c7e1034928377e464c9808dd
Linh <linh@ajax.org>
2010-05-10 14:11:33 +0000

    * small fix by Ruben
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@4914 3c3c2983-eddd-4584-88e6-321645f70290

--- core/window.js
+++ core/window.js
@@ -1471,37 +1471,37 @@ apf.window = function(){
                 // #endif
           }
         }); //async
-
-        //#ifdef __WITH_WINDOW_FOCUS
-        var lastFocusElement;
-        this.addEventListener("focus", function(e){
-            if (!apf.document.activeElement && lastFocusParent && !apf.isIphone) {
-                lastFocusElement.focus();
-                /*
-                if (lastFocusParent.$isWindowContainer < 0) {
-                    if (lastFocusParent.$tabList.length)
-                        apf.window.moveNext(null, lastFocusParent.$tabList[0]);
-                    else
-                        apf.window.$focus(lastFocusParent);
-                }
-                else 
-                    apf.window.$focusLast(lastFocusParent);*/
-            }
-        });
-        this.addEventListener("blur", function(e){
-            if (!apf.document.activeElement || apf.isIphone)
-                return;
-    
-            apf.document.activeElement.blur(true, {srcElement: this});//, {cancelBubble: true}
-            lastFocusParent   = apf.document.activeElement.$focusParent;
-            lastFocusElement  = apf.document.activeElement;
-            apf.activeElement = apf.document.activeElement = null;
-        });
-        this.getLastActiveElement = function(){
-            return apf.activeElement || lastFocusElement;
-        }
-        //#endif
     };
+    
+    //#ifdef __WITH_WINDOW_FOCUS
+    var lastFocusElement;
+    this.addEventListener("focus", function(e){
+        if (!apf.document.activeElement && lastFocusParent && !apf.isIphone) {
+            lastFocusElement.focus();
+            /*
+            if (lastFocusParent.$isWindowContainer < 0) {
+                if (lastFocusParent.$tabList.length)
+                    apf.window.moveNext(null, lastFocusParent.$tabList[0]);
+                else
+                    apf.window.$focus(lastFocusParent);
+            }
+            else 
+                apf.window.$focusLast(lastFocusParent);*/
+        }
+    });
+    this.addEventListener("blur", function(e){
+        if (!apf.document.activeElement || apf.isIphone)
+            return;
+
+        apf.document.activeElement.blur(true, {srcElement: this});//, {cancelBubble: true}
+        lastFocusParent   = apf.document.activeElement.$focusParent;
+        lastFocusElement  = apf.document.activeElement;
+        apf.activeElement = apf.document.activeElement = null;
+    });
+    this.getLastActiveElement = function(){
+        return apf.activeElement || lastFocusElement;
+    }
+    //#endif
 
     /**
      * @private
