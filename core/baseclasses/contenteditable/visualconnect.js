55291ccce1aa331c5f9085062030d18b07ac9795
Linh <linh@ajax.org>
2010-06-02 16:05:49 +0000

    * applied design to visualconnect * added attributes dropdown menu in new design * delete button working in new design * changing value working in new design * worked on positioning connection drawing and making interaction work on connection elements
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@5092 3c3c2983-eddd-4584-88e6-321645f70290

--- core/baseclasses/contenteditable/visualconnect.js
+++ core/baseclasses/contenteditable/visualconnect.js
@@ -42,6 +42,104 @@ apf.visualConnect = function (sel){
     var _self = this;
     var prevSelection;
     
+    var css = '.atchart_box {\
+    color       : #333333;\
+    font-family : Tahoma;\
+    font-size   : 120px;\
+    height      : 29px;\
+    min-width   : 30px;\
+    min-height  : 29px;\
+    max-height  : 29px;\
+    overflow    : hidden;\
+    cursor      : default;\
+    position    : absolute;\
+}\
+\
+.atchart_box .left {\
+    float         : left;\
+    width         : 5px;\
+    height        : 29px;\
+}\
+\
+.atchart_box .lbl {\
+    height        : 24px;\
+    padding-top   : 5px;\
+    text-align    : center;\
+    margin        : 0 5px;\
+    white-space   : nowrap;\
+    overflow      : hidden;\
+    text-overflow : ellipsis;\
+}\
+\
+.atchart_box .right {\
+    float        : right;\
+    width        : 5px;\
+    height       : 29px;\
+}\
+\
+.atchart_box .red_button {\
+    float        : right;\
+    margin-left  : 2px;\
+    width        : 20px;\
+    background   : url("images/delete.png") no-repeat 50% 50%;\
+    height       : 29px;\
+}\
+\
+.atchart_box .left {\
+    background : url("images/backg_left.png") no-repeat 0 0;\
+}\
+\
+.atchart_box .lbl {\
+    background : url("images/backg_middle.png") repeat-x 0 0;\
+}\
+\
+.atchart_box .right {\
+    background : url("images/backg_right.png") no-repeat 0 0;\
+}\
+\
+.atchart_box span,\
+.atchart_box label,\
+.atchart_box input {\
+    padding  : 1px 0;\
+    border   : 1px solid transparent;\
+    position : relative;\
+    overflow : hidden;\
+    float:left;\
+    font-weight:bold;\
+}\
+\
+.atchart_box span.section1 {\
+}\
+\
+.atchart_box span.section2 {\
+    color:#3799ea;\
+    text-decoration:underline;\
+    padding-right:10px;\
+    background : url("images/arrow_down.png") no-repeat right 7px;\
+}\
+\
+.atchart_box span.section2:hover,\
+.atchart_box input.section5:hover {\
+    background-color:#fcfcfc;\
+}\
+\
+.atchart_box span.section3 {\
+    margin:0 2px;\
+}\
+\
+.atchart_box span.section4 {\
+    color:#aaaaaa;\
+}\
+\
+INPUT {\
+    border:0;\
+    width:105px;\
+    background-color:#fcfcfc;\
+    color       : #333333;\
+    font-family : Tahoma;\
+    font-size   : 12px;\
+}';
+    apf.importCssString(css);
     // init draw api
     var width = document.body.clientWidth;
     //@todo adjust height to browser window height?
@@ -94,7 +192,6 @@ apf.visualConnect = function (sel){
             div.style.left = "0px";
             div.style.top = "0px";
             div.style.zIndex = 100000001;
-            
     /*
             var showAllTimer = setTimeout(function(){
                 // lets show the drawing till someone clicks and then its gone
@@ -482,6 +579,7 @@ apf.visualConnect = function (sel){
         }
 
         function getTemplate() {
+        /*
             var oDiv = document.createElement("div");
             oDiv.style.position = "absolute";
             oDiv.style.width = "320px";
@@ -519,6 +617,12 @@ apf.visualConnect = function (sel){
             oDiv.appendChild(tgtInput);
             oDiv.appendChild(saveDiv);
             oDiv.appendChild(delDiv);
+        */
+
+            var oDiv = document.createElement("div");
+            oDiv.setAttribute("class", "atchart_box");
+            oDiv.setAttribute("style", "width:285px;color:#000000;font-family:Tahoma;font-size:12px;height:29px;min-width:30px;min-height:29px;max-height:29px;overflow:hidden;cursor:default;position:absolute;");
+            oDiv.innerHTML = '<div class="left"> </div><div class="red_button"> </div><div class="right"> </div><div class="lbl"><span class="section1">Button.</span><span class="section2">caption</span><span class="section3">=</span><span class="section4">&quot;</span><label class="section5">{button2.value}</label><span class="section4">&quot;</span></div>';
             
             return oDiv;
         }
@@ -535,44 +639,150 @@ apf.visualConnect = function (sel){
             div.style.position = "absolute";
             div.style.left = "0px";
             div.style.top = "0px";
+            div.style.width = width + "px";
+            div.style.height = height + "px";
+            
+            div.onmousedown = div.onmouseup = function(e) {
+                (e||event).cancelBubble = true;
+            }
+
             div.style.zIndex = 100000001;
             
             for (var id in connections) {
-                for (var aDiv, centerPos, pos1, pos2, i = 0, l = connections[id].length; i < l; i++) {
+                var curConnections = connections[id];
+                for (var toId, toConns, i = 0, l = connections[id].length; i < l; i++) {
+                    if (toConns = connections[(toId = connections[id][i].to.el.id)]) {
+                        for (var ji = 0, jl = toConns.length; ji < jl; ji++) {
+                            if (toConns[ji].to.el.id == id) {
+                                moveConn = toConns.splice(ji, 1)[0];
+                                curConnections.push(moveConn);
+                            }
+                        }
+                    }
+                    //if (connections[id][i]
+                }
+                
+                for (var aDiv, container, containerEls, ddAtts, delBtn, centerPos, pos1, pos2, i = 0, l = curConnections.length; i < l; i++) {
                     // default positions for lines, start and end
-                    pos1 = connections[id][i].from.pos;
-                    pos2 = connections[id][i].to.pos;
-
+                    pos1 = curConnections[i].from.pos;
+                    pos2 = curConnections[i].to.pos;
                     // calculate center of line
                     centerPos = [Math.round((pos1[0]+pos2[0])/2), Math.round((pos1[1]+pos2[1])/2)];
                     
+                    
                     // value divs
                     aDiv = cTemplate.cloneNode(true);
-                    aDiv.getElementsByTagName("div")[0].innerHTML = connections[id][i].from.el.id + "." + connections[id][i].from.at
+                    //aDiv.getElementsByTagName("div")[0].innerHTML = curConnections[i].from.el.id + "." + curConnections[i].from.at
+                    
+                    container = aDiv.getElementsByTagName("div")[3];
+                    (containerEls = container.getElementsByTagName("span"))[0].innerHTML = (fromEl = curConnections[i].from.el).id + ".";
+                    (ddAtts = containerEls[1]).innerHTML = curConnections[i].from.at;
+                    ddAtts.setAttribute("el", fromEl.id);
+                    (lblVal = container.getElementsByTagName("label")[0]).innerHTML = "{" + curConnections[i].to.el.id + "." + curConnections[i].to.at + "}";
+                    lblVal.setAttribute("el", curConnections[i].from.el.id);
+                    lblVal.setAttribute("at", curConnections[i].from.at);
+                    /* @todo add interaction to value (make editable)
                     aDiv.getElementsByTagName("input")[0].id = id + "_" + i;
-                    aDiv.getElementsByTagName("input")[0].value = "{" + connections[id][i].to.el.id + "." + connections[id][i].to.at + "}";
+                    aDiv.getElementsByTagName("input")[0].value = "{" + curConnections[i].to.el.id + "." + curConnections[i].to.at + "}";
                     aDiv.getElementsByTagName("input")[0].onmousedown = function(e) {
                         (e || event).cancelBubble = true;
                     }
                     aDiv.getElementsByTagName("input")[0].onmouseup = function(e) {
                         (e || event).cancelBubble = true;
                     }
-                    // redraw on property change
-                    var at = connections[id][i].from.at;
-                    //connections[id][i].from.el.addEventListener("prop." + connections[id][i].from.at, draw);
+                    */
                     
                     // saveBtn
-                    aDiv.getElementsByTagName("div")[1].setAttribute("onmousedown", connections[id][i].from.el.id + ".setAttribute('" + connections[id][i].from.at + "', document.getElementById('" + id + "_" + i + "').value); event.cancelBubble = true;");
+                    /*
+                    aDiv.getElementsByTagName("div")[1].setAttribute("onmousedown", curConnections[i].from.el.id + ".setAttribute('" + curConnections[i].from.at + "', document.getElementById('" + id + "_" + i + "').value); event.cancelBubble = true;");
                     aDiv.getElementsByTagName("div")[1].setAttribute("onmouseup", "event.cancelBubble = true;");
+                    */
                     
                     // delBtn
-                    aDiv.getElementsByTagName("div")[2].setAttribute("el", connections[id][i].from.el.id);
-                    aDiv.getElementsByTagName("div")[2].setAttribute("at", connections[id][i].from.at);
-                    aDiv.getElementsByTagName("div")[2].onmousedown = function(e) {
+                    (delBtn = aDiv.getElementsByTagName("div")[1]).setAttribute("el", curConnections[i].from.el.id);
+                    delBtn.setAttribute("at", curConnections[i].from.at);
+                    ddAtts.onmousedown = function(e) {
+                        (e||event).cancelBubble = true;
+                    }
+                    ddAtts.onmouseup  = function(e) {
+                        var e = e || event;
+                        if (!this.getAttribute("el")) return;;
+                        var fromEl = apf.document.getElementById(this.getAttribute("el"));
+                        if (!fromEl) return;
+                        for (var name, attList = [], ai = 0, al = fromEl.attributes.length; ai < al; ai++) {
+                            if (ignoreFromAtts.indexOf((name = fromEl.attributes[ai].name)) > -1) continue;
+                            attList.push(new apf.item({
+                                caption: name
+                            }));
+                        }
+
+                        attMenu = new apf.menu({
+                          htmlNode   : div,
+                          id         : "attMenu",
+                          childNodes : attList
+                        });
+                        
+                        var pos = apf.getAbsolutePosition(this);
+
+                        var x = pos[0], y = pos[1] + this.offsetHeight;
+                        setTimeout(function(){
+                            attMenu.display(x, y);
+                            attMenu.$ext.style.zIndex = 100000002;
+                        });
+                        attMenu.addEventListener("mousedown", function(e) {
+                            //apf.console.info("mousedown on attMenu");
+                            (e||event).cancelBubble = true;
+                        });
+                        attMenu.addEventListener("mouseup", function(e) {
+                            //apf.console.info("mouseup on attMenu");
+                            (e||event).cancelBubble = true;
+                        });
+                        attMenu.addEventListener("itemclick", function(e) {
+                            this.innerHTML = e.value;
+                            attMenu.setProperty("visible", false);
+                            showConnections();
+                            //fromEl.setAttribute(e.value, inputVal.value);
+                            (e||event).cancelBubble = true;
+                        });                        
                         
                         (e||event).cancelBubble = true;
                     }
-                    aDiv.getElementsByTagName("div")[2].onmouseup = function(e) {
+                    lblVal.onmousedown = function(e) {
+                        (e||event).cancelBubble = true;
+                    }
+                    lblVal.onmouseover = function(e) {
+                        var lblVal = this;
+                        var inputVal = document.createElement("input");
+                        inputVal.setAttribute("type", "text");
+                        inputVal.setAttribute("class", "section5");
+                        inputVal.setAttribute("value", lblVal.innerHTML);
+                        inputVal.onkeydown = inputVal.onkeypress = inputVal.onmousedown = inputVal.onmouseup = function(e) {
+                            (e||event).cancelBubble = true;
+                        }
+                        inputVal.onblur = function() {
+                            this.onkeydown = this.onkeypress = this.onkeyup = null;
+                            lblVal.setAttribute("value", this.value);
+                            apf.document.getElementById(lblVal.getAttribute("el")).setAttribute(lblVal.getAttribute("at"), this.value);
+                            this.replaceNode(lblVal, this);
+                            
+                            (e||event).cancelBubble = true;
+                        }
+                        inputVal.onkeyup = function(e) {
+                            if ((e||event).keyCode == 13) {
+                                lblVal.setAttribute("value", "x"+this.value);
+                                apf.document.getElementById(lblVal.getAttribute("el")).setAttribute(lblVal.getAttribute("at"), this.value);
+                                //this.replaceNode(lblVal, this);
+                            }
+                            (e||event).cancelBubble = true;
+                        }
+                        lblVal.replaceNode(inputVal, lblVal);
+                        
+                        (e||event).cancelBubble = true;
+                    }
+                    delBtn.onmousedown = function(e) {
+                        (e||event).cancelBubble = true;
+                    }
+                    delBtn.onmouseup = function(e) {
                         apf.document.getElementById(this.getAttribute("el")).setAttribute(this.getAttribute("at"), '');
                         if (createConnections(selection))
                             showConnections();
@@ -585,7 +795,7 @@ apf.visualConnect = function (sel){
                     }
                     
                     // delBtn
-                    //aDiv.getElementsByTagName("div")[2].setAttribute("onmousedown", connections[id][i].from.el.id + ".setAttribute('" + connections[id][i].from.at + "', ''); event.cancelBubble = true;");
+                    //aDiv.getElementsByTagName("div")[2].setAttribute("onmousedown", curConnections[i].from.el.id + ".setAttribute('" + curConnections[i].from.at + "', ''); event.cancelBubble = true;");
                     //aDiv.getElementsByTagName("div")[2].setAttribute("onmouseup", "event.cancelBubble = true;");
                     
                     // even number of connections
