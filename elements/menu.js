3d8bef6183cd539ad6e22fe3f9fa9bc5cffca17c
Łukasz Lipiński <uzza17@gmail.com>
2010-06-07 08:56:19 +0000

    * fix for patch from my last commit
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@5101 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/menu.js
+++ elements/menu.js
@@ -283,7 +283,9 @@ apf.menu = function(struct, tagName){
             : apf.menu.lastFocus || apf.document.activeElement;
         
         apf.popup.last = null;
-        //this.focus();
+        
+        if (!apf.isGecko)
+            this.focus();
 
         //Make the component that provides context appear to have focus
 
