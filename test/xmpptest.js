f3711454262357c6abf4491fb32c66e4affc3b90
Eddy Bruel <eddy@ajax.org>
2010-07-26 15:14:23 +0000

    Prototyped the first 7 tests for XMPP
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@5278 3c3c2983-eddd-4584-88e6-321645f70290

--- test/xmpptest.js
+++ test/xmpptest.js
@@ -86,12 +86,9 @@ function hookErrors(xmpp){
 	xmpp.addEventListener("authfailure", func);
 	xmpp.addEventListener("connectionerror", func);
 	xmpp.addEventListener("registererror", func);
-	//unknownstanza
 }
 
 function test0() {
-    var testName = "Test1";
-    
 	// Create a bot
     var bot1	= createBot("bot1", "bot1", 1);
 
@@ -106,30 +103,30 @@ function test0() {
 	bot1.remote.addEventListener("rdbinit", function (e) {
 		//Set initial document
 		console.log("### RDBINIT called: " + e.resource + ", " + e.model);
-		e.model.load("<data><caption>" + testName + ": 1</caption></data>");
+		e.model.load("<data><caption>" + user + ": 1</caption></data>");
 		model = e.model;
 		
 		//Set priority
-		bot1.xmpp.setAttribute("priority", bot1.xmpp.priority + 10);
-		bot1.xmpp.botRegister("worknets.com");
+		cMyXmpp.setAttribute("priority", cMyXmpp.priority + 10);
+		cMyXmpp.botRegister("worknets.com");
 	});
 	
-	var count = 0;
 	client1.label.addEventListener("prop.caption", function(){
-		if (++count == 2)
-		    console.log("TEST complete");
+		alert("test complete");
 	});
-	
+		
 	client1.model.addEventListener("change", function(){
 		console.log("Model CHANGE");
+		this.setQueryValue("/data/caption", user + ": " + i++);
     });
 
     client1.xmpp.addEventListener("datastatuschange", function(e){
-		console.log("DATA STATUS CHANGE");
-		client1.model.setQueryValue("/data/caption", testName + ": " + i++);
+		console.log("DATA STATUS CHANGE")
+			this.setQueryValue("/data/caption", user + ": " + i++);
     })
 }
 
+// test1: logging in (accepted)
 function test1() {
 	// Create bots
 	var bot1 = createBot("bot1", "bot1", 0);
@@ -138,9 +135,338 @@ function test1() {
 	var client1 = createClient("client1", "client1", 0);
 	
 	bot1.remote.addEventListener("rdbinit", function (e) {
-		//
-		console.log("");
+		console.log("Join request received from client " + e.annotator);
+		
+		e.model.load("<data><caption/></data>");
+		return true;
+	});
+	
+	client1.xmpp.addEventListener("datastatuschange", function (e) {
+		console.log("Join request accepted");
+		finishTest(true);
+	});
+	
+	client1.xmpp.addEventListener("joinerror", function (e) {
+		console.log("Join request denied");
+		finishTest(false);
+	});
+}
+
+// test2: logging in (denied)
+function test2() {
+	// Create bots
+	var bot1 = createBot("bot1", "bot1", 0);
+	
+	// Create clients
+	var client1 = createClient("client1", "client1", 0);
+	
+	bot1.remote.addEventListener("rdbinit", function (e) {
+		console.log("Join request received from client " + e.annotator);
+		
+		// e.model.load("<a><b>yin</b><c>yang</c></a>");
+		return false;
+	});
+	
+	client1.xmpp.addEventListener("datastatuschange", function (e) {
+		console.log("Join request accepted");
+		finishTest(false);
+	});
+	
+	client1.xmpp.addEventListener("joinerror", function (e) {
+		console.log("Join request denied");
+		finishTest(true);
 	})
 }
 
-test0();
+// test3: sending change messages (single client)
+function test3() {
+	// Create bots
+	var bot1 = createBot("bot1", "bot1", 0);
+	
+	// Create clients
+	var client1 = createClient("client1", "client1", 0);
+	
+	bot1.remote.addEventListener("rdbinit", function (e) {
+		console.log("Join request received from client " + e.annotator);
+		
+		e.model.load("<data><caption/></data>");
+	});
+	
+	client1.xmpp.addEventListener("datastatuschange", function (e) {
+		console.log("Join request accepted");
+		
+		client1.model.setQueryValue("/data/caption", "test");
+		setTimeout(function () {
+			console.log("Change message timed out");
+
+			finishTest(false);
+		}, 3000);
+	});
+	
+	client1.xmpp.addEventListener("joinerror", function (e) {
+		console.log("Join request denied");
+		
+		finishTest(false);
+	});
+	
+	var nChanges = 0;
+	
+	function handleChange(e) {
+		console.log("Change message received by user " + e.to + " for document " + e.session);
+	
+		clearTimeout();
+		if (++nChanges == 2)
+			finishTest(true);
+	}
+	
+	bot1.xmpp.addEventListener("datachange", handleChange);
+	client1.xmpp.addEventListener("datachange", handleChange);
+}
+
+// test4: sending change messages (multiple clients)
+function test4() {
+	// Create bots
+	var bot1 = createBot("bot1", "bot1", 0);
+	
+	// Create clients
+	var client1 = createClient("client1", "client1", 0);
+	var client2 = createClient("client2", "client2", 0);
+	var client3 = createClient("client3", "client3", 0);
+	
+	bot1.remote.addEventListener("rdbinit", function (e) {
+		console.log("Join request received from client " + e.annotator);
+		
+		e.model.load("<data><caption/></data>");
+	});
+	
+	var nJoined = 0;
+	
+	function handleAccept(e) {
+		console.log("Join request accepted for user " + e.to);
+		
+		if (++nJoined == 3) {
+			client1.model.setQueryValue("/data/caption", "test");
+			setTimeout(function () {
+				console.log("Change message timed out");
+
+				finishTest(false);
+			}, 3000);
+		}
+	}
+	
+	function handleDenied(e) {
+		console.log("Join request denied");
+		finishTest(false);		
+	}	
+	
+	client1.xmpp.addEventListener("datastatuschange", handleAccept);
+	client2.xmpp.addEventListener("datastatuschange", handleAccept);
+	client3.xmpp.addEventListener("datastatuschange", handleAccept);
+	
+	client1.xmpp.addEventListener("joinerror", handleDenied);
+	client2.xmpp.addEventListener("joinerror", handleDenied);
+	client3.xmpp.addEventListener("joinerror", handleDenied);
+	
+	var nChanges = 0;
+	
+	function handleChange(e) {
+		console.log("Change message received by user " + e.to + " for document " + e.session);
+	
+		clearTimeout();
+		if (++nChanges == 4)
+			finishTest(true;)
+	}
+	
+	bot1.xmpp.addEventListener("datachange", handleChange);
+	client1.xmpp.addEventListener("datachange", handleChange);
+	client2.xmpp.addEventListener("datachange", handleChange);
+	client3.xmpp.addEventListener("datachange", handleChange);
+}
+
+// test5: sending change messages (multiple clients, multiple documents)
+function test5() {
+	// Create bots
+	var bot1 = createBot("bot1", "bot1", 0);
+	
+	// Create clients
+	var client1 = createClient("client1", "client1", 0);
+	var client2 = createClient("client2", "client2", 0);
+	var client3 = createClient("client3", "client3", 1);
+	var client4 = createClient("client4", "client4", 1);
+	
+	bot1.remote.addEventListener("rdbinit", function (e) {
+		console.log("Join request received from client " + e.annotator);
+		
+		e.model.load("<data><caption/></data>");
+	});
+	
+	var nJoined = 0;
+	
+	function handleAccept(e) {
+		console.log("Join request accepted for user " + e.to);
+		
+		if (++nJoined == 4) {
+			client1.model.setQueryValue("/data/caption", "test1");
+			client3.model.setQueryValue("/data/caption", "test2")
+			setTimeout(function () {
+				console.log("Change message timed out");
+
+				finishTest(false);
+			}, 3000);
+		}
+	}
+	
+	function handleDenied(e) {
+		console.log("Join request denied");
+		finishTest(false);		
+	}	
+	
+	client1.xmpp.addEventListener("datastatuschange", handleAccept);
+	client2.xmpp.addEventListener("datastatuschange", handleAccept);
+	client3.xmpp.addEventListener("datastatuschange", handleAccept);
+	client4.xmpp.addEventListener("datastatuschange", handleAccept);
+	
+	client1.xmpp.addEventListener("joinerror", handleDenied);
+	client2.xmpp.addEventListener("joinerror", handleDenied);
+	client3.xmpp.addEventListener("joinerror", handleDenied);
+	client4.xmpp.addEventListener("joinerror", handleDenied);
+	
+	var nChanges = 0;
+	
+	function handleChange(e) {
+		console.log("Change message received by user " + e.to + " for document " + e.session);
+	
+		clearTimeout();
+		if (++nChanges == 4)
+			finishTest(true;)
+	}
+	
+	bot1.xmpp.addEventListener("datachange", handleChange);
+	client1.xmpp.addEventListener("datachange", handleChange);
+	client2.xmpp.addEventListener("datachange", handleChange);
+	client3.xmpp.addEventListener("datachange", handleChange);
+	client4.xmpp.addEventListener("datachange", handleChange);
+}
+
+// test6: unregistering bots (document reinited)
+function test6() {
+	// Create bots
+	var bot1 = createBot("bot1", "bot1", 0);
+	var bot2 = createBot("bot2", "bot2", 1);
+	
+	// Create clients
+	var client1 = createClient("client1", "client1", 0);
+	
+	var currentBot = 0;
+	
+	bot1.remote.addEventListener("rdbinit", function (e) {
+		console.log("Join request received from client " + e.annotator);
+	
+		if (currentBot == 0)
+			currentBot = 1;
+		else
+			finishTest(false);
+		e.model.load("<data><caption/></data>");
+	});
+	
+	client1.xmpp.addEventListener("datastatuschange", function (e) {
+		console.log("Join request accepted");
+		client1.model.setQueryValue("/data/caption", "test");
+		setTimeout(function () {
+			console.log("Change message timed out");
+
+			finishTest(false);
+		}, 3000);
+	});
+	
+	client1.remote.addEventListener("joinerror", function (e) {
+		console.log("Join request denied");
+		finishTest(false);
+	});
+	
+	client1.xmpp.addEventListener("datachange", function (e) {
+		console.log("Change message received by user " + e.to + " for document " + e.session);
+				
+		clearTimeout();
+		bot1.disconnect();		
+	})
+	
+	bot2.remote.addEventListener("rdbinit", function (e) {
+		console.log("Join request received from client " + e.annotator);
+	
+		if (currentBot == 1) {	
+			client1.model.setQueryValue("/data/caption", "test");
+			setTimeout(function () {
+				console.log("Change message timed out");
+
+				finishTest(false);
+			}, 3000);
+		} else
+			finishTest(false);
+	});
+	
+	client1.xmpp.addEventListener("datachange", function (e) {
+		console.log("Change message received by user " + e.to + " for document " + e.session);
+				
+		clearTimeout();
+		finishTest(true);
+	})
+}
+
+// test7: unregistering bots (document destroyed)
+function test7() {
+	// Create bots
+	var bot1 = createBot("bot1", "bot1", 0);
+	
+	// Create clients
+	var client1 = createClient("client1", "client1", 0);
+	
+	bot1.remote.addEventListener("rdbinit", function (e) {
+		console.log("Join request received from client " + e.annotator);
+	
+		if (currentBot == 0)
+			currentBot = 1;
+		else
+			finishTest(false);
+		e.model.load("<data><caption/></data>");
+	});
+	
+	client1.xmpp.addEventListener("datastatuschange", function (e) {
+		console.log("Join request accepted");
+		client1.model.setQueryValue("/data/caption", "test");
+		setTimeout(function () {
+			console.log("Change message timed out");
+
+			finishTest(false);
+		}, 3000);
+		finishTest(true);
+	});
+	
+	client1.remote.addEventListener("joinerror", function (e) {
+		console.log("Join request denied");
+		finishTest(false);
+	});
+	
+	client1.xmpp.addEventListener("datachange", function (e) {
+		console.log("Change message received by user " + e.to + " for document " + e.session);
+				
+		clearTimeout();
+		bot1.disconnect();
+		setTimeout(function () {
+			console.log("Presence message timed out");
+
+			finishTest(false);
+		}, 3000);	
+	})
+	
+	// I am not sure what kind of event to listen for when the document has been destroyed
+	client1.xmpp.addEventListener("???", function (e) {
+		console.log("Presence message received by user " + e.to + " for document " + e.session);
+				
+		clearTimeout();
+		finishTest(true);
+	})	
+}
+
+// test8: bot priorities
+// test9: rpc calls
