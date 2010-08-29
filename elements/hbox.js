f8b9267bb4b96c3aac023570ff05a7b1411d41d1
Ruben Daniels <ruben@ajax.org>
2010-08-29 20:25:56 +0000

    * Small 'fix' for hbox
    git-svn-id: svn://svn.ajax.org/platform/source/trunk@5541 3c3c2983-eddd-4584-88e6-321645f70290

--- elements/hbox.js
+++ elements/hbox.js
@@ -392,8 +392,9 @@ apf.vbox = function(struct, tagName){
                 else {
                     if (apf.getStyle(amlNode.$ext, "display") == "inline")
                         amlNode.$ext.style.display = "block"; //@todo undo
-                    if (apf.getStyle(amlNode.$ext, "position") == "absolute")
-                        amlNode.$ext.style.position = "relative"; //@todo undo
+                    //This is nice for positioning elements in the context of an hbox/vbox
+                    //if (apf.getStyle(amlNode.$ext, "position") == "absolute")
+                        //amlNode.$ext.style.position = "relative"; //@todo undo
                 }
                 
                 amlNode.$ext.style[apf.CSSPREFIX + "BoxSizing"] = "border-box";
