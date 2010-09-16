45c2685ea33f7c0c0bf8f3b72cbf32496a78bc45
Ruben Daniels <ruben@ajax.org>
2010-09-16 14:09:01 +0200

    * Added local tree plugin

---
+++ client/ext/localtree/localtree.js
@@ -0,0 +1,58 @@
+/**
+ * Code Editor for the Ajax.org Cloud IDE
+ */
+require.def("ext/localtree/localtree",
+    ["core/ide", "core/ext", "text!ext/localtree/localtree.xml"],
+    function(ide, ext, markup) {
+
+        var plugin = {
+            name    : "Local Tree",
+            type    : ext.GENERAL,
+            markup  : markup,
+
+            init : function(){
+                plugin.trFiles = trFiles;
+                ide.vbMain.selectSingleNode("a:hbox[1]/a:vbox[1]").appendChild(trFiles);
+
+                trFiles.addEventListener("afterselect", this.$afterselect = function() {
+                    var node = this.selected;
+                    if (node.tagName != 'file')
+                        return;
+
+                    ext.openEditor(trFiles.value, trFiles.selected);
+
+                    /*if (node.selectSingleNode("data"))
+                        return;
+
+                    apf.getData('{davProject.read([@id])}', {
+                        xmlNode : node,
+                        callback: function(data) {
+                            var xml = apf.getXml(
+                                '<data><![CDATA[' + data + ']]></data>'
+                            );
+                            apf.b(node).append(xml);
+                        }
+                    });*/
+                });
+            },
+
+            enable : function(){
+                trFiles.show();
+            },
+
+            disable : function(){
+                trFiles.hide();
+            },
+
+            destroy : function(){
+                davProject.destroy(true, true);
+                mdlFiles.destroy(true, true);
+                trFiles.destroy(true, true);
+                
+                trFiles.removeEventListener("afterselect", this.$afterselect);
+            }
+        };
+
+        return plugin;
+    }
+);
