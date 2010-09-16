12523b4b59878f57c2749dce91c322d01d0929f8
Fabian Jakobs <fabian.jakobs@web.de>
2010-09-16 17:01:23 +0200

    first node runner implementation

--- client/ext/noderunner/noderunner.js
+++ client/ext/noderunner/noderunner.js
@@ -2,8 +2,12 @@
  * Node Runner Module for the Ajax.org Cloud IDE
  */
 require.def("ext/noderunner/noderunner",
-    ["core/ide", "core/ext", "text!ext/noderunner/noderunner.xml"],
-    function(ide, ext, markup) {
+    ["core/ide",
+     "core/ext",
+     "ext/tree/tree",
+     "text!ext/noderunner/noderunner.xml",
+     "/socket.io/socket.io.js"],
+    function(ide, ext, tree, markup) {
         return {
             type   : ext.GENERAL,
             markup : markup,
@@ -16,6 +20,64 @@ require.def("ext/noderunner/noderunner",
                     ide.barTools.appendChild(button);
                     this.nodes.push(button);
                 }
+
+                this.nodes[0].onclick = this.run.bind(this);
+                this.nodes[1].onclick = this.kill.bind(this);
+
+                var options = {
+                    transports: ['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling', 'jsonp-polling']
+                };
+                this.socket = new io.Socket(null, options);
+                this.socket.on("message", this.onMessage.bind(this));
+                this.socket.on("connect", this.onConnect.bind(this));
+                this.socket.connect();
+            },
+
+            onMessage : function(message) {
+                var message = JSON.parse(message);
+
+                console.log("MSG:", message);
+                switch(message.type) {
+                    case "node-exit":
+                        stRunning.deactivate();
+                        break;
+
+                    case "node-data":
+                        //ide.log(message.data);
+                        break;
+                }
+            },
+
+            onConnect : function() {
+                console.log("connected");
+                stConnected.activate();
+            },
+
+            run : function() {
+                if (stRunning.active || !stConnected.active)
+                    return;
+
+                var file = tree.trFiles.selected;
+                if (!file)
+                    return;
+
+                if (file.getAttribute("contenttype").indexOf("application/javascript") != 0)
+                    return;
+
+                console.log("running", file);
+                var command = {
+                    "command": "Run",
+                    "file": this.$getPath(file)
+                };
+                this.socket.send(JSON.stringify(command));
+                stRunning.activate();
+            },
+
+            kill : function() {
+                if (!stRunning.active)
+                    return
+
+                this.socket.send(JSON.stringify({"command": "kill"}));
             },
 
             enable : function(){
@@ -35,6 +97,17 @@ require.def("ext/noderunner/noderunner",
                     item.destroy(true, true);
                 });
                 this.nodes = [];
+            },
+
+            $getPath : function(fileEl) {
+                var path = [fileEl.getAttribute("name")];
+
+                while (fileEl.parentNode.tagName == "folder") {
+                    fileEl = fileEl.parentNode;
+                    path.push(fileEl.getAttribute("name"));
+                }
+
+                return path.reverse().join("/");
             }
         };
     }
