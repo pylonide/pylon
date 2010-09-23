a7c0f7b5befa7e2fe1055281fa329ee4b1ae910c
U-Arcbook7\Arc <Arc@Arcbook7.(none)>
2010-09-23 23:24:47 +0200

    easy start

--- cloud9-win32.bat
+++ cloud9-win32.bat
@@ -5,9 +5,18 @@ IF EXIST common\ace\LICENSE goto skip
 echo ----- Please wait, initializing submodules for first launch -----
 
 git submodule update --init --recursive
-	
+
+IF %ERRORLEVEL% NEQ 0 goto error
+
 echo ------ Submodules installed ----
 
 :skip
 PATH=%PATH%:common\nodebuilds\win32\node;
 common\nodebuilds\win32\node bin/cloud9.js -a"cmd /c start"
+IF %ERRORLEVEL% EQ 0 goto exit
+
+:error
+echo "Something failed unfortunately, please try a clean clone"
+pause
+
+:exit
