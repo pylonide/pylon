63de847cd306953419336a293719b2208972b560
rikarends <rik@ajax.org>
2010-10-04 19:20:24 +0200

    support

--- paths.js
+++ paths.js
@@ -1,6 +1,6 @@
 require.paths.unshift(__dirname);
 require.paths.unshift(__dirname + "/lib");
 
-require.paths.unshift(__dirname + "/vendor/apf");
-require.paths.unshift(__dirname + "/vendor/async.js/lib");
-require.paths.unshift(__dirname + "/vendor/node-o3-xml/lib");
+require.paths.unshift(__dirname + "/support/apf");
+require.paths.unshift(__dirname + "/support/async.js/lib");
+require.paths.unshift(__dirname + "/support/node-o3-xml/lib");
