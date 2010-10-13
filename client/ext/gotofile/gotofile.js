8a6d61696d66af073a3fb0e4343bb3aa0385545d
mikedeboer <info@mikedeboer.nl>
2010-10-13 16:46:45 +0200

    integrated gotofile plugin with jsDAV backend

--- client/ext/gotofile/gotofile.js
+++ client/ext/gotofile/gotofile.js
@@ -8,9 +8,10 @@ require.def("ext/gotofile/gotofile",
     ["core/ide", 
      "core/ext",
      "ext/filesystem/filesystem", 
-     "ext/settings/settings", 
+     "ext/settings/settings",
+     "ext/tree/tree",
      "text!ext/gotofile/gotofile.xml"],
-    function(ide, ext, fs, settings, markup) {
+    function(ide, ext, fs, settings, tree, markup) {
         
 return ext.register("ext/gotofile/gotofile", {
     name    : "Filter Tree",
@@ -41,9 +42,13 @@ return ext.register("ext/gotofile/gotofile", {
     init : function() {
         txtGoToFile.addEventListener("keydown", function(e){
             if (e.keyCode == 13){
-                mdlGoToFile.load("ext/gotofile/search.xml", {
-                    keyword: txtGoToFile.value
-                });
+                var node = trFiles.selected;
+                if (!node)
+                    trFiles.select(node = trFiles.xmlRoot.selectSingleNode("folder[1]"));
+                while (node.tagName != "folder")
+                    node = node.parentNode;
+                mdlGoToFile.load("{davProject.report('" + node.getAttribute("path")
+                    + "', 'filesearch', {query: '" + txtGoToFile.value + "'})}");
             }
             else if (e.keyCode == 40 && dgGoToFile.length) {
                 var first = dgGoToFile.getFirstTraverseNode();
@@ -60,15 +65,33 @@ return ext.register("ext/gotofile/gotofile", {
                 if (this.selected == this.getFirstTraverseNode())
                     txtGoToFile.focus();
             }
-            else if (e.keyCode == 27)
+            else if (e.keyCode == 27) {
                 _self.toggleDialog(-1);
-            else if (restricted.indexOf(e.keyCode) == -1)
+            }
+            else if (restricted.indexOf(e.keyCode) == -1) {
                 txtGoToFile.focus();
+            }
         }, true);
+
+        dgGoToFile.addEventListener("afterchoose", function(e) {
+            winGoToFile.hide();
+            var root = trFiles.selected;
+            while (root.tagName != "folder")
+                root = root.parentNode;
+            var path   = root.getAttribute("path") + apf.getTextNode(e.xmlNode).nodeValue,
+                chunks = path.split("/"),
+                node   = apf.n("<file />")
+                    .attr("name", chunks[chunks.length-1])
+                    .attr("contenttype", "application/javascript")
+                    .attr("path", path)
+                    .node();
+            ide.dispatchEvent("openfile", {node: node});
+        });
     },
     
     gotofile : function(){
         this.toggleDialog(true);
+        return false;
     },
     
     toggleDialog: function(forceShow) {
