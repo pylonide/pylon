11a64f652cfbb3315e56841d38659d2a200bfe47
Fabian Jakobs <fabian.jakobs@web.de>
2010-11-01 17:01:40 +0100

    debugger fixes

--- elements/debugger.js
+++ elements/debugger.js
@@ -74,27 +74,29 @@ apf.dbg = function(struct, tagName){
             dbgImpl.addEventListener("changeFrame", _self.$onChangeFrame.bind(_self));
             
             _self.$loadSources(function() {           
-	            dbgImpl.setBreakpoints(_self.$mdlBreakpoints, function() {                    
-                    var frame = _self.$mdlStack.queryNode("frame[1]");
-                    if (frame) {
-                        var scriptId = frame.getAttribute("scriptid");
-                        var scriptName = _self.$mdlSources.queryValue("file[@scriptid='" + scriptId + "']/@scriptname");
-                        
-                        if (scriptName) {
-                            var line = frame.getAttribute("line");
-                            var bp = _self.$mdlBreakpoints.queryNode("breakpoint[@script='" + scriptName + "' and @line='" + line + "']");
-                        }
-                        if (!scriptName || !bp) {
-                           _self.$debugger.continueScript();
-                        }
-                    }
+	            dbgImpl.setBreakpoints(_self.$mdlBreakpoints, function() {      
+	                _self.$debugger.backtrace(_self.$mdlStack, function() {              
+	                    var frame = _self.$mdlStack.queryNode("frame[1]");
+	                    if (frame) {
+	                        var scriptId = frame.getAttribute("scriptid");
+	                        var scriptName = _self.$mdlSources.queryValue("file[@scriptid='" + scriptId + "']/@scriptname");
+	                        
+	                        if (scriptName) {
+	                            var line = frame.getAttribute("line");
+	                            var bp = _self.$mdlBreakpoints.queryNode("breakpoint[@script='" + scriptName + "' and @line='" + line + "']");
+	                        }
+	                        if (!scriptName || !bp) {
+	                           _self.$debugger.continueScript();
+	                        }
+	                    }
+                    });
 	            });
             });
         });
     };
     
     this.$onChangeRunning = function() {
-        var isRunning = this.$debugger.isRunning();
+        var isRunning = this.$debugger && this.$debugger.isRunning();
         if (this.$stRunning.active && !isRunning)
             this.$onBreak();
         
@@ -106,6 +108,9 @@ apf.dbg = function(struct, tagName){
     
     this.$onBreak = function() {
         var _self = this;
+        if (!this.$debugger || this.$debugger.isRunning())
+            return;
+            
         this.$debugger.backtrace(this.$mdlStack, function() {
             _self.dispatchEvent("break");
         });
@@ -189,8 +194,8 @@ apf.dbg = function(struct, tagName){
     };
 
     this.continueScript = function(callback) {
-        this.dispatchEvent("beforecontinue");
-        this.$stRunning.setProperty("active", true);
+//        this.dispatchEvent("beforecontinue");
+//        this.$stRunning.setProperty("active", true);
         
         if (this.$debugger)
             this.$debugger.continueScript(callback);
@@ -199,22 +204,22 @@ apf.dbg = function(struct, tagName){
     };
 
     this.stepInto = function() {
-        this.dispatchEvent("beforecontinue");
-        this.$stRunning.setProperty("active", true);
+//        this.dispatchEvent("beforecontinue");
+//        this.$stRunning.setProperty("active", true);
         
         this.$debugger && this.$debugger.stepInto();
     };
 
     this.stepNext = function() {
-        this.dispatchEvent("beforecontinue");
-        this.$stRunning.setProperty("active", true);
+//        this.dispatchEvent("beforecontinue");
+//        this.$stRunning.setProperty("active", true);
         
         this.$debugger && this.$debugger.stepNext();
     };
 
     this.stepOut = function() {
-        this.dispatchEvent("beforecontinue");
-        this.$stRunning.setProperty("active", true);
+//        this.dispatchEvent("beforecontinue");
+//        this.$stRunning.setProperty("active", true);
         
         this.$debugger && this.$debugger.stepOut();
     };    
