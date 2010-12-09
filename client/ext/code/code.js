792a2077c6618b9649243bc3a1aeb20e6c66116c
Fabian Jakobs <fabian.jakobs@web.de>
2010-12-09 11:43:23 +0100

    add python support

--- client/ext/code/code.js
+++ client/ext/code/code.js
@@ -39,7 +39,8 @@ return ext.register("ext/code/code", {
         "text/plain",
         "application/x-httpd-php",
         "text/html",
-        "application/xhtml+xml"
+        "application/xhtml+xml",
+        "text/x-script.python"
     ],
     markup  : markup,
 
