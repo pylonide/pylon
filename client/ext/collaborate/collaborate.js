796580afce3e75b8a82b73fa3c4cd1fda5241cc3
Matt Pardee <matt.pardee@gmail.com>
2011-02-01 04:14:20 -0800

    Tabs background image fix

--- client/ext/collaborate/collaborate.js
+++ client/ext/collaborate/collaborate.js
@@ -27,7 +27,56 @@ return ext.register("ext/collaborate/collaborate", {
 
     openChat : function(listItem) {
 
-        tempChatWindow = new apf.panel({
+        tempChatWindow = new apf.modalwindow({
+            htmlNode   : document.body,
+            id         : "chatUser",
+            buttons    : "close",
+            title      : "New Chat",
+            visible    : true,
+            right      : "42",
+            width      : "250",
+            height     : "350",
+            skin       : "todowin",
+            childNodes : [
+                new apf.toolbar({
+                  childNodes : [
+                    new apf.bar({
+                      height     : 36,
+                      childNodes : [
+                        new apf.hbox({
+                          edge : 5
+                        })
+                      ]
+                    })
+                  ]
+                }),
+                new apf.panel({
+                    skin    : "todopanel",
+                    visible : "true",
+                    childNodes : [
+                        new apf.panel({
+                            skin    : "todopanel",
+                            'class' : "top_border"
+                        })
+                    ]
+                }),
+                new apf.toolbar({
+                    'class' : "top_border",
+                    childNodes : [
+                        new apf.bar({
+                            height : 27,
+                            childNodes : [
+                                new apf.button({
+                                    left    : 0,
+                                    icon    : "debugger/monitorexpression_tsk{this.disabled ? '_disabled' : ''}.gif"
+                                })
+                            ]
+                        })
+                    ]
+                })
+            ]
+        });
+        /*tempChatWindow = new apf.panel({
             htmlNode   : document.body,
             id         : "chatUser",
             buttons    : "close",
@@ -51,10 +100,31 @@ return ext.register("ext/collaborate/collaborate", {
                 }),
                 new apf.panel({
                     skin    : "todopanel",
-                    visible : "true"
+                    visible : "true",
+                    childNodes : [
+                        new apf.panel({
+                            skin    : "todopanel",
+                            'class' : "top_border"
+                        })
+                    ]
+                }),
+                new apf.toolbar({
+                    'class' : "top_border",
+                    childNodes : [
+                        new apf.bar({
+                            height : 27,
+                            childNodes : [
+                                new apf.button({
+                                    icon    : "debugger/monitorexpression_tsk{this.disabled ? '_disabled' : ''}.gif",
+                                    onclick : "require('c9/ext/todo/todo').expandNewTodo(this);",
+                                    left    : 0
+                                })
+                            ]
+                        })
+                    ]
                 })
             ]
-        });
+        });*/
                       
         //apf.document.body.insertMarkup('/static/ext/collaborate/chatwindow.xml');
         //dock.registerChatWindow();
