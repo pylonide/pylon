ea8b38725755d423d71c62277359efe3a9b481f3
Matt Pardee <matt.pardee@gmail.com>
2011-02-08 01:50:30 -0800

    Smart chat window and chat objects

--- client/ext/collaborate/collaborate.js
+++ client/ext/collaborate/collaborate.js
@@ -23,6 +23,9 @@ return ext.register("ext/collaborate/collaborate", {
     hotitems        : {},
     
     nodes : [],
+    
+    // Array of objects for holding user information
+    chatUsers: [],
 
     openChat : function(listItem) {
 
@@ -51,16 +54,7 @@ return ext.register("ext/collaborate/collaborate", {
             skin       : "todowin",
             childNodes : [
                 new apf.toolbar({
-                  childNodes : [
-                    new apf.bar({
-                      height     : 36,
-                      childNodes : [
-                        new apf.hbox({
-                          edge : 5
-                        })
-                      ]
-                    })
-                  ]
+                    height  : "5"
                 }),
                 new apf.panel({
                     skin    : "todopanel",
@@ -68,7 +62,7 @@ return ext.register("ext/collaborate/collaborate", {
                     flex    : "1",
                     childNodes : [
                         new apf.hbox({
-                            height     : "243",
+                            height     : "273",
                             edge       : "3 0 5 7",
                             childNodes : [
                                 new apf.text({
@@ -108,6 +102,29 @@ return ext.register("ext/collaborate/collaborate", {
             ]
         });
 
+        // Add new chat to our chatUsers object
+        this.chatUsers[chatUserID] = {
+            win           : eval("chatUser" + chatUserID),
+            textOutput    : tempChatWindow.selectSingleNode("panel/hbox[1]/text"),
+            textInput     : tempChatWindow.selectSingleNode("panel/hbox[2]/textarea"),
+            sbChat        : tempChatWindow.selectSingleNode("panel/hbox[1]/scrollbar"),
+            
+            sbPos         : 0,
+
+            messages      : [],
+            msgFirstDate  : -1,
+            msgLastFromMe : false
+        };
+        
+        var _self = this;
+
+        this.chatUsers[chatUserID].textOutput.setValue(
+                '<div class="chat_history_overview">\
+                <img src="/static/style/images/chat_history.png" />\
+                1 Day - 1 Week - 2 Weeks - 1 Month\
+               - 3 Months - 6 Months - 1 year - All</div>'
+        );
+
         dock.registerWindow(tempChatWindow, {
                 dockPosition: "bottom",
                 backgroundImage: "/static/style/images/collaboration_panel_sprite.png",
@@ -119,11 +136,7 @@ return ext.register("ext/collaborate/collaborate", {
             true    // Force this chat window to show right away
         );
         
-        cta = eval("chatUser" + chatUserID).selectSingleNode("panel/hbox[2]/textarea");
-        
-        var _self = this;
-
-        cta.onkeydown = function(event) {
+        this.chatUsers[chatUserID].textInput.onkeydown = function(event) {
             if(event.keyCode == 13) {
                 event.preventDefault();
 
@@ -132,17 +145,16 @@ return ext.register("ext/collaborate/collaborate", {
                     _self.appendToChatWin(chatUserID, 'chatmessage',
                                             {
                                                 message: chatInput,
-                                                fromMe: true
+                                                fromUserID: -1
                                             }
                                         );
-                    eval("sbChat" + chatUserID).setPosition(1);
+                    _self.chatUsers[chatUserID].sbChat.setPosition(1);
                     
                     // Reset heights of elements if they have been modified
                     if(this.getHeight() > 24) {
                         this.setHeight(24);
-                        tcu = eval("chatUser" + chatUserID);
-                        tcu.selectSingleNode("panel/hbox[1]").setHeight(243);
-                        tcu.selectSingleNode("panel/hbox[2]").setHeight(41);
+                        _self.chatUsers[chatUserID].win.selectSingleNode("panel/hbox[1]").setHeight(273);
+                        _self.chatUsers[chatUserID].win.selectSingleNode("panel/hbox[2]").setHeight(41);
                     }
                 }
                 
@@ -151,23 +163,26 @@ return ext.register("ext/collaborate/collaborate", {
             }
         };
         
-        cta.onkeyup = function(event) {
+        this.chatUsers[chatUserID].textInput.onkeyup = function(event) {
+            
             // For every keypress besides [enter] and [tab], we will process
-            // the height of the 
-            if(event.keyCode != 9){
+            // the height of the input area to see if we should expand its 
+            // height, to a limit
+            
+            if(event.keyCode != 9 && event.keyCode != 13){
                 if(event.htmlEvent) {
                     if(event.htmlEvent.target.clientHeight < 
                             event.htmlEvent.target.scrollHeight) {
                         if(this.getHeight() < 65) {
                             this.setHeight(this.getHeight() + 15);
-                            tcu = eval("chatUser" + chatUserID);
-                            tcu.selectSingleNode("panel/hbox[1]").setHeight(
-                                tcu.selectSingleNode("panel/hbox[1]").getHeight()
+                            
+                            _self.chatUsers[chatUserID].win.selectSingleNode("panel/hbox[1]").setHeight(
+                                _self.chatUsers[chatUserID].win.selectSingleNode("panel/hbox[1]").getHeight()
                                     - 15
                             );
                             
-                            tcu.selectSingleNode("panel/hbox[2]").setHeight(
-                                tcu.selectSingleNode("panel/hbox[2]").getHeight()
+                            _self.chatUsers[chatUserID].win.selectSingleNode("panel/hbox[2]").setHeight(
+                                _self.chatUsers[chatUserID].win.selectSingleNode("panel/hbox[2]").getHeight()
                                     + 15
                             );
                         }
@@ -176,7 +191,7 @@ return ext.register("ext/collaborate/collaborate", {
             }
         };
         
-        cta.focus();
+        this.chatUsers[chatUserID].textInput.focus();
     },
     
     closeChat: function(ev, anchorEl){
@@ -189,42 +204,94 @@ return ext.register("ext/collaborate/collaborate", {
      */
     onChatHide: function(userID){
         if(apf.isWebkit) {
-            tUserSB = eval("sbChat" + userID);
-            // Store this somewhere...
-            collaborators.queryNode("item[@user_id=" 
-                    + userID + "]").setAttribute("sb_pos", tUserSB.getPosition());
+            this.chatUsers[userID].sbPos = 
+                this.chatUsers[userID].sbChat.getPosition();
         }
     },
     
     onChatShow: function(userID){
         if(apf.isWebkit) {
-            sb_pos = collaborators.queryNode("item[@user_id=" + userID
-                        + "]").getAttribute("sb_pos");
-            eval("sbChat" + userID).setPosition(sb_pos);
+            this.chatUsers[userID].sbChat.setPosition(
+                this.chatUsers[userID].sbPos
+            );
         }
         
-        eval("chatUser" + userID).selectSingleNode("panel/hbox[2]/textarea").focus();
+        this.chatUsers[userID].textInput.focus();
     },
 
     appendToChatWin: function(userID, type, messageDetails){
-        txtOutputWin = eval("txtChat" + userID);
-        
+
+        messageDetails.timestamp = new Date();
+
         switch(type){
             case 'chatmessage':
-                var outStr = '<div class="chat_message_block">';
-                if(messageDetails.fromMe == true) {
-                    outStr += '<div class="chat_me_header">\
-                                    <span class="chat_msg_name">You</span>\
-                                    <span class="chat_msg_timestamp">12:01 AM</span></div>\
-                                  <div class="chat_message">'
-                                  + messageDetails.message + '</div>'
+                var outStr = '<div class="chat_message_block';
+                                
+                // Compare this message with the last message;
+                // If there is more than 5 minutes difference and the message
+                // was from the same user, then place a timestamp break
+                
+                var bTimeStampOut = true;
+                var bOutputUser = true;
+                
+                var numMessages = this.chatUsers[userID].messages.length;
+                
+                if(numMessages > 0) {
+                    if(messageDetails.fromUserID == 
+                            this.chatUsers[userID].messages[numMessages - 1].fromUserID) {
+
+                        tsDiff = messageDetails.timestamp - 
+                                    this.chatUsers[userID].messages[numMessages-1].timestamp;
+                        if(tsDiff > 300000) {
+                            
+                            outStr += '"><div class="chat_header">';
+                            
+                            // Last message was over 5 minutes ago, append
+                            // timestamp divider
+                            outStr += '<div class="timestamp_divider"></div>\
+                                 <span class="chat_msg_timestamp cmt_divider">'
+                                 + '12:02' + '</span>';
+                            bTimeStampOut = false;
+                        }
+                        
+                        else {
+                            outStr += ' same_user"><div class="chat_header">';
+                        }
+                        
+                        bOutputUser = false;
+                    }
                 }
                 
-                outStr += '</div>';
+                // Message not from same user, output name
+                if(bOutputUser == true) {
+                    outStr += '"><div class="chat_header">';
+                    outStr += '<span class="chat_msg_name"';
+                    
+                    if(messageDetails.fromUserID == -1) {
+                        outStr += ' style="color: #ababab">You';
+                    }
+
+                    // Look up name from collaborator list
+                    else {
+                        
+                    }
+                    
+                    outStr += '</span>';
+                    
+                    if(bTimeStampOut) {
+                        outStr += '</span><span class="chat_msg_timestamp">'
+                                   +  '12:01 AM</span>';
+                    }
+                }
+
+                outStr += '</div><div class="chat_message">'
+                              + messageDetails.message + '</div></div>';
                 
-                txtOutputWin.addValue(outStr);
+                this.chatUsers[userID].textOutput.addValue(outStr);
                 break;
         }
+        
+        this.chatUsers[userID].messages.push(messageDetails);
     },
 
     init : function(amlNode){
