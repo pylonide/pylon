63cb105d292347a4f520dd2c5dc89bf5b8d15b13
Fabian Jakobs <fabian.jakobs@web.de>
2011-02-17 20:59:38 +0100

    use commonJS style and add close handler for the alert window

--- client/core/util.js
+++ client/core/util.js
@@ -4,33 +4,41 @@
  * @copyright 2010, Ajax.org B.V.
  * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
  */
-require.def("core/util",
-    ["core/ide"],
-    function(ide) {
-        return {
-            alert : function(title, header, msg) {
-                winAlert.show();
-                winAlert.setAttribute('title', title);
-                winAlertHeader.$ext.innerHTML = header;
-                winAlertMsg.$ext.innerHTML = msg;
-            },
-            confirm : function(title, header, msg, onconfirm) {
-                winConfirm.show();   
-                winConfirm.setAttribute("title", title);
-                winConfirmHeader.$ext.innerHTML = header;
-                winConfirmMsg.$ext.innerHTML = msg;
-                btnConfirmOk.onclick = onconfirm;
-            },
-            question : function(title, header, msg, onyes, onyestoall, onno, onnotoall) {
-                winQuestion.show();   
-                winQuestion.setAttribute("title", title);
-                winQuestionHeader.$ext.innerHTML = header;
-                winQuestionMsg.$ext.innerHTML = msg;
-                btnQuestionYes.onclick = onyes;
-                btnQuestionYesToAll.onclick = onyestoall;
-                btnQuestionNo.onclick = onno;
-                btnQuestionNoToAll.onclick = onnotoall;
-            }
+define(function(require, exports, module) {
+    
+var ide = require("core/ide");
+
+exports.alert = function(title, header, msg, onhide) {
+    winAlert.show();
+    winAlert.setAttribute('title', title);
+    winAlertHeader.$ext.innerHTML = header;
+    winAlertMsg.$ext.innerHTML = msg;
+    if (onhide)
+        winAlert.onhide = function() {
+            winAlertMsg.onhide = null;
+            onhide();
         };
-    }
-);
+    else
+        winAlert.onhide = null;
+};
+
+exports.confirm = function(title, header, msg, onconfirm) {
+    winConfirm.show();   
+    winConfirm.setAttribute("title", title);
+    winConfirmHeader.$ext.innerHTML = header;
+    winConfirmMsg.$ext.innerHTML = msg;
+    btnConfirmOk.onclick = onconfirm;
+};
+
+exports.question = function(title, header, msg, onyes, onyestoall, onno, onnotoall) {
+    winQuestion.show();   
+    winQuestion.setAttribute("title", title);
+    winQuestionHeader.$ext.innerHTML = header;
+    winQuestionMsg.$ext.innerHTML = msg;
+    btnQuestionYes.onclick = onyes;
+    btnQuestionYesToAll.onclick = onyestoall;
+    btnQuestionNo.onclick = onno;
+    btnQuestionNoToAll.onclick = onnotoall;
+};
+
+});
