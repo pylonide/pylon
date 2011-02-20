5738b929e3a3ffa73b37d7a402cc6c2b7ffdd7eb
Ruben <ruben@ajax.org>
2011-02-21 00:35:37 +0100

    finally fixed 'cd' command

--- client/ext/console/console.js
+++ client/ext/console/console.js
@@ -448,7 +448,7 @@ return ext.register("ext/console/console", {
             case "cd":
                 res = message.body;
                 if (res.cwd) {
-                    this.$cwd = res.cwd;
+                    this.$cwd = res.cwd.replace(ide.workspaceDir, "/workspace");
                     this.write("Working directory changed.");
                 }
                 break;
