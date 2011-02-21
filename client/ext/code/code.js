d5d04d74a2ca13e2e502f21be8667fc824bcad31
Fabian Jakobs <fabian.jakobs@web.de>
2011-02-21 07:58:15 +0100

    wait for settings load event

--- client/ext/code/code.js
+++ client/ext/code/code.js
@@ -117,20 +117,22 @@ return ext.register("ext/code/code", {
     init : function(amlPage) {
         var def = ceEditor.getDefaults();
         
-        //check default settings...
-        var settings = require("ext/settings/settings"),
-            model    = settings.model,
-            base     = model.data.selectSingleNode("editors/code");
-        if (!base)
-            base = model.appendXml('<code name="' + this.name +'" page="' + settings.getSectionId(this.name) + '" />', "editors");
+        ide.addEventListener("loadsettings", function() {
+            //check default settings...
+            var settings = require("ext/settings/settings"),
+                model    = settings.model,
+                base     = model.data.selectSingleNode("editors/code");
+            if (!base)
+                base = model.appendXml('<code name="' + this.name +'" page="' + settings.getSectionId(this.name) + '" />', "editors");
         
-        // go through all default settings and append them to the XML if they're not there yet
-        for (var prop in def) {
-            if (!prop) continue;
-            if (!base.getAttribute(prop))
-                base.setAttribute(prop, def[prop]);
-        }
-        apf.xmldb.applyChanges("synchronize", base);
+            // go through all default settings and append them to the XML if they're not there yet
+            for (var prop in def) {
+                if (!prop) continue;
+                if (!base.getAttribute(prop))
+                    base.setAttribute(prop, def[prop]);
+            }
+            apf.xmldb.applyChanges("synchronize", base);
+        });
 
         amlPage.appendChild(ceEditor);
         ceEditor.show();
