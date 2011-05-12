44a89a45c9dc417f780cf8d9e9225060fa80f70f
mikedeboer <info@mikedeboer.nl>
2011-05-12 17:15:06 +0200

    added support for node-o3-xml-v4

--- paths.js
+++ paths.js
@@ -4,3 +4,4 @@ require.paths.unshift(__dirname + "/lib");
 require.paths.unshift(__dirname + "/support/apf");
 require.paths.unshift(__dirname + "/support/async.js/lib");
 require.paths.unshift(__dirname + "/support/node-o3-xml/lib");
+require.paths.unshift(__dirname + "/support/node-o3-xml-v4/lib");
