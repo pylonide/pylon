0141a01486a94803e9b85336a26ee1f5187d8f77
mikedeboer <info@mikedeboer.nl>
2011-07-08 13:33:37 +0200

    fixed console output - removing tty escape sequences

--- client/ext/console/console.js
+++ client/ext/console/console.js
@@ -121,6 +121,8 @@ return ext.register("ext/console/console", {
                 .replace(wsrRe, "<a href='javascript:void(0)' onclick='require(\"ext/console/console\").jump(\"" + davPrefix + "/$1\", \"$2\", \"$3\")'>$1$2$3</a>")
                 .replace(wsRe, "<a href='javascript:void(0)' onclick='require(\"ext/console/console\").jump(\"" + davPrefix + "/$1\", \"$2\", \"$3\")'>"+workspaceDir+"/$1$2$3</a>")
                 .replace(/(((http:\/\/)|(www\.))[\w\d\.-]*(:\d+)?(\/[\w\d]+)?)/, "<a href='$1' target='_blank'>$1</a>")
+                // tty escape sequences (http://ascii-table.com/ansi-escape-sequences.php)
+                .replace(/\\u00(07|1b)\[(K|2J)/g, "")
                 .replace(/\033\[(?:(\d+);)?(\d+)m/g, function(m, extra, color) {
                     style = "color:" + (colors[color] || "#eee");
                     if (extra == 1) {
