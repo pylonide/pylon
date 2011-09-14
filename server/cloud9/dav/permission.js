1817f90444835a731d5f9e7d776521163f437cd2
Luis Merino <mail@luismerino.name>
2011-09-14 18:39:24 +0200

    [BACKLOG-1589] User 'visitor' permissions are wrong; users should not be allowed to READ/WRITE in private workspaces

--- server/cloud9/dav/permission.js
+++ server/cloud9/dav/permission.js
@@ -6,26 +6,47 @@ module.exports = Permission = function(handler) {
     jsDAV_ServerPlugin.call(this, handler);
     
     this.handler = handler;
-    
-    handler.addEventListener("beforeBind", this.checkPermission.bind(this));
-    handler.addEventListener("beforeWriteContent", this.checkPermission.bind(this));
-    handler.addEventListener("beforeUnbind", this.checkPermission.bind(this));
-    handler.addEventListener("beforeCreateFile", this.checkPermission.bind(this));
+    handler.addEventListener("beforeMethod", this.checkPermission.bind(this));
 };
 
 sys.inherits(Permission, jsDAV_ServerPlugin);
 
 (function() {
 
-    this.checkPermission = function(e) {
+    this.READ_METHODS = {
+        "OPTIONS":1,
+        "GET":1,
+        "HEAD":1,
+        "PROPFIND":1,
+        "REPORT":1
+    };
+    
+    this.WRITE_METHODS = {
+        "DELETE":1,
+        "MKCOL":1,
+        "PUT":1,
+        "PROPPATCH":1,
+        "COPY":1,
+        "MOVE":1,
+        "LOCK":1,
+        "UNLOCK":1
+    };
+
+    this.checkPermission = function(e, method) {
         var permissions = this.handler.server.permissions;
-        if (!permissions || permissions.dav !== "rw") {
-            this.handler.httpResponse.writeHead(403);
-            this.handler.httpResponse.end("operation not permitted!");
-            e.stop();
-        } else {
-            e.next();
-        }
+        
+        if (!permissions)
+            return e.next();
+            
+        if (this.READ_METHODS[method] && permissions.dav.indexOf("r") > -1)
+            return e.next();
+            
+        if (this.WRITE_METHODS[method] && permissions.dav.indexOf("w") > -1)
+            return e.next();
+            
+        this.handler.httpResponse.writeHead(403);
+        this.handler.httpResponse.end("operation not permitted!");
+        e.stop();
     };
 
 }).call(Permission.prototype);
