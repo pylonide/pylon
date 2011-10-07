905d7a9efd261ad28a888c7819ea9d6a210e8daf
Sergi Mansilla <sergi.mansilla@gmail.com>
2011-10-07 17:33:24 +0200

    [BACKLOG-1772] Several fixes now spaces are stripped from every command.

--- client/ext/stripws/stripws.js
+++ client/ext/stripws/stripws.js
@@ -19,15 +19,21 @@ var Range = require("ace/range").Range;
 var settings = require("text!ext/stripws/settings.xml");
 var extSettings = require("ext/settings/settings");
 
-var strip = function (editor) {
+var RE_WS = /[ \t\r\f\v]+\n/g;
+// Attaching to exports.module for testing purposes
+var strip = module.exports.strip = function () {
+    if (!editors.currentEditor.ceEditor)
+        return;
+
     var editor = editors.currentEditor.ceEditor.$editor;
     var session = editor.getSession()
     var source = session.getValue();
     var pos = editor.getCursorPosition();
 
-    var result = source.replace(/[ \t\r\f\v]+\n/g, "\n");
+    var result = source.replace(RE_WS, "\n");
     session.setValue(result);
     var lineLength = session.getLine(pos.row).length;
+
     editor.moveCursorTo(pos.row, pos.column >= lineLength ? lineLength : pos.column);
 
     return result;
@@ -41,7 +47,7 @@ module.exports = ext.register("ext/stripws/stripws", {
 
     commands: {
         "stripws": {
-            hint: "strip whitespace at the end of the lines"
+            hint: "strip whitespace at the end of each line"
         }
     },
 
@@ -62,23 +68,23 @@ module.exports = ext.register("ext/stripws/stripws", {
         );
 
         ide.addEventListener("beforefilesave", function(data) {
-            console.log("SETTINGS", extSettings.model)
-            if (extSettings.model.data.
-                selectSingleNode("editors/code/@stripws").
-                firstChild.nodeValue == "true")
-            {
-                data.doc.setValue(strip());
-            }
-        });
+            var node =
+                extSettings.model.data.selectSingleNode("editors/code/@stripws");
 
-        canon.addCommand({
-            name: "stripws",
-            exec: function() { strip(); }
+            // If the 'Strip whitespace on save' option is enabled, we strip
+            // whitespaces from the node value just before the file is saved.
+            if (node && node.firstChild && node.firstChild.nodeValue == "true") {
+                strip();
+            }
         });
 
         ide.addEventListener("init.ext/settings/settings", function (e) {
             barSettings.insertMarkup(settings);
         });
+    },
+
+    stripws: function() {
+        strip();
     },
 
     enable: function () {
