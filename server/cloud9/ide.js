09f2ada631c07e1ed1925916c959f6903215f93d
Zef Hemel <zef@zef.me>
2011-10-12 14:21:22 +0200

    Code completion plug-in

--- server/cloud9/ide.js
+++ server/cloud9/ide.js
@@ -123,7 +123,8 @@ Ide.DEFAULT_PLUGINS = [
     "ext/dragdrop/dragdrop",
     "ext/beautify/beautify",
     "ext/offline/offline",
-    "ext/zen/zen"
+    "ext/zen/zen",
+    "ext/codecomplete/codecomplete"
     //"ext/acebugs/acebugs"
 ];
 
