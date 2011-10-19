f2bb59777c0c1fa9c82e8307094e3595e2c9db0b
Cloud9 <cloud9@fhemuwac.joyent.us>
2011-10-19 16:05:26 +0000

    Revert "[BACKLOG-1709] quick search should toggle between visible and invisible with CMD+F"
    This reverts commit e2fdf7ba1d4037dc943ad2fb40daad8e7b9c428e.

--- client/ext/quicksearch/quicksearch.js
+++ client/ext/quicksearch/quicksearch.js
@@ -193,7 +193,7 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
     },
 
     quicksearch : function(){
-        this.toggleDialog(-1);
+        this.toggleDialog(1);
     },
 
     execSearch: function(close, backwards) {
@@ -244,7 +244,7 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
     },
     
     find: function() {
-        this.toggleDialog(-1);
+        this.toggleDialog(1);
         return false;
     },
     
