49ed8142a0bee68d62362d4d0884c9b67d72d7e2
Cloud9 <cloud9@fhemuwac.joyent.us>
2011-10-19 16:05:14 +0000

    Revert "[BACKLOG-1709] quick search should toggle between visible and invisible with CMD+F"
    This reverts commit fdca3636b24c8d2847e79bbe15838fea5956cdcf.

--- client/ext/quicksearch/quicksearch.js
+++ client/ext/quicksearch/quicksearch.js
@@ -43,7 +43,7 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
         canon.addCommand({
             name: "find",
             exec: function(env, args, request) {
-                _self.toggleDialog(-1);
+                _self.toggleDialog(1);
             }
         });
     },
@@ -60,9 +60,9 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
                         _self.execSearch(false, false);
                     return false;
                 case 27: //ESCAPE
-                    _self.toggleDialog(1);
+                    _self.toggleDialog(-1);
                     if (e.htmlEvent)
-                        apf.stopEvent(e.htmlEvent);
+                        apf.stopEvent(e.htmlEvent)
                     else if (e.stop)
                         e.stop();
                     return false;
@@ -83,7 +83,7 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
             }
         });
         
-        winQuickSearch.addEventListener("blur", function(e) {
+        winQuickSearch.addEventListener("blur", function(e){
             if (!apf.isChildOf(winQuickSearch, e.toElement))
                 _self.toggleDialog(-1);
         });
@@ -97,7 +97,7 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
             editor.ceEditor.parentNode.appendChild(winQuickSearch);
     },
     
-    navigateList : function(type) {
+    navigateList : function(type){
         var settings = require("ext/settings/settings");
         if (!settings) return;
         
@@ -123,26 +123,23 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
     
     handleQuicksearchEscape : function(e) {
         if (e.keyCode == 27)
-            this.toggleDialog(1);
+            this.toggleDialog(-1);
     },
 
     toggleDialog: function(force) {
         ext.initExtension(this);
 
-        /*if (this.control && this.control.stop)
+        if (this.control && this.control.stop)
             this.control.stop();
-        
-        if (this.control && this.control.state == apf.tween.RUNNING)
-            return;
-*/
+
         var editorPage = tabEditors.getPage();
         if (!editorPage) return;
 
         var editor = editors.currentEditor;
         if (!editor || !editor.ceEditor)
             return;
-        
-        if (!winQuickSearch.visible) {
+
+        if (!force && !winQuickSearch.visible || force > 0) {
             this.position = 0;
             
             var sel   = editor.getSelection();
@@ -158,7 +155,6 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
 
             winQuickSearch.$ext.style.top = "-30px";
             winQuickSearch.show();
-            
             txtQuickSearch.focus();
             txtQuickSearch.select();
             
@@ -177,22 +173,20 @@ module.exports = ext.register("ext/quicksearch/quicksearch", {
             txtQuickSearch.focus();
             txtQuickSearch.select();
             
-            if (force > 0) {
-                //Animate
-                apf.tween.single(winQuickSearch, {
-                    type     : "top",
-                    anim     : apf.tween.NORMAL,
-                    from     : winQuickSearch.$ext.offsetTop,
-                    to       : -30,
-                    steps    : 8,
-                    interval : 10,
-                    control  : (this.control = {}),
-                    onfinish : function() {
-                        winQuickSearch.hide();
-                        editor.ceEditor.focus();
-                    }
-                });
-            }
+            //Animate
+            apf.tween.single(winQuickSearch, {
+                type     : "top",
+                anim     : apf.tween.NORMAL,
+                from     : winQuickSearch.$ext.offsetTop,
+                to       : -30,
+                steps    : 8,
+                interval : 10,
+                control  : (this.control = {}),
+                onfinish : function(){
+                    winQuickSearch.hide();
+                    editor.ceEditor.focus();
+                }
+            });
         }
 
         return false;
