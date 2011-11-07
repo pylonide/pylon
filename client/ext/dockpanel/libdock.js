c02882461ad5343906f7a7698308b1be777f0795
Garen Torikian <gjtorikian@gmail.com>
2011-11-07 15:25:08 -0800

    Adding tooltips to debugger/dock buttons on right

--- client/ext/dockpanel/libdock.js
+++ client/ext/dockpanel/libdock.js
@@ -1377,6 +1377,9 @@ var DockableLayout = module.exports = function(parentHBox, cbFindPage, cbStorePa
         
         if (options && (tmp = options.primary)) {
             var span = button.$ext.getElementsByTagName("span");
+            
+            span[2].title = options.menu.split("/").pop();
+            
             span[2].style.background = 'url("' 
                 + tmp.backgroundImage + '") '
                 + tmp.defaultState.x + 'px '
