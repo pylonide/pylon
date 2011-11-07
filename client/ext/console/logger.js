1d3070538230b917c37f86200977189307373d02
Jan Jongboom <janjongboom@gmail.com>
2011-11-07 11:28:01 +0100

    Revert "better URL regex"
    This reverts commit 3e6af1b55dd9425e1cb63cec74e6d78b5a49b0bd.

--- client/ext/console/logger.js
+++ client/ext/console/logger.js
@@ -32,8 +32,6 @@ exports.logNodeStream = function(data, stream, useOutput) {
     var wsRe = new RegExp(Lang.escapeRegExp(workspaceDir) + "\\/([^:]*)(:\\d+)(:\\d+)*", "g");
     // relative workspace files
     var wsrRe = /(?:\s|^|\.\/)([\w\_\$-]+(?:\/[\w\_\$-]+)+(?:\.[\w\_\$]+))?(\:\d+)(\:\d+)*/g;
-    // url
-    var urlRe = /\b((?:[a-z][\w\-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()[\]{};:'".,<>?«»“”‘’]))/i;
 
     for (var i = 0, l = lines.length; i < l; i++) {
         if (!lines[i])
@@ -44,7 +42,7 @@ exports.logNodeStream = function(data, stream, useOutput) {
             .replace(/\s{2,}/g, function(str) {
                 return Lang.stringRepeat("&nbsp;", str.length);
             })
-            .replace(urlRe, "<a href='$1' target='_blank'>$1</a>")
+            .replace(/(((http:\/\/)|(www\.))[\w\d\.-]*(:\d+)?(\/[\w\d]+)?)/, "<a href='$1' target='_blank'>$1</a>")
             // tty escape sequences (http://ascii-table.com/ansi-escape-sequences.php)
             .replace(/(\u0007|\u001b)\[(K|2J)/g, "")
             .replace(/\033\[(?:(\d+);)?(\d+)m/g, function(m, extra, color) {
