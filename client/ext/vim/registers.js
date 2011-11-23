bfa44dded4d38d7b554b75bcf54c5feba191bdcb
Sergi Mansilla <sergi.mansilla@gmail.com>
2011-11-23 16:30:09 +0100

    Forgot to add registers.js

---
+++ client/ext/vim/registers.js
@@ -0,0 +1,17 @@
+"use strict";
+
+define(function(require, exports, module) {
+
+var util = require("ext/vim/maps/util");
+var motions = require("ext/vim/maps/motions");
+var operators = require("ext/vim/maps/operators");
+var alias = require("ext/vim/maps/aliases");
+
+var Registers = module.exports = {
+    _default: {
+        text: "",
+        isLine: false
+    }
+};
+
+});
