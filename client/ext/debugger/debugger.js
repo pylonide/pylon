a65e00acb6607267fda1b09c37007e34f1a5b643
Jan Jongboom <janjongboom@gmail.com>
2011-11-23 10:33:17 +0100

    Breakpoint window unbinds from model when clearing one breakpoint

--- client/ext/debugger/debugger.js
+++ client/ext/debugger/debugger.js
@@ -54,9 +54,9 @@ module.exports = ext.register("ext/debugger/debugger", {
         
         ide.addEventListener("loadsettings", function (e) {
             // restore the breakpoints from the IDE settings
-            var bpFromIde = e.model.data.selectSingleNode("//breakpoints");
+            var bpFromIde = e.model.data.selectSingleNode("//bp");
             if (bpFromIde) {
-                mdlDbgBreakpoints.load(bpFromIde);
+                mdlDbgBreakpoints.load(apf.xmldb.getCleanCopy(bpFromIde).xml);
             }
         });
         
@@ -215,20 +215,21 @@ module.exports = ext.register("ext/debugger/debugger", {
             // get the current IDE settings
             var settingsMdl = settings.model.data;
             // create a new element
-            var node = settingsMdl.ownerDocument.createElement("breakpoints");
+            var node = settingsMdl.ownerDocument.createElement("bp");
             
             // find out all the breakpoints in the breakpoint model and iterate over them
             var breakpoints = e.currentTarget.data.selectNodes("//breakpoint");
             for (var ix = 0; ix < breakpoints.length; ix++) {
                 // clone and add to our new element
-                var cln = breakpoints[ix].cloneNode();
+                var cln = apf.xmldb.getCleanCopy(breakpoints[ix]);
                 node.appendChild(cln);
             }
             
             // if there is already a breakpoints section in the IDE settings remove it
-            var bpInSettingsFile = settingsMdl.selectSingleNode("//breakpoints");
-            if (bpInSettingsFile) {
-                bpInSettingsFile.parentNode.removeChild(bpInSettingsFile);
+            var bpInSettingsFile = settingsMdl.selectNodes("//bp");
+            if (bpInSettingsFile.length) {
+                for (ix = 0; ix < bpInSettingsFile.length; ++ix)
+                    bpInSettingsFile[ix].parentNode.removeChild(bpInSettingsFile[ix]);
             }
             
             // then append the current breakpoints to the IDE settings, tah dah
