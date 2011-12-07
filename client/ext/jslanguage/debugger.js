850e9843daa952691e282d99e5fd4c9c03c8a8a3
Zef Hemel <zef@zef.me>
2011-12-07 15:51:43 +0100

    Comments!

--- client/ext/jslanguage/debugger.js
+++ client/ext/jslanguage/debugger.js
@@ -34,37 +34,47 @@ define(function(require, exports, module) {
         var result;
         
         d.rewrite(
+            // var someVar = ...
             'VarDeclInit(x, _)', function(b) {
                 result = b.x.value;
             },
+            // e.x
             'PropAccess(e, x)', function(b) {
                 result = getExpressionValue(b.e) + "." + b.x.value;
             },
+            // x
             'Var(x)', function(b) {
                 result = b.x.value;
             },
+            // e(arg, ...)
             'Call(e, args)', function(b) {
                 var method = getExpressionValue(b.e);
                 var args = b.args.toArray().map(getExpressionValue).join(", ");
                 result = method + "(" + args + ")";
             },
+            // 10
             'Num(n)', function(b) {
                 result = b.n.value;
             },
+            // e[idx]
             'Index(e, idx)', function(b) {
                 result = getExpressionValue(b.e) + "[" + getExpressionValue(b.idx) + "]";
             },
+            // new SomeThing(arg, ...)
             'New(e, args)', function(b) {
                 var method = getExpressionValue(b.e);
                 var args = b.args.toArray().map(getExpressionValue).join(", ");
                 result = "new " + method + "(" + args + ")";
             },
+            // x (function argument)
             'FArg(x)', function(b) {
                 result = b.x.value;
             },
+            // 10 + 4
             'Op(op, e1, e2)', function(b) {
                 result = getExpressionValue(b.e1) + " " + b.op.value + " " + getExpressionValue(b.e2);
             },
+            // if nuthin' else matches
             function() {
                 if(!result)
                     result = "";
