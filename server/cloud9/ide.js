e103ceade081a5620611ab374793d100e5f60e98
Garen Torikian <gjtorikian@gmail.com>
2011-12-06 22:43:57 -0800

    Turn help into a default plugin

--- server/cloud9/ide.js
+++ server/cloud9/ide.js
@@ -107,6 +107,7 @@ Ide.DEFAULT_PLUGINS = [
     "ext/quicksearch/quicksearch",
     "ext/gotoline/gotoline",
     "ext/html/html",
+    "ext/help/help",
     //"ext/ftp/ftp",
     "ext/code/code",
     "ext/imgview/imgview",
