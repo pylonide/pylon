5ec050cf0d028dbad418cb0a94430254a0cdb939
mikedeboer <info@mikedeboer.nl>
2011-12-13 18:19:54 +0100

    removed trailing comma

--- elements/bindingrule.js
+++ elements/bindingrule.js
@@ -75,7 +75,7 @@ apf.BindingRule = function(struct, tagName){
     //1 = force no bind rule, 2 = force bind rule
     this.$attrExcludePropBind = apf.extend({
         value : 1,
-        match : 1,
+        match : 1
     }, this.$attrExcludePropBind);
 
     this.$booleanProperties["hasaml"] = true;
