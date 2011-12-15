a52728caab6cdc30b39f550a6b59aa8d518af14a
Ruben Daniels <ruben@ajax.org>
2011-12-15 14:01:14 +0100

    updated ide.js

--- server/cloud9/ide.js
+++ server/cloud9/ide.js
@@ -126,8 +126,6 @@ Ide.DEFAULT_PLUGINS = [
     "ext/offline/offline",
     "ext/stripws/stripws",
     "ext/testpanel/testpanel",
-    "ext/selenium/selenium",
-    "ext/selenium/editor",
     "ext/nodeunit/nodeunit", 
     "ext/zen/zen",
     "ext/codecomplete/codecomplete",
