7a59e71a40890c62971ecb6aedc718b9cc9c73b7
mikedeboer <info@mikedeboer.nl>
2012-01-16 16:37:44 +0100

    fixed JS error in settings

--- client/ext/settings/settings.js
+++ client/ext/settings/settings.js
@@ -102,7 +102,7 @@ module.exports = ext.register("ext/settings/settings", {
     },
 
     showsettings: function(e){
-        panels.initPanel(this);
+        panels.activate(this);
         this.enable();
         return false;
     },
