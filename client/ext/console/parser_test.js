b1e7d163e7697db6546dea9390a992eda50055ca
Sergi Mansilla <sergi.mansilla@gmail.com>
2012-01-27 19:20:19 +0100

    Improved testing

--- client/ext/console/parser_test.js
+++ client/ext/console/parser_test.js
@@ -10,31 +10,32 @@ if (typeof process !== "undefined") {
     require("../../../support/paths");
     require.paths.unshift(__dirname + "/../..");
 }
-
-var assert = require("assert");
-var parseLine = require("ext/console/parser");
-module.exports = {
-    "test normal cli": function(next) {
-        var expected1 = ["this", "is", "-a", "test"];
-        assert.equal(parseLine("this is -a test").toString(), expected1.toString());
-        next();
-    },
-    "test quoted cli": function(next) {
-        var expected = ["this", "\"is\"", "'a'", "`test`"];
-        assert.equal(parseLine("this \"is\" 'a' `test`").toString(), expected.toString());
-        next();
-    },
-    "test spaces in cli": function(next) {
-        var expected = ["this", "is", "a", "test"];
-        assert.equal(parseLine("this      is    a     test     ").toString(), expected.toString());
-        next();
-    },
-    "test quoted spaces cli": function(next) {
-        var expected = ["this", "\"is\"", "'a really awesome'", "`test`"];
-        assert.equal(parseLine("this \"is\" 'a really awesome' `test`").toString(), expected.toString());
-        next();
-    }
-};
+define(function(require, exports, module) {
+    var assert = require("assert");
+    var parseLine = require("ext/console/parser");
+    module.exports = {
+        "test normal cli": function(next) {
+            var expected1 = ["this", "is", "-a", "test"];
+            assert.equal(parseLine("this is -a test").toString(), expected1.toString());
+            next();
+        },
+        "test quoted cli": function(next) {
+            var expected = ["this", "\"is\"", "'a'", "`test`"];
+            assert.equal(parseLine("this \"is\" 'a' `test`").toString(), expected.toString());
+            next();
+        },
+        "test spaces in cli": function(next) {
+            var expected = ["this", "is", "a", "test"];
+            assert.equal(parseLine("this      is    a     test     ").toString(), expected.toString());
+            next();
+        },
+        "test quoted spaces cli": function(next) {
+            var expected = ["this", "\"is\"", "'a really awesome'", "`test`"];
+            assert.equal(parseLine("this \"is\" 'a really awesome' `test`").toString(), expected.toString());
+            next();
+        }
+    };
+});
 
 if (typeof module !== "undefined" && module === require.main) {
     require("asyncjs").test.testcase(module.exports).exec()
