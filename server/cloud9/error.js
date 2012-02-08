b72ae994b64a69bbffb36b6cdb307ff38a29367b
Fabian Jakobs <fabian.jakobs@web.de>
2012-02-08 16:17:01 +0100

    it is camel cased automatically

--- server/cloud9/error.js
+++ server/cloud9/error.js
@@ -18,7 +18,7 @@ sys.inherits(exports.HttpError, Error);
 
 
 var statusCodes = {
-    400: "BadRequest",
+    400: "Bad Request",
     401: "Unauthorized",
     402: "Payment Required",
     403: "Forbidden",
@@ -36,26 +36,26 @@ var statusCodes = {
     415: "Unsupported Media Type",
     416: "Requested Range Not Satisfiable",
     417: "Expectation Failed",
-    418: "ImATeapot", // (RFC 2324) http://tools.ietf.org/html/rfc2324
-    420: "EnhanceYourCalm", // Returned by the Twitter Search and Trends API when the client is being rate limited
-    422: "UnprocessableEntity", // (WebDAV) (RFC 4918)
+    418: "Im a Teapot", // (RFC 2324) http://tools.ietf.org/html/rfc2324
+    420: "Enhance Your Calm", // Returned by the Twitter Search and Trends API when the client is being rate limited
+    422: "Unprocessable Entity", // (WebDAV) (RFC 4918)
     423: "Locked", // (WebDAV) (RFC 4918)
-    424: "FailedDependency", // (WebDAV) (RFC 4918)
-    425: "UnorderedCollection", // (RFC 3648)
-    426: "UpgradeRequired", // (RFC 2817)
-    428: "PreconditionRequired",
-    429: "TooManyRequests", // Used for rate limiting
-    431: "RequestHeaderFieldsTooLarge",
-    444: "NoResponse", // An nginx HTTP server extension. The server returns no information to the client and closes the connection (useful as a deterrent for malware).
-    449: "RetryWith", // A Microsoft extension. The request should be retried after performing the appropriate action.
-    450: "BlockedByWindowsParentalControls",
-    499: "ClientClosedRequest",
+    424: "Failed Dependency", // (WebDAV) (RFC 4918)
+    425: "Unordered Collection", // (RFC 3648)
+    426: "Upgrade Required", // (RFC 2817)
+    428: "Precondition Required",
+    429: "Too Many Requests", // Used for rate limiting
+    431: "Request Header Fields Too Large",
+    444: "No Response", // An nginx HTTP server extension. The server returns no information to the client and closes the connection (useful as a deterrent for malware).
+    449: "Retry With", // A Microsoft extension. The request should be retried after performing the appropriate action.
+    450: "Blocked By Windows Parental Controls",
+    499: "Client Closed Request",
     500: "Internal Server Error",
     501: "Not Implemented",
     502: "Bad Gateway",
     503: "Service Unavailable",
     504: "Gateway Timeout",
-    505: "HTTPVersionNotSupported"
+    505: "HTTP Version Not Supported"
 };
 
 for (var status in statusCodes) {
