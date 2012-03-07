3730c884538b4b28a49066b06581a75bb5e50be0
Christoph Dorn <christoph@christophdorn.com>
2012-03-07 15:37:34 -0800

    added log.log() and direct logging calls to respective nodejs console functions

--- plugins-server/cloud9.log/log-service.js
+++ plugins-server/cloud9.log/log-service.js
@@ -5,6 +5,7 @@ module.exports = function startup(options, imports, register) {
 
     register(null, {
         log: {
+            log: logger.log.bind(logger),
             info: logger.info.bind(logger),
             warn: logger.warn.bind(logger),
             error: logger.error.bind(logger)
@@ -16,16 +17,20 @@ var LogService = module.exports.LogService = function() {};
 
 (function() {
 
-    this.info = function() {
+    this.log = function() {
         console.log.apply(console, arguments);
+    };
+    
+    this.info = function() {
+        console.info.apply(console, arguments);
     };
 
     this.warn = function() {
-        console.log.apply(console, arguments);
+        console.warn.apply(console, arguments);
     };
 
     this.error = function() {
-        console.log.apply(console, arguments);
+        console.error.apply(console, arguments);
     };
 
 }).call(LogService.prototype);
