5f156c5c2d8ce9b40681ff1a7267ca17a48ed36d
Christoph Dorn <christoph@christophdorn.com>
2012-03-14 07:56:27 -0700

    getModule() for connect

--- plugins-server/cloud9.alive/alive.js
+++ plugins-server/cloud9.alive/alive.js
@@ -1,14 +1,14 @@
 
 module.exports = function setup(options, imports, register) {
 
-    var connect = imports.connect;
+    imports.connect.useStart(imports.connect.getModule().router(function(app) {
+        app.get(/^\/alive$/, function(req, res) {
 
-    connect.addRoute("get", /^\/alive$/, function(req, res) {
+            // TODO: Perform internal check to verify that process is still operating properly.
 
-        // TODO: Perform internal check to verify that process is still operating properly.
-
-        res.end("OK");
-    });
+            res.end("OK");
+        });
+    }));
     
     register(null, {});
 };
