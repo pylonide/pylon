b85a832f5edeea3d275d1f726079a26f63ba7d10
Garen Torikian <gjtorikian@gmail.com>
2012-03-29 16:51:14 -0700

    Fix up dialog for searchinfiles & folder selection

--- client/ext/searchinfiles/searchinfiles.js
+++ client/ext/searchinfiles/searchinfiles.js
@@ -111,8 +111,7 @@ module.exports = ext.register("ext/searchinfiles/searchinfiles", {
         else
             selectedNode = this.getSelectedTreeNode();
         // get selected node in tree and set it as selection
-        var filepath = selectedNode.getAttribute("path").split("/");
-        var name = filepath[filepath.length - 2];
+        var name = selectedNode.getAttribute("name");
         
         if (name.length > 25) {
             name = name.substr(0, 22) + "...";
