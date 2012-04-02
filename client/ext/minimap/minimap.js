pylonide/pylon

commit 4a4a0137e0579203ebc418b2b2481da8a79db01e

* fix drag drop files- fixes #1029 * fix for errors if only image is open on load of the editor and update zen, statusbar, minimap plugins * add check if the ceEditor is not available

client/ext/minimap/minimap.js
@@ -52,11 +52,25 @@ return module.exports = ext.register("ext/minimap/minimap", {
         ide.addEventListener("loadsettings", function(e) {
             _self.map_enabled = e.model.queryValue("editors/code/@minimap");
         });
+        
+        tabEditors.addEventListener("afterswitch", function(){
+            if(_self.initFail && !_self.initDone) {
+                setTimeout(function(){
+                        _self.initFail = false;
+                        _self.init();
+                }, 200);
+            }
+        });
     },
 
     init : function() {
         var _self = this;
-
+        
+        if(typeof ceEditor === "undefined") {
+            this.initFail = true;
+            return;
+        }
+        
         apf.importCssString((this.css || ""));
 
         this.editor = ceEditor.$editor;
@@ -95,6 +109,8 @@ return module.exports = ext.register("ext/minimap/minimap", {
         }
 
         this.setupChangeListener();
+        
+        this.initDone = true;
     },
     
     setupChangeListener : function() {

client/ext/statusbar/statusbar.js
@@ -94,6 +94,10 @@ module.exports = ext.register("ext/statusbar/statusbar", {
                 _self.editorSession.selection.removeEventListener("changeSelection", _self.$changeEvent);
 
             setTimeout(function() {
+                if(_self.initFail && !_self.initDone) {
+                    _self.initFail = false;
+                    _self.init();
+                }
                 if(editors.currentEditor.ceEditor) {
                     _self.editorSession = editors.currentEditor.ceEditor.$editor.session;
                     _self.editorSession.selection.addEventListener("changeSelection", _self.$changeEvent = function(e) {
@@ -120,7 +124,12 @@ module.exports = ext.register("ext/statusbar/statusbar", {
         });
     },
 
-    init : function(){
+    init : function(){        
+        if(typeof ceEditor === "undefined") {
+            this.initFail = true;
+            return;
+        }
+        
         var _self = this;
         var editor = editors.currentEditor;
         if (editor && editor.ceEditor) {
@@ -170,7 +179,8 @@ module.exports = ext.register("ext/statusbar/statusbar", {
                     lblInsertActive.show();
             }
         });
-
+        
+        this.initDone = true;
         this.inited = true;
     },
 
@@ -245,7 +255,7 @@ module.exports = ext.register("ext/statusbar/statusbar", {
     },
 
     setPosition : function() {
-        if (ceEditor && ceEditor.$editor) {
+        if (typeof ceEditor !== "undefined" && ceEditor.$editor) {
             var _self = this;
             var cw = ceEditor.$editor.renderer.scroller.clientWidth;
             var sw = ceEditor.$editor.renderer.scroller.scrollWidth;

client/ext/zen/zen.js
@@ -81,9 +81,24 @@ module.exports = ext.register("ext/zen/zen", {
 
             _self.updateButtonPosition();
         });
+        
+        tabEditors.addEventListener("afterswitch", function(){
+            if(_self.initFail && !_self.initDone) {
+                setTimeout(function(){
+                        _self.initFail = false;
+                        _self.init();
+                }, 200);
+            }
+        });
     },
 
     init : function(amlNode){
+        var _self = this;
+        if(typeof ceEditor === "undefined") {
+            this.initFail = true;
+            return;
+        }
+        
         // Create all the elements used here
         this.animateZen = document.createElement("div");
         this.animateZen.setAttribute("id", "animateZen");
@@ -125,12 +140,13 @@ module.exports = ext.register("ext/zen/zen", {
         this.animateZen = document.getElementById("animateZen");
         this.animateZenPosition = document.getElementById("animateZenPosition");
 
-        var _self = this;
         vbZen.addEventListener("resize", function(e) {
             if (_self.isFocused) {
                 _self.calculatePositions();
             }
         });
+        
+        this.initDone = true;
     },
 
     updateButtonPosition : function() {
@@ -139,7 +155,7 @@ module.exports = ext.register("ext/zen/zen", {
 
         // Extra safe default width
         var sbWidth = 20;
-        if (ceEditor && ceEditor.$editor)
+        if (ceEditor.$editor)
             sbWidth = ceEditor.$editor.renderer.scrollBar.width;
 
         btnZenFullscreen.setAttribute("right", sbWidth + this.offsetWidth);

client/ext/code/code.js
@@ -356,7 +356,8 @@ module.exports = ext.register("ext/code/code", {
         });
 
         tabEditors.addEventListener("afterswitch", function(e) {
-            ceEditor.afterOpenFile(ceEditor.getSession());
+            if(typeof ceEditor != "undefined")
+                ceEditor.afterOpenFile(ceEditor.getSession());
         });
     },
 

client/ext/dragdrop/dragdrop.js
@@ -52,8 +52,10 @@ module.exports = ext.register("ext/dragdrop/dragdrop", {
         });
         
         ide.addEventListener("init.ext/tree/tree", function(){
-            _self.nodes.push(trFiles.$ext);
-            decorateNode(trFiles.$ext);
+            setTimeout(function(){
+                _self.nodes.push(trFiles.$ext);
+                decorateNode(trFiles.$ext);
+            }, 200);
         });
         
         this.dragStateEvent = {"dragenter": dragEnter};
@@ -194,9 +196,10 @@ module.exports = ext.register("ext/dragdrop/dragdrop", {
         var node = trFiles.selected;
         if (!node)
             node = trFiles.xmlRoot.selectSingleNode("folder");
-            
-        if (node.getAttribute("type") != "folder" && node.tagName != "folder")
+        
+        while (node.getAttribute("type") != "folder" && node.tagName != "folder") {
             node = node.parentNode;
+        }
             
         var path     = node.getAttribute("path");
         var filename = file.name;
@@ -205,10 +208,26 @@ module.exports = ext.register("ext/dragdrop/dragdrop", {
 
         function check(exists) {
             if (exists) {
+                util.confirm(
+                    "Are you sure?",
+                    "\"" + file.name + "\" already exists, do you want to replace it?",
+                    "A file or folder with the same name already exists. "
+                    + "Replacing it will overwrite it's current contents.",
+                    removeExisting);
+            }
+            else {
+                upload();
+            }
+            /*if (exists) {
                 filename = file.name + "." + index++;
                 fs.exists(path + "/" + filename, check);
             } else
-                upload();
+                upload();*/
+        }
+        
+        function removeExisting(){
+            apf.xmldb.removeNode(trFiles.queryNode('//file[@path="' + path + "/" + filename + '"]'));
+            fs.remove(path + "/" + filename, upload);
         }
         
         function upload() {

client/ext/editors/editors.js
@@ -517,7 +517,8 @@ module.exports = ext.register("ext/editors/editors", {
             this.initEditor(editor);
         
         this.currentEditor = editor;
-        editor.ceEditor.focus();
+        if(editor.ceEditor)
+            editor.ceEditor.focus();
 
         //toHandler.$rbEditor.select();
 

client/ext/filesystem/filesystem.js
@@ -283,12 +283,15 @@ module.exports = ext.register("ext/filesystem/filesystem", {
         return true;
     },
 
-    remove: function(path) {
+    remove: function(path, callback) {
         var page = tabEditors.getPage(path);
         if (page)
             tabEditors.remove(page);
-
-        davProject.remove(path, false, function() {});
+        
+        if(!callback)
+            callback = function() {};
+            
+        davProject.remove(path, false, callback);
     },
 
     /**** Init ****/

client/ext/imgview/imgview.js
@@ -43,6 +43,9 @@ module.exports = ext.register("ext/imgview/imgview", {
     setDocument : function(doc, actiontracker){
         doc.session = doc.getNode().getAttribute("path");
         imgEditor.setProperty("value", doc.session);
+        
+        var d = new Date();
+        imgEditor.$ext.getElementsByTagName('img')[0].setAttribute('src', imgEditor.$ext.getElementsByTagName('img')[0].getAttribute('src') + '?' + d.getTime())
     },
 
     hook : function() {},

commit a5489d8794cf9405c2169ac3c16757e3323969fc

Fixed breaking changes introduced in 4a4a0137

client/ext/minimap/minimap.js
@@ -45,32 +45,42 @@ return module.exports = ext.register("ext/minimap/minimap", {
 
         ide.addEventListener("afteropenfile", function() {
             ext.initExtension(_self);
-            if (_self.editor)
-                _self.updateMap();
+            
         });
 
         ide.addEventListener("loadsettings", function(e) {
             _self.map_enabled = e.model.queryValue("editors/code/@minimap");
         });
-        
-        tabEditors.addEventListener("afterswitch", function(){
-            if(_self.initFail && !_self.initDone) {
-                setTimeout(function(){
-                        _self.initFail = false;
-                        _self.init();
+
+        tabEditors.addEventListener("afterswitch", function(e){
+            if (e.nextPage.type === "ext/imgview/imgview")
+                return;
+
+            function afterSwitch() {
+                if (_self.editor)
+                    _self.updateMap();
+
+                setTimeout(function() {
+                    _self.setupChangeListener();
                 }, 200);
+            }
+
+            if (!_self.inited) {
+                // Wait a moment for the editor to get into place
+                setTimeout(function() {
+                    ext.initExtension(_self);
+                    afterSwitch();
+                });
+            }
+            else {
+                afterSwitch();
             }
         });
     },
 
     init : function() {
         var _self = this;
-        
-        if(typeof ceEditor === "undefined") {
-            this.initFail = true;
-            return;
-        }
-        
+
         apf.importCssString((this.css || ""));
 
         this.editor = ceEditor.$editor;
@@ -95,13 +105,6 @@ return module.exports = ext.register("ext/minimap/minimap", {
                 _self.map.resize(_self.map_width, ceEditor.getHeight());
         });
 
-        tabEditors.addEventListener("afterswitch", function() {
-            _self.updateMap();
-            setTimeout(function() {
-                _self.setupChangeListener();
-            }, 200);
-        });
-
         if (apf.isTrue(this.map_enabled)) {
             setTimeout(function() {
                 _self.show();
@@ -109,10 +112,8 @@ return module.exports = ext.register("ext/minimap/minimap", {
         }
 
         this.setupChangeListener();
-        
-        this.initDone = true;
     },
-    
+
     setupChangeListener : function() {
         if (this.$changeEvent)
             this.editorSession.removeEventListener("change", this.$changeEvent);

client/ext/statusbar/statusbar.js
@@ -39,10 +39,6 @@ module.exports = ext.register("ext/statusbar/statusbar", {
 
     hook : function(){
         var _self = this;
-        ide.addEventListener("afteropenfile", this.$aofListener = function() {
-            ext.initExtension(_self);
-            ide.removeEventListener("afteropenfile", _self.$aofListener);
-        });
 
         ide.addEventListener("loadsettings", function(e){
             var strSettings = e.model.queryValue("auto/statusbar");
@@ -89,7 +85,17 @@ module.exports = ext.register("ext/statusbar/statusbar", {
             _self.setPosition();
         });
 
-        tabEditors.addEventListener("afterswitch", function() {
+        tabEditors.addEventListener("afterswitch", function(e) {
+            if (e.nextPage.type === "ext/imgview/imgview")
+                return;
+
+            if (!_self.inited) {
+                // Wait a moment for the editor to get into place
+                setTimeout(function() {
+                    ext.initExtension(_self);
+                });
+            }
+
             if (_self.$changeEvent)
                 _self.editorSession.selection.removeEventListener("changeSelection", _self.$changeEvent);
 
@@ -111,6 +117,9 @@ module.exports = ext.register("ext/statusbar/statusbar", {
     },
 
     init : function(){
+        if (typeof ceEditor === "undefined")
+            return;
+
         var _self = this;
         var editor = editors.currentEditor;
         if (editor && editor.ceEditor) {
@@ -160,8 +169,6 @@ module.exports = ext.register("ext/statusbar/statusbar", {
                     lblInsertActive.show();
             }
         });
-
-        this.inited = true;
     },
 
     addToolsItem: function(menuItem, position){

client/ext/zen/zen.js
@@ -3,7 +3,6 @@
  *
  * @TODO
  * - Disabling the extension doesn't call the disable() function
- * - Exit zen mode when doing any keybinding operation (except openfiles, quicksearch, gotoline)
  * - While animating, disable ability to toggle zen mode (better: cancel and reverse the operation)
  *
  * @copyright 2011, Ajax.org B.V.
@@ -52,14 +51,6 @@ module.exports = ext.register("ext/zen/zen", {
 
     hook : function(){
         var _self = this;
-        ide.addEventListener("openfile", function() {
-            if (_self.neverShown) {
-                setTimeout(function() {
-                    ext.initExtension(_self);
-                }, 1000);
-                _self.neverShown = false;
-            }
-        });
 
         ide.addEventListener("loadsettings", function(e){
             var strSettings = e.model.queryValue("auto/zen");
@@ -82,23 +73,22 @@ module.exports = ext.register("ext/zen/zen", {
             _self.updateButtonPosition();
         });
         
-        tabEditors.addEventListener("afterswitch", function(){
-            if(_self.initFail && !_self.initDone) {
-                setTimeout(function(){
-                        _self.initFail = false;
-                        _self.init();
-                }, 200);
+        tabEditors.addEventListener("afterswitch", function(e){
+            if (e.nextPage.type === "ext/imgview/imgview")
+                return;
+
+            if (!_self.inited) {
+                // Wait a moment for the editor to get into place
+                setTimeout(function() {
+                    ext.initExtension(_self);
+                });
             }
         });
     },
 
-    init : function(amlNode){
+    init : function(){
         var _self = this;
-        if(typeof ceEditor === "undefined") {
-            this.initFail = true;
-            return;
-        }
-        
+
         // Create all the elements used here
         this.animateZen = document.createElement("div");
         this.animateZen.setAttribute("id", "animateZen");
@@ -149,8 +139,6 @@ module.exports = ext.register("ext/zen/zen", {
         ide.addEventListener("exitfullscreen", function() {
             _self.escapeFromZenMode(false, true);
         });
-        
-        this.initDone = true;
     },
 
     updateButtonPosition : function() {