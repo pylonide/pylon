eb91acd3d544bfb41994148a8f6a9474ec48e712
Vlad Zinculescu <vlad@c9.io>
2012-04-03 12:45:54 +0200

    Improved console skin

--- client/ext/console/console.js
+++ client/ext/console/console.js
@@ -331,6 +331,7 @@ module.exports = ext.register("ext/console/console", {
     },
 
     init: function(amlNode){
+
         var _self = this;
         this.panel = tabConsole;
         this.$cwd  = "/workspace"; // code smell
@@ -496,7 +497,7 @@ module.exports = ext.register("ext/console/console", {
                 height: this.height,
                 dbgVisibleMethod: "show",
                 chkExpandedMethod: "check",
-                animFrom: 65,
+                animFrom: this.height*0.95,
                 animTo: this.height > this.minHeight ? this.height : this.minHeight,
                 animTween: "easeOutQuint"
             };
