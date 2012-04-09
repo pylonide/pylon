8bf6e11d868a419a85e4c7dc3b4685824543e95b
Ruben Daniels <ruben@c9.io>
2012-04-09 06:10:40 +0200

    * Working on refactor of gotofile

--- client/ext/gotofile/gotofile.js
+++ client/ext/gotofile/gotofile.js
@@ -25,6 +25,7 @@ module.exports = ext.register("ext/gotofile/gotofile", {
     },
     hotitems: {},
 
+    dirty   : true,
     nodes   : [],
 
     hook : function(){
@@ -49,6 +50,11 @@ module.exports = ext.register("ext/gotofile/gotofile", {
                 }
             }))
         );
+                
+        ide.addEventListener("init.ext/editors/editors", function(){
+            _self.markupInsertionPoint = tabEditors;
+            //tabEditors.appendChild(winGoToFile);
+        });
 
         this.hotitems["gotofile"] = [this.nodes[0]];
     },
@@ -56,17 +62,27 @@ module.exports = ext.register("ext/gotofile/gotofile", {
     init : function() {
         var _self = this;
         
+        txtGoToFile.addEventListener("prop.visible", function(e){
+            if (e.value) {
+                txtGoToFile.select();
+                txtGoToFile.focus();
+                _self.dirty = true;
+            }
+        });
+        
         txtGoToFile.addEventListener("keydown", function(e){
             if (txtGoToFile.value == "") {
                 return;
             }
 
             if (e.keyCode == 13){
-                //var node = trFiles.xmlRoot.selectSingleNode("folder[1]");
-                mdlGoToFile.load("{davProject.report('" + ide.davPrefix //node.getAttribute("path")
-                    + "', 'filesearch', {query: '" + txtGoToFile.value + "'})}");
+                _self.searchFor(txtGoToFile.value);
+
                 ide.dispatchEvent("track_action", {type: "gotofile"});
                 return false;
+            }
+            else if (e.keyCode == 27) {
+                winGoToFile.hide();
             }
             else if (e.keyCode == 40 && dgGoToFile.length) {
                 var first = dgGoToFile.getFirstTraverseNode();
@@ -94,14 +110,78 @@ module.exports = ext.register("ext/gotofile/gotofile", {
         dgGoToFile.addEventListener("afterchoose", function(e) {
             _self.openFile();
         });
+        
+        this.fetchAllFiles();
 
         this.nodes.push(winGoToFile);
+    },
+    
+    fetchAllFiles : function(){
+        var _self = this;
+
+        //@todo create an allfiles plugin that plugins like gotofile can depend on
+        //@todo soon this protocol needs to be turned into a json protocol
+        davProject.report(ide.davPrefix, 'filesearch', {query: ""}, //@todo filelist needs some fixing
+          function(data, state, extra){
+            if (state == apf.ERROR) {
+                if (data && data.indexOf("jsDAV_Exception_FileNotFound") > -1) {
+                    return;
+                }
+
+                //@todo
+                return;
+            }
+            if (state == apf.TIMEOUT)
+                return; //@todo
+
+            var re = new RegExp("^\.|\\.bzr|\\.cdv|\\.dep|\\.dot|\\.nib|\\.plst|\\.git|\\.hg|\\.pc|\\.svn|blib|CVS|RCS|SCCS|_darcs|_sgbak|autom4te\\.cache|cover_db|_build|\\.tmp");
+            var nodes = data.firstChild.childNodes;
+            for (var i = nodes.length - 1; i >= 0; i--) {
+                if (re.test(nodes[i].firstChild.nodeValue))
+                    nodes[i].parentNode.removeChild(nodes[i]);
+            }
+
+            _self.cachedFileList = data;
+
+            if (winGoToFile.visible)
+                _self.updateSearchResults();
+            
+            mdlGoToFile.load(data);
+        });
+    },
+    
+    updateSearchResults : function(){
+        var data = this.cachedFileList;
+        
+        /*var nodes = data.selectNodes("//d:href");
+        for (var node, i = 0; i < nodes.length; i++) {
+            node = nodes[i];
+
+            //@todo support for submodules
+            if (node.firstChild.nodeValue.match(/_test\.js$/)) {
+                var file = apf.getXml("<file />");
+                var path = ide.davPrefix + "/" + node.firstChild.nodeValue;
+                file.setAttribute("name", path.split("/").pop());
+                file.setAttribute("path", path);
+                file.setAttribute("type", "nodeunit");
+                apf.xmldb.appendChild(testpanel.findParent(path), file);
+            }
+        }*/
+    },
+    
+    searchFor : function(keyword){
+        this.lastSearch = keyword;
+                
+//        mdlGoToFile.load("{davProject.report('" + ide.davPrefix 
+//            + "', 'filesearch', {query: '" + txtGoToFile.value + "'})}");
+
+        
     },
     
     openFile: function(){
         var nodes = dgGoToFile.getSelection();
         
-        if(nodes.length == 0)
+        if (nodes.length == 0)
             return false;
             
         winGoToFile.hide();
