3b1e8cb50a989cf6502fe356cf9fec2fa0a87af1
cadorn <christoph@christophdorn.com>
2012-04-10 13:32:36 -0700

    POST for /alive

--- plugins-server/cloud9.alive/alive.js
+++ plugins-server/cloud9.alive/alive.js
@@ -3,12 +3,20 @@ module.exports = function setup(options, imports, register) {
 
     imports.connect.useStart(imports.connect.getModule().router(function(app) {
         app.get(/^\/alive$/, function(req, res) {
-
-            // TODO: Perform internal check to verify that process is still operating properly.
-
-            res.end("OK");
+            checkAlive(req, res);
+        });
+        app.post(/^\/alive$/, function(req, res) {
+            checkAlive(req, res);
         });
     }));
-    
+
     register(null, {});
 };
+
+
+function checkAlive(req, res) {
+
+    // TODO: Perform internal check to verify that process is still operating properly.
+
+    res.end("OK");
+}
