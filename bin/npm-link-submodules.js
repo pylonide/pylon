5fd6500709e5b6417b2c8318ef13d47095e56655
Jan Jongboom <janjongboom@gmail.com>
2012-04-20 09:41:36 +0200

    NPM link script

---
+++ bin/npm-link-submodules.js
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd `dirname $0`/..
+
+git submodule update --init --recursive
+
+cd support/architect
+npm link
+cd ../..
+
+cd support/ace
+npm link
+cd ../..
+
+cd support/jsdav
+npm link
+cd ../..
+
+npm link architect
+npm link ace
+npm link jsdav
+
+npm install
