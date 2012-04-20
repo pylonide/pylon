690dfe557d8fe7c1e97895fe361e5f40206a0eed
Giannis <bone.jp@gmail.com>
2012-04-20 18:19:01 +0300

    * fix when closing tabs the first time, they are still in the tabs dropdown issue 784 in infra

--- client/ext/tabbehaviors/tabbehaviors.js
+++ client/ext/tabbehaviors/tabbehaviors.js
@@ -661,10 +661,11 @@ if (!page.getAttribute("caption").trim()) debugger;
 
     removeItem: function(page) {
         var item, idx, keyId;
-        var i = this.menuOffset;
+        var i = 0;
         var l = this.nodes.length;
         var _self = this;
-        for (; i < l; ++i) {
+        
+        for (; i < l; i++) {
             if ((item = this.nodes[i]).relPage == page.id) {
                 item.destroy(true, true);
                 this.nodes.splice(i, 1);
