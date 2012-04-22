pylonide/pylon

commit 8fc4813e21e493e815d3604bf88a0a16ec9c3c49

server: 'sys' module is deprecated, use 'util'

server/cloud9/ext/git/git.js
@@ -5,7 +5,7 @@
  * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
  */
 var Plugin = require("cloud9/plugin");
-var sys = require("sys");
+var util = require("util");
 var util = require("cloud9/util");
 
 var ShellGitPlugin = module.exports = function(ide, workspace) {
@@ -14,7 +14,7 @@ var ShellGitPlugin = module.exports = function(ide, workspace) {
     this.name = "git";
 };
 
-sys.inherits(ShellGitPlugin, Plugin);
+util.inherits(ShellGitPlugin, Plugin);
 
 (function() {
     var githelp     = "",

server/cloud9/ext/shell/shell.js
@@ -7,7 +7,7 @@
 
 "use strict";
 
-var sys = require("sys");
+var util = require("util");
 var Plugin = require("cloud9/plugin");
 var Fs = require("fs");
 var Path = require("path");
@@ -21,7 +21,7 @@ var ShellPlugin = module.exports = function(ide, workspace) {
     this.name = "shell";
 };
 
-sys.inherits(ShellPlugin, Plugin);
+util.inherits(ShellPlugin, Plugin);
 
 (function() {
 

server/cloud9/ide.js
@@ -7,7 +7,7 @@ var DavPermission = require("./dav/permission");
 var Async = require("asyncjs");
 var User = require("./user");
 var fs = require("fs");
-var sys = require("sys");
+var util = require("util");
 var Url = require("url");
 var template = require("./template");
 var Workspace = require("cloud9/workspace");
@@ -91,7 +91,7 @@ var Ide = module.exports = function(options, httpServer, exts, socket) {
     }
 };
 
-sys.inherits(Ide, EventEmitter);
+util.inherits(Ide, EventEmitter);
 
 var exts = require("../../client/ext/all");
 

server/cloud9/user.js
@@ -1,4 +1,4 @@
-var sys = require("sys");
+var util = require("util");
 var util = require("./util");
 var EventEmitter = require("events").EventEmitter;
 
@@ -18,7 +18,7 @@ var User = function (uid, permissions, data) {
     this.$server_exclude = {};
 };
 
-sys.inherits(User, EventEmitter);
+util.inherits(User, EventEmitter);
 
 User.OWNER_PERMISSIONS = {
     client_exclude: "",

server/cloud9/dav/permission.js
@@ -1,5 +1,5 @@
 var jsDAV_ServerPlugin = require("DAV/plugin").jsDAV_ServerPlugin;
-var sys = require("sys");
+var util = require("util");
 var Exec = require("child_process").exec;
 
 var Permission = module.exports = function(handler) {
@@ -9,7 +9,7 @@ var Permission = module.exports = function(handler) {
     handler.addEventListener("beforeMethod", this.checkPermission.bind(this));
 };
 
-sys.inherits(Permission, jsDAV_ServerPlugin);
+util.inherits(Permission, jsDAV_ServerPlugin);
 
 (function() {
 

server/cloud9/error.js
@@ -1,4 +1,4 @@
-var sys = require("sys");
+var util = require("util");
 
 exports.HttpError = function(message, code) {
     Error.call(this, message);
@@ -6,7 +6,7 @@ exports.HttpError = function(message, code) {
     this.message = message;
     this.code = code;
 };
-sys.inherits(exports.HttpError, Error);
+util.inherits(exports.HttpError, Error);
 
 (function() {
 
@@ -85,7 +85,7 @@ for (var status in statusCodes) {
         };
     })(defaultMsg, status);
 
-    sys.inherits(error, exports.HttpError);
+    util.inherits(error, exports.HttpError);
 
     var className = toCamelCase(defaultMsg);
     exports[className] = error;

server/cloud9/ext/auth/auth.js
@@ -5,7 +5,7 @@
  * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
  */
 var Plugin = require("cloud9/plugin");
-var sys = require("sys");
+var util = require("util");
 
 var AuthPlugin = module.exports = function(ide, workspace) {
     Plugin.call(this, ide, workspace);
@@ -13,7 +13,7 @@ var AuthPlugin = module.exports = function(ide, workspace) {
     this.name = "auth";
 };
 
-sys.inherits(AuthPlugin, Plugin);
+util.inherits(AuthPlugin, Plugin);
 
 (function() {
     

server/cloud9/ext/blame/blame.js
@@ -6,7 +6,7 @@
  */
 var Plugin = require("cloud9/plugin");
 var Fs     = require("fs");
-var sys    = require("sys");
+var util   = require("util");
 
 var BlamePlugin = module.exports = function(ide, workspace) {
     Plugin.call(this, ide, workspace);
@@ -14,7 +14,7 @@ var BlamePlugin = module.exports = function(ide, workspace) {
     this.name  = "blame";
 };
 
-sys.inherits(BlamePlugin, Plugin);
+util.inherits(BlamePlugin, Plugin);
 
 (function() {
 

commit e43bf3a5170c7f2ee63574ba120e7959550373eb

server: fix util module name clash
Fixes regression introduced in 8fc4813.

server/cloud9/ext/git/git.js
@@ -5,8 +5,8 @@
  * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
  */
 var Plugin = require("cloud9/plugin");
+var Util = require("cloud9/util");
 var util = require("util");
-var util = require("cloud9/util");
 
 var ShellGitPlugin = module.exports = function(ide, workspace) {
     Plugin.call(this, ide, workspace);
@@ -50,14 +50,14 @@ util.inherits(ShellGitPlugin, Plugin);
         }
 
         function onfinish() {
-            util.extend(commands, githelp);
+            Util.extend(commands, githelp);
             callback();
         }
     };
 
     this.augmentCommand = function(cmd, struct) {
         var map = commandsMap[cmd] || commandsMap["default"];
-        return util.extend(struct, map || {});
+        return Util.extend(struct, map || {});
     };
 
     this.command = function(user, message, client) {

server/cloud9/ext/shell/shell.js
@@ -12,7 +12,7 @@ var Plugin = require("cloud9/plugin");
 var Fs = require("fs");
 var Path = require("path");
 var Async = require("asyncjs");
-var util = require("cloud9/util");
+var Util = require("cloud9/util");
 
 var ShellPlugin = module.exports = function(ide, workspace) {
     Plugin.call(this, ide, workspace);
@@ -114,7 +114,7 @@ util.inherits(ShellPlugin, Plugin);
                  }
                  else {
                      if (oExt.metadata && oExt.metadata.commands)
-                         util.extend(commands, oExt.metadata.commands);
+                         Util.extend(commands, oExt.metadata.commands);
                      next();
                  }
              })

server/cloud9/ide.js
@@ -12,7 +12,7 @@ var Url = require("url");
 var template = require("./template");
 var Workspace = require("cloud9/workspace");
 var EventEmitter = require("events").EventEmitter;
-var util = require("./util");
+var Util = require("./util");
 
 var Ide = module.exports = function(options, httpServer, exts, socket) {
     EventEmitter.call(this);
@@ -55,9 +55,9 @@ var Ide = module.exports = function(options, httpServer, exts, socket) {
         real: options.real
     };
     // precalc regular expressions:
-    this.indexRe = new RegExp("^" + util.escapeRegExp(this.options.baseUrl) + "(?:\\/(?:index.html?)?)?$");
-    this.reconnectRe = new RegExp("^" + util.escapeRegExp(this.options.baseUrl) + "\\/\\$reconnect$");
-    this.workspaceRe = new RegExp("^" + util.escapeRegExp(this.options.davPrefix) + "(\\/|$)");
+    this.indexRe = new RegExp("^" + Util.escapeRegExp(this.options.baseUrl) + "(?:\\/(?:index.html?)?)?$");
+    this.reconnectRe = new RegExp("^" + Util.escapeRegExp(this.options.baseUrl) + "\\/\\$reconnect$");
+    this.workspaceRe = new RegExp("^" + Util.escapeRegExp(this.options.davPrefix) + "(\\/|$)");
 
     this.$users = {};
 
@@ -72,7 +72,7 @@ var Ide = module.exports = function(options, httpServer, exts, socket) {
     };
 
     if (options.remote)
-        util.extend(davOptions, options.remote);
+        Util.extend(davOptions, options.remote);
     else
         davOptions.path = this.options.mountDir;
 
@@ -149,13 +149,13 @@ exports.DEFAULT_DAVPLUGINS = ["auth", "codesearch", "filelist", "filesearch"];
             });
 
             var permissions = _self.getPermissions(req);
-            var plugins = util.arrayToMap(_self.options.plugins);
+            var plugins = Util.arrayToMap(_self.options.plugins);
 
-            var client_exclude = util.arrayToMap(permissions.client_exclude.split("|"));
+            var client_exclude = Util.arrayToMap(permissions.client_exclude.split("|"));
             for (plugin in client_exclude)
                 delete plugins[plugin];
 
-            var client_include = util.arrayToMap((permissions.client_include || "").split("|"));
+            var client_include = Util.arrayToMap((permissions.client_include || "").split("|"));
             for (plugin in client_include)
                 if (plugin)
                     plugins[plugin] = 1;

server/cloud9/user.js
@@ -1,5 +1,5 @@
 var util = require("util");
-var util = require("./util");
+var Util = require("./util");
 var EventEmitter = require("events").EventEmitter;
 
 var User = function (uid, permissions, data) {
@@ -60,7 +60,7 @@ User.VISITOR_PERMISSIONS = {
 (function() {
     
     this.setPermissions = function(permissions) {
-        this.$server_exclude = util.arrayToMap(permissions.server_exclude.split("|"));
+        this.$server_exclude = Util.arrayToMap(permissions.server_exclude.split("|"));
         if (this.permissions === permissions)
             return;
         