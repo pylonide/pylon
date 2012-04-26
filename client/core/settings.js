c2c63531a7fe2e250451b7b6639b255ab0dc55c5
Garen Torikian <gjtorikian@gmail.com>
2012-04-26 13:03:19 -0700

    Revert the change made in 1a3295173116262a5585d17b558a37f62e70344b

--- client/core/settings.js
+++ client/core/settings.js
@@ -89,7 +89,7 @@ module.exports = {
     /**
      * Initializes the settings. The settings can come from different sources:
      * - Template (used for when no settings have been stored previously)
-     * - Parsed into the index file (by the backend - apf.IdeSettings)
+     * - Parsed into the index file (by the backend - cloud9config.settings)
      * - LocalStorage (saved for use when starting in offline mode only)
      */
     init : function(){
@@ -105,12 +105,12 @@ module.exports = {
             xml = localStorage[sIdent];
 
         // Load from template
-        else if (!apf.IdeSettings || apf.IdeSettings == "defaults")
+        else if (!cloud9config.settings || cloud9config.settings == "defaults")
             xml = template
 
         // Load from parsed settings in the index file
-        else if (apf.IdeSettings)
-            xml = apf.IdeSettings;
+        else if (cloud9config.settings)
+            xml = cloud9config.settings;
 
         if (!xml) {
             ide.addEventListener("socketMessage", function(e){
