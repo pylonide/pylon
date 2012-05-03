abd46bf40609e21eecb24b3c23dfa3819d329b9a
Garen Torikian <gjtorikian@gmail.com>
2012-05-03 01:16:33 -0700

    Update for working with node0.6

--- package.js
+++ package.js
@@ -1,5 +1,9 @@
-require("./paths");
-var Sys   = require("sys"),
+require("asyncjs");
+require("apf");
+require("node-o3-xml");
+require("node-o3-xml-v4");
+
+var Util   = require("util"),
     Fs    = require("fs"),
     Path  = require("path");
 
@@ -20,23 +24,23 @@ if (module === require.main) {
 
 
 function boot() {
-    require("apf-node");
+    require("apf/apf-node");
     
-    require("proc");
-    require("files");
-    require("defines");
-    require("xslt");
-    require("proc/combine");
-    require("proc/compress");
-    require("proc/encoder");
-    require("proc/inline");
-    require("proc/docgen");
+    require("./lib/proc");
+    require("./lib/files");
+    require("./lib/defines");
+    require("./lib/xslt");
+    require("./lib/proc/combine");
+    require("./lib/proc/compress");
+    require("./lib/proc/encoder");
+    require("./lib/proc/inline");
+    require("./lib/proc/docgen");
     //require("proc/vcs");
     
     var argv = process.argv;
 
     var project = (argv.length == 3) ? argv[2] : "apf_release.apr";
-    Sys.puts("using file: " + __dirname + "/projects/" + project);
+    Util.puts("using file: " + __dirname + "/projects/" + project);
 
     if (!Path.existsSync(project)) {
         project = __dirname + "/projects/" + project;
--- paths.js
+++
@@ -1,7 +0,0 @@
-require.paths.unshift(__dirname);
-require.paths.unshift(__dirname + "/lib");
-
-require.paths.unshift(__dirname + "/support/apf");
-require.paths.unshift(__dirname + "/support/async.js/lib");
-require.paths.unshift(__dirname + "/support/node-o3-xml/lib");
-require.paths.unshift(__dirname + "/support/node-o3-xml-v4/lib");
