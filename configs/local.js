dc6c0c5b0b8f7af144c3ae23af14f9f5e2359707
cadorn <christoph@christophdorn.com>
2012-05-03 12:32:49 -0700

    config overwrites for local version

--- configs/local.js
+++ configs/local.js
@@ -4,11 +4,7 @@ var config = require("./default");
 config.containers.master.plugins.forEach(function(plugin) {
     if (plugin.packagePath) {
         if (/\/cloud9.core$/.test(plugin.packagePath)) {
-            plugin.contexts = {
-                "location": {
-                    "local": true
-                }
-            };
+            plugin.socketIoTransports = ["websocket", "htmlfile", "xhr-multipart", "xhr-polling"];
         }
     }
 });
