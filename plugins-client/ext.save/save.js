c25046bdc3b1c095219bd7f9c5a62ffcbe5358bf
Vlad Zinculescu <vlad@c9.io>
2012-05-07 23:21:07 +0200

    fixed wrapAround button in search. moved save status button to the left

--- plugins-client/ext.save/save.js
+++ plugins-client/ext.save/save.js
@@ -144,7 +144,7 @@ module.exports = ext.register("ext/save/save", {
         });
 
         this.nodes.push(
-            menus.$insertByIndex(saveBar, new apf.button({
+            menus.$insertByIndex(barTools, new apf.button({
                 id       : "btnSave",
                 icon     : "save.png",
                 caption  : "Save",
