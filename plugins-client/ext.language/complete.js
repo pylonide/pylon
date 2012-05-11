155bc477003c6e436ed48f5071b55e14e9181868
Lennart Kats <lclkats@gmail.com>
2012-05-11 09:12:35 +0200

    Show non-inference completions in gray if inference completions available

--- plugins-client/ext.language/complete.js
+++ plugins-client/ext.language/complete.js
@@ -177,14 +177,24 @@ module.exports = {
         var _self = this;
         _self.completionElement.innerHTML = "";
         var cursorConfig = ceEditor.$editor.renderer.$cursorLayer.config;
+        var hasIcons = false;
+        matches.forEach(function(match) {
+            if (match.icon)
+                hasIcons = true;
+        });
         matches.forEach(function(match, idx) {
             var matchEl = dom.createElement("div");
             matchEl.className = idx === _self.selectedIdx ? "cc_complete_option_selected" : "cc_complete_option";
             var html = "";
             
-            if(match.icon)
+            if (match.icon)
                 html = "<img src='/static/ext/language/img/" + match.icon + ".png'/>";
-            html += "<span class='main'><u>" + _self.prefix + "</u>" + match.name.substring(_self.prefix.length);
+            if (!hasIcons || match.icon)
+                html += "<span class='main'><u>" + _self.prefix + "</u>" + match.name.substring(_self.prefix.length);
+            else {
+                html += "<span class='main'>" + match.name;
+                matchEl.style.color = "#666666";
+            }
             if(match.meta) {
                 html += '<span class="meta">' + match.meta + '</span>';
             }
@@ -201,6 +211,7 @@ module.exports = {
                 amlEditor.focus();
             });
             matchEl.style.height = cursorConfig.lineHeight + "px";
+            matchEl.style.color = 0xaaaaaa;
             _self.completionElement.appendChild(matchEl);
             _self.matchEls.push(matchEl);
         });
