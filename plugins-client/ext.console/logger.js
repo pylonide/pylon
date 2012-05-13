0d49612dcda22ae16bd1cf9b433d509449c346d6
matt <matt.pardee@gmail.com>
2012-05-13 15:02:49 -0700

    Prevent default and open ze file

--- plugins-client/ext.console/logger.js
+++ plugins-client/ext.console/logger.js
@@ -168,8 +168,11 @@ module.exports.logNodeStream = function(data, stream, useOutput, ide) {
     if (eventsAttached !== true) {
         parentEl.addEventListener("click", function(e) {
             var node = e.target;
-            if (node.hasAttribute("data-wsp"))
+            if (node.hasAttribute("data-wsp")) {
                 openLinkedFile.apply(null, e.target.getAttribute("data-wsp").split(","));
+                apf.preventDefault(e);
+                return false;
+            }
         });
         eventsAttached = true;
     }
