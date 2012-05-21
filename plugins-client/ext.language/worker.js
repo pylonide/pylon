29d8364fb28b85dbb37f4cdf6bfbab0ac770efee
Lennart Kats <lclkats@gmail.com>
2012-05-21 11:43:58 +0200

    Show function completion proposals before their property equivalents

--- plugins-client/ext.language/worker.js
+++ plugins-client/ext.language/worker.js
@@ -492,7 +492,13 @@ function asyncParForEach(array, fn, callback) {
                         return 1;
                     else if (a.score > b.score)
                         return -1;
-                    else if(a.name < b.name)
+                    else if (a.id && a.id === b.id) {
+                        if (a.isFunction)
+                            return -1;
+                        else if (b.isFunction)
+                            return 1;
+                    }
+                    if (a.name < b.name)
                         return -1;
                     else if(a.name > b.name)
                         return 1;
