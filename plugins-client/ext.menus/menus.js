d2b7e747830776b012e9ac5bb067f60a8921e2cb
Giannis <bone.jp@gmail.com>
2012-05-22 12:43:28 +0300

    User window.cloud9config.hosted instead of eventlistener

--- plugins-client/ext.menus/menus.js
+++ plugins-client/ext.menus/menus.js
@@ -158,14 +158,14 @@ module.exports = ext.register("ext/menus/menus", {
         }
         
         //update c9 main logo link
-        ide.addEventListener("init.ext/auth/auth", function(){
+        if(window.cloud9config.hosted) {
             var mainlogo = logobar.$ext.getElementsByClassName('mainlogo');
             if(mainlogo && (mainlogo = mainlogo[0])) {
                 mainlogo.title = "back to dashboard";
                 mainlogo.href = "/dashboard.html";
                 mainlogo.innerHTML = "Dashboard";
             }
-        });
+        }
     },
     
     $insertByIndex : function(parent, item, index) {
