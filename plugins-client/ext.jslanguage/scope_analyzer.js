b61daa4a228dcc6cb75ee15e0804a744c614c027
Lennart Kats <lclkats@gmail.com>
2012-05-29 17:18:44 +0200

    Add defined check
    Conflicts:
    	plugins-client/ext.jslanguage/scope_analyzer.js

--- plugins-client/ext.jslanguage/scope_analyzer.js
+++ plugins-client/ext.jslanguage/scope_analyzer.js
@@ -531,7 +531,7 @@ handler.analyze = function(doc, ast, callback) {
                 if (localVariables[i].uses.length === 0) {
                     var v = localVariables[i];
                     v.declarations.forEach(function(decl) {
-                        if (decl.value === decl.value.toUpperCase() || isRequireVarDecl(decl.parent))
+                        if ((decl.value && decl.value === decl.value.toUpperCase()) || isRequireVarDecl(decl.parent))
                             return;
                         markers.push({
                             pos: decl.getPos(),
