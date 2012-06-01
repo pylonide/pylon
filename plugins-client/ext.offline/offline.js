edaaca042b0d1bde185eac14e2aa0cf27ae3162e
Garen Torikian <gjtorikian@gmail.com>
2012-06-01 00:19:47 -0700

    Remove local storage additions

--- plugins-client/ext.offline/offline.js
+++ plugins-client/ext.offline/offline.js
@@ -274,9 +274,6 @@ module.exports = ext.register("ext/offline/offline", {
         });
 
         var ident = "cloud9.filetree." + ide.workspaceId;
-        function saveModel(){
-            localStorage[ident] = fs.model.data.xml;
-        }
 
         //@todo after being longer than 5 minutes offline reload tree when coming online
 
@@ -287,13 +284,7 @@ module.exports = ext.register("ext/offline/offline", {
                     fs.projectName = fs.model.queryValue("folder[@root='1']/@name");
                 }
             }
-            else {
-                saveModel();
-            }
         });
-
-        fs.model.addEventListener("update", saveModel);
-        fs.model.addEventListener("afterload", saveModel);
 
         //File contents
         /**
