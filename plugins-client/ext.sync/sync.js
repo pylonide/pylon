3dc39f559ec43a4b9e1b6cd108539505a9053547
cadorn <christoph@christophdorn.com>
2012-06-06 12:50:22 -0700

    working sync client plugin

--- plugins-client/ext.sync/sync.js
+++ plugins-client/ext.sync/sync.js
@@ -8,6 +8,7 @@
 define(function(require, exports, module) {
 
 var ext = require("core/ext");
+var ide = require("core/ide");
 
 module.exports = ext.register("ext/sync/sync", {
     dev    : "Ajax.org",
@@ -20,21 +21,35 @@ module.exports = ext.register("ext/sync/sync", {
     init : function(amlNode){
         var _self = this;
 
-        // TEMPORARY
+        ide.addEventListener("socketMessage", function (event) {
+            if (event.message.type === "sync") {
+                _self.handleMessage(event);
+            }
+        });
+
+        // TEMPORARY (REMOVE WHEN HOOKED UP TO SYNC ENABLE/DISABLE TOGGLE)
         window.tmpEnableSync = this.enableSync.bind(this);
         window.tmpDisableSync = this.disableSync.bind(this);
+    },
+ 
+    handleMessage : function(event) {
+        if (event.message.action === "notify") {
+
+console.log(event.message);
+
+        }
     },
 
     enableSync: function() {
         apf.ajax("/api/sync/enable", {
             method: "POST",
             data: "payload=" + encodeURIComponent(JSON.stringify({
-                workspaceId: window.cloud9config.workspaceId
+                workspaceId: ide.workspaceId
             })),
             async: true,
             callback: function( data, state ) {
                 if (state == apf.SUCCESS && JSON.parse(data).success === true) {
-                    // Success. Nothing more to do.
+                    // Success. Nothing more to do. (UI sync state will update via socket.io push event)
                 } else {
                     // TODO: Display error?
                 }
@@ -46,12 +61,12 @@ module.exports = ext.register("ext/sync/sync", {
         apf.ajax("/api/sync/disable", {
             method: "POST",
             data: "payload=" + encodeURIComponent(JSON.stringify({
-                workspaceId: window.cloud9config.workspaceId
+                workspaceId: ide.workspaceId
             })),
             async: true,
             callback: function( data, state ) {
                 if (state == apf.SUCCESS && JSON.parse(data).success === true) {
-                    // Success. Nothing more to do.
+                    // Success. Nothing more to do. (UI sync state will update via socket.io push event)
                 } else {
                     // TODO: Display error?
                 }
