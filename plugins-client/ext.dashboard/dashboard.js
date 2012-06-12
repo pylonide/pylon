8a9cf5feccaaafa82d44d08b0c2039d9bf985aa2
Vlad Zinculescu <vlad@c9.io>
2012-06-12 20:43:01 +0200

    dashboard: show dashboard popup

--- plugins-client/ext.dashboard/dashboard.js
+++ plugins-client/ext.dashboard/dashboard.js
@@ -36,7 +36,7 @@ module.exports = ext.register("ext/dashboard/dashboard", {
     init : function(amlNode){
         apf.importCssString(this.css || "");
 
-        profile.show();
+        dashboard.show();
     },
 
     enable : function(){
