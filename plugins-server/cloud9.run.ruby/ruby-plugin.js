51d931f106daaebf865e504f86983cb3655f7121
Jan Jongboom <janjongboom@gmail.com>
2012-06-25 15:46:40 +0200

    Remove log

--- plugins-server/cloud9.run.ruby/ruby-plugin.js
+++ plugins-server/cloud9.run.ruby/ruby-plugin.js
@@ -8,8 +8,6 @@ module.exports = function setup(options, imports, register) {
     RubyRunner.call(this, options.url, imports.vfs, pm, sandbox, false, function (err) {
         if (err) return register(err);
         
-        console.log("RUBY RUNNER CALLBACK");
-        
         register(null, {
             "run-ruby": {
                 Runner: RubyRunner.Runner
