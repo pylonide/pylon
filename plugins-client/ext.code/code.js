a875da1e078af8e6f96079828e85073dd7d1f44a
Lennart Kats <lclkats@gmail.com>
2012-06-26 12:46:13 +0200

    Change keybinding for record/play macro

--- plugins-client/ext.code/code.js
+++ plugins-client/ext.code/code.js
@@ -460,6 +460,12 @@ module.exports = ext.register("ext/code/code", {
                 editor.afterOpenFile(editor.getSession());
         });
 
+        // Override ACE key bindings (conflict with goto definition)
+        commands.commands.togglerecording.bindKey = { mac: "Command-Shift-R", win: "Ctrl-Shift-R" };
+        commands.commands.replaymacro.bindKey = { mac: "Command-Ctrl-R", win: "Ctrl-Alt-R" };
+        commands.addCommand(commands.commands.togglerecording);
+        commands.addCommand(commands.commands.replaymacro);
+
         c = 20000;
         this.menus.push(
             menus.addItemByPath("Tools/~", new apf.divider(), c += 100),
