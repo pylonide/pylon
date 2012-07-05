0800338250b622f587a69a109b4d770d53c81f4d
Lennart Kats <lclkats@gmail.com>
2012-07-05 10:21:06 +0200

    Merge base_handler.js from refactor/jvm

--- plugins-client/ext.language/base_handler.js
+++ plugins-client/ext.language/base_handler.js
@@ -33,7 +33,7 @@ module.exports = {
     /**
      * If the language handler implements parsing, this function should take
      * the source code and turn it into an AST (in treehugger format)
-     * @param code code to parse
+     * @param doc the Document object repersenting the source
      * @return treehugger AST or null if not implemented
      */
     parse: function(doc, callback) {
@@ -49,16 +49,8 @@ module.exports = {
     },
     
     /**
-     * Invoked on a successful parse
-     * @param ast the resulting AST in treehugger format
-     */
-    onParse: function(ast, callback) {
-        callback();
-    },
-    
-    /**
      * Invoked when the document has been updated (possibly after a certain interval)
-     * @param doc the document object
+     * @param doc the Document object repersenting the source
      */
     onUpdate: function(doc, callback) {
         callback();
@@ -67,10 +59,11 @@ module.exports = {
     /**
      * Invoked when a new document has been opened
      * @param path the path of the newly opened document
-     * @param doc the document object
+     * @param doc the Document object repersenting the source
      * @param oldPath the path of the document that was active before
      */
     onDocumentOpen: function(path, doc, oldPath, callback) {
+        this.refactorInProgress = false;
         callback();
     },
     
@@ -79,11 +72,16 @@ module.exports = {
      * @param path the path of the file
      */
     onDocumentClose: function(path, callback) {
+        this.refactorInProgress = false;
         callback();
     },
     
     /**
      * Invoked when the cursor has been moved inside to a different AST node
+     * @param doc the Document object repersenting the source
+     * @param fullAst the entire AST of the current file (if exists)
+     * @param cursorPos the current cursor position (object with keys 'row' and 'column')
+     * @param currentNode the AST node the cursor is currently at (if exists)
      * @return a JSON object with three optional keys: {markers: [...], hint: {message: ...}, enableRefactoring: [...]}
      */
     onCursorMovedNode: function(doc, fullAst, cursorPos, currentNode, callback) {
@@ -92,9 +90,22 @@ module.exports = {
     
     /**
      * Invoked when an outline is required
+     * @param doc the Document object repersenting the source
+     * @param fullAst the entire AST of the current file (if exists)
      * @return a JSON outline structure or null if not supported
      */
-    outline: function(doc, ast, callback) {
+    outline: function(doc, fullAst, callback) {
+        callback();
+    },
+
+    /**
+     * Invoked when an type hierarchy is required
+     * (either for a selected element, or the enclosing element)
+     * @param doc the Document object repersenting the source
+     * @param cursorPos the current cursor position (object with keys 'row' and 'column')
+     * @return a JSON hierarchy structure or null if not supported
+     */
+    hierarchy: function(doc, cursorPos, callback) {
         callback();
     },
     
@@ -107,13 +118,13 @@ module.exports = {
     
     /**
      * Performs code completion for the user based on the current cursor position
-     * @param doc the entire source code as string
-     * @param fullAst the entire AST of the current file
-     * @param data contains pos, the current cursor position (object with keys 'row' and 'column')
-     *        and staticPrefix, the current loading URL
-     * @param currentNode the AST node the cursor is currently at
+     * @param doc the Document object repersenting the source
+     * @param fullAst the entire AST of the current file (if exists)
+     * @param cursorPos the current cursor position (object with keys 'row' and 'column')
+     * @param currentNode the AST node the cursor is currently at (if exists)
+     * @return an array of completion matches
      */
-    complete: function(doc, fullAst, data, currentNode, callback) {
+    complete: function(doc, fullAst, cursorPos, currentNode, callback) {
         callback();
     },
 
@@ -126,6 +137,9 @@ module.exports = {
     
     /**
      * Enables the handler to do analysis of the AST and annotate as desired
+     * @param doc the Document object repersenting the source
+     * @param fullAst the entire AST of the current file (if exists)
+     * @return an array of error and warning markers
      */
     analyze: function(doc, fullAst, callback) {
         callback();
@@ -133,9 +147,51 @@ module.exports = {
     
     /**
      * Invoked when inline variable renaming is activated
-     * @return an array of positions of the currently selected variable
+     * @param doc the Document object repersenting the source
+     * @param fullAst the entire AST of the current file (if exists)
+     * @param pos the current cursor position (object with keys 'row' and 'column')
+     * @param currentNode the AST node the cursor is currently at (if exists)
+     * @return an object with the main occurence and array of other occurences of the selected element
      */
     getVariablePositions: function(doc, fullAst, pos, currentNode, callback) {
+        callback();
+    },
+
+    /**
+     * Invoked when refactoring is started -> So, for java, saving the file is no more legal to do
+     * @param doc the Document object repersenting the source
+     * @param oldId the old identifier wanted to be refactored
+     */
+    onRenameBegin: function(doc) {
+      this.refactorInProgress = true;
+    },
+
+    /**
+     * Invoked when a refactor request is being finalized and waiting for a status
+     * @param doc the Document object repersenting the source
+     * @param oldId the old identifier wanted to be refactored
+     * @param newName the new name of the element after refactoring
+     * @return boolean indicating whether to progress or an error message if refactoring failed
+     */
+    commitRename: function(doc, oldId, newName, callback) {
+        this.refactorInProgress = false;
+        callback();
+    },
+
+    /**
+     * Invoked when a refactor request is cancelled
+     */
+    onRenameCancel: function(callback) {
+        this.refactorInProgress = false;
+        callback();
+    },
+
+    /**
+     * Invoked when an automatic code formating is wanted
+     * @param doc the Document object repersenting the source
+     * @return a string value representing the new source code after formatting or null if not supported
+     */
+    codeFormat: function(doc, callback) {
         callback();
     }
 };
