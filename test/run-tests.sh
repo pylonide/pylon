7dc61bbb69079fb8f6420cf3c2d37d1b4f00d709
Bas <bas@c9.io>
2012-08-08 15:05:10 +0300

    Update test/run-tests.sh

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -4,15 +4,17 @@ npm install amd-loader
 BLACKLIST=`cat test/blacklist.txt`
 for F in `find . -name '*_test.js' | grep -v backup- | grep -v node_modules`; do
   BLACKLISTED=
-  echo $F
   for G in $BLACKLIST; do
     if [ "$F" == "$G" ]; then
       BLACKLISTED=1
     fi
   done
   if ! [ $BLACKLISTED ]; then
-    echo -------------------------------
     echo $F
+    echo ----
     node $F
+    echo ----
+  else
+    echo $F SKIPPED
   fi
 done
