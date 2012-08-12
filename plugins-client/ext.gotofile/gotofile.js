810ac9f8624ba0ade3ffa686985a0f7d68e1cb09
Lennart Kats <lclkats@gmail.com>
2012-08-12 12:07:27 +0200

    Remove guard to fix focus when opening gotofile

--- plugins-client/ext.gotofile/gotofile.js
+++ plugins-client/ext.gotofile/gotofile.js
@@ -338,10 +338,8 @@ module.exports = ext.register("ext/gotofile/gotofile", {
             vp.change(0, vp.limit, true);
             
             setTimeout(function(){
-                if (!dgGoToFile.selected) {
-                    dgGoToFile.select(dgGoToFile.getFirstTraverseNode())
-                    txtGoToFile.focus();
-                }
+                dgGoToFile.select(dgGoToFile.getFirstTraverseNode())
+                txtGoToFile.focus();
             });
         }
         
