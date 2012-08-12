13d63c92afa5987435c56d8c8a7b25d7394d05c0
Lennart Kats <lclkats@gmail.com>
2012-08-12 11:55:02 +0200

    Handle failed process kills

--- plugins-server/cloud9.ide.run-python/run-python.js
+++ plugins-server/cloud9.ide.run-python/run-python.js
@@ -124,9 +124,10 @@ util.inherits(PythonRuntimePlugin, Plugin);
     };
 
     this.$kill = function(pid, message, client) {
+        var self = this;
         this.pm.kill(pid, function(err) {
             if (err)
-                return this.error(err, 1, message, client);
+                return self.error(err, 1, message, client);
         });
     };
 
