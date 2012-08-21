e2e450f4caa0f977b1543a5fbad7883f2a12d654
Giannis <bone.jp@gmail.com>
2012-08-21 14:20:52 +0300

    * fix for gotofile * fix "gotofile popup corruption" * fixes ajaxorg/cloud9infra#2441

--- plugins-client/ext.gotofile/gotofile.js
+++ plugins-client/ext.gotofile/gotofile.js
@@ -325,8 +325,10 @@ module.exports = ext.register("ext/gotofile/gotofile", {
                 dgGoToFile.clear("loading")
                 this.filter("");
             }
-            else
+            else {
+                dgGoToFile.removeAttribute("height");
                 dgGoToFile.clear("empty");
+            }
         }
         else {
             dgGoToFile.$removeClearMessage();
@@ -348,7 +350,8 @@ module.exports = ext.register("ext/gotofile/gotofile", {
         }
         
         if (!vp.length) {
-            dgGoToFile.setAttribute('height', 18);
+            if (this.arraySearchResults.length)
+                dgGoToFile.setAttribute("height", 18);
         }
         else if (vp.length < 100) {
             var sh = vp.getScrollHeight();
@@ -357,10 +360,10 @@ module.exports = ext.register("ext/gotofile/gotofile", {
             
             var ht = Math.min(350, sh);
             if (ht != dgGoToFile.height)
-                dgGoToFile.setAttribute('height', ht);
+                dgGoToFile.setAttribute("height", ht);
         }
         else if (dgGoToFile.height != 350) {
-            dgGoToFile.setAttribute('height', 350);
+            dgGoToFile.setAttribute("height", 350);
         }
     },
     
