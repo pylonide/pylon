0b24fe8a66e5c4ebe1978f8ed07b4f70e0ea711f
Mostafa Eweda <mostafa.eweda17@gmail.com>
2012-09-03 13:56:07 +0200

    Remove processTypes - only treat supported runners based on their message type string format

--- plugins-client/ext.console/console.js
+++ plugins-client/ext.console/console.js
@@ -120,7 +120,7 @@ module.exports = ext.register("ext/console/console", {
                 }
             }
             else {
-                command_id = this.createNodeProcessLog(spi);
+                command_id = this.createProcessLog(spi);
             }
 
             this.tracerToPidMap[command_id] = spi;
@@ -403,8 +403,8 @@ module.exports = ext.register("ext/console/console", {
         }
     },
 
-    createNodeProcessLog: function(message_pid) {
-        var command_id = this.createOutputBlock("Running Node Process", true);
+    createProcessLog: function(message_pid, lang) {
+        var command_id = this.createOutputBlock("Running " + lang + " Process", true);
         this.tracerToPidMap[command_id] = message_pid;
         this.pidToTracerMap[message_pid] = command_id;
     
@@ -412,58 +412,6 @@ module.exports = ext.register("ext/console/console", {
         containerEl.setAttribute("rel", command_id);
         apf.setStyleClass(containerEl, "has_pid");
     
-        this.command_id_tracer++;
-        return command_id;
-    },
-
-    createPhpProcessLog : function(message_pid) {
-        var command_id = this.createOutputBlock("Running PHP Process", true);
-        this.tracerToPidMap[command_id] = message_pid;
-        this.pidToTracerMap[message_pid] = command_id;
-
-        var containerEl = this.getLogStreamOutObject(command_id).$ext;
-        containerEl.setAttribute("rel", command_id);
-        apf.setStyleClass(containerEl, "has_pid");
-
-        this.command_id_tracer++;
-        return command_id;
-    },
-    
-    createApacheProcessLog: function (message_pid) {
-        var command_id = this.createOutputBlock("Running Apache Process", true);
-        this.tracerToPidMap[command_id] = message_pid;
-        this.pidToTracerMap[message_pid] = command_id;
-
-        var containerEl = this.getLogStreamOutObject(command_id).$ext;
-        containerEl.setAttribute("rel", command_id);
-        apf.setStyleClass(containerEl, "has_pid");
-
-        this.command_id_tracer++;
-        return command_id;
-    },
-
-    createPythonProcessLog : function(message_pid) {
-        var command_id = this.createOutputBlock("Running Python Process", true);
-        this.tracerToPidMap[command_id] = message_pid;
-        this.pidToTracerMap[message_pid] = command_id;
-
-        var containerEl = this.getLogStreamOutObject(command_id).$ext;
-        containerEl.setAttribute("rel", command_id);
-        apf.setStyleClass(containerEl, "has_pid");
-
-        this.command_id_tracer++;
-        return command_id;
-    },
-
-    createRubyProcessLog : function(message_pid) {
-        var command_id = this.createOutputBlock("Running Ruby Process", true);
-        this.tracerToPidMap[command_id] = message_pid;
-        this.pidToTracerMap[message_pid] = command_id;
-
-        var containerEl = this.getLogStreamOutObject(command_id).$ext;
-        containerEl.setAttribute("rel", command_id);
-        apf.setStyleClass(containerEl, "has_pid");
-
         this.command_id_tracer++;
         return command_id;
     },
@@ -485,80 +433,67 @@ module.exports = ext.register("ext/console/console", {
                 this.command_id_tracer = extra.command_id + 1;
         }
 
+        function capitalise (word) {
+            return word[0].toUpperCase() + word.substring(1);
+        }
+
+        var lang;
+        if ((lang = /^(\w+)-start$/.exec(message.type))) {
+            lang = capitalise(lang[1]);
+            var clearOnRun = settings.model.queryValue("auto/console/@clearonrun");
+            if (apf.isTrue(clearOnRun) && window["txtOutput"])
+                txtOutput.clear();
+
+            this.createProcessLog(message.pid, lang);
+            return;
+        } else if ((lang = /^(\w+)-data$/.exec(message.type))) {
+            if (message.data && message.data.indexOf("Tip: you can") === 0) {
+                (function () {
+                    var prjmatch = message.data.match(/http\:\/\/([\w_-]+)\.([\w_-]+)\./);
+                    if (!prjmatch) return;
+
+                    var user = prjmatch[2];
+                    var project = prjmatch[1];
+
+                    var urlPath = window.location.pathname.split("/").filter(function (f) { return !!f; });
+
+                    if (project !== ide.projectName) {
+                        // concurrency bug, project does not match
+                        apf.ajax("/api/debug", {
+                            method: "POST",
+                            contentType: "application/json",
+                            data: JSON.stringify({
+                                agent: navigator.userAgent,
+                                type: "Concurrency bug, project does not match",
+                                e: [user, project, urlPath],
+                                workspaceId: ide.workspaceId
+                            })
+                        });
+                    }
+                    else if (urlPath.length && user !== urlPath[0]) {
+                        // concurrency bug, user does not match
+                        apf.ajax("/api/debug", {
+                            method: "POST",
+                            contentType: "application/json",
+                            data: JSON.stringify({
+                                agent: navigator.userAgent,
+                                type: "Concurrency bug, user does not match",
+                                e: [user, project, urlPath],
+                                workspaceId: ide.workspaceId
+                            })
+                        });
+                    }
+                }());
+            }
+
+            logger.logNodeStream(message.data, message.stream, this.getLogStreamOutObject(message.pid, true), ide);
+            return;
+        } else if ((lang = /^(\w+)-exit$/.exec(message.type))) {
+            this.markProcessAsCompleted(message.pid, true);
+            return;
+        }
+
         switch (message.type) {
-            case "node-start":
-                var clearOnRun = settings.model.queryValue("auto/console/@clearonrun");
-                if (apf.isTrue(clearOnRun) && window["txtOutput"]) txtOutput.clear();
-                this.createNodeProcessLog(message.pid);
-                return;
-            case "php-start":
-                this.createPhpProcessLog(message.pid);
-                return;
-            case "apache-start":
-                this.createApacheProcessLog(message.pid);
-                return;
-            case "python-start":
-                this.createPythonProcessLog(message.pid);
-                return;
-            case "ruby-start":
-                this.createRubyProcessLog(message.pid);
-                return;
-            case "node-data":
-            case "apache-data":
-            case "php-data":
-            case "python-data":
-            case "ruby-data":
-            case "php-data":            
-                if (message.data && message.data.indexOf("Tip: you can") === 0) {
-                    (function () {
-                        var prjmatch = message.data.match(/http\:\/\/([\w_-]+)\.([\w_-]+)\./);
-                        if (!prjmatch) return;
-                        
-                        var user = prjmatch[2];
-                        var project = prjmatch[1];
-                        
-                        var urlPath = window.location.pathname.split("/").filter(function (f) { return !!f; });
-                        
-                        if (project !== ide.projectName) {
-                            // concurrency bug, project does not match
-                            apf.ajax("/api/debug", {
-                                method: "POST",
-                                contentType: "application/json",
-                                data: JSON.stringify({
-                                    agent: navigator.userAgent,
-                                    type: "Concurrency bug, project does not match",
-                                    e: [user, project, urlPath],
-                                    workspaceId: ide.workspaceId
-                                })
-                            });
-                        }
-                        else if (urlPath.length && user !== urlPath[0]) {
-                            // concurrency bug, user does not match
-                            apf.ajax("/api/debug", {
-                                method: "POST",
-                                contentType: "application/json",
-                                data: JSON.stringify({
-                                    agent: navigator.userAgent,
-                                    type: "Concurrency bug, user does not match",
-                                    e: [user, project, urlPath],
-                                    workspaceId: ide.workspaceId
-                                })
-                            });
-                        }
-                        
-                        return;
-                    }());
-                }
-                
-                logger.logNodeStream(message.data, message.stream, this.getLogStreamOutObject(message.pid, true), ide);
-                return;
-            case "node-exit":
-            case "php-exit":
-            case "python-exit":
-            case "ruby-exit":
-            case "apache-exit":
-                this.markProcessAsCompleted(message.pid, true);
-                return;
             case "npm-module-start":
                 if (!extra.original_line || !this.inited)
                     return;
