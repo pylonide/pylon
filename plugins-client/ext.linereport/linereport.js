43be9b727e39aec37f6a744888d08760b23a16e3
Lennart Kats <lclkats@gmail.com>
2012-09-24 09:38:19 +0200

    Add extension wrapping, parser submodule

--- plugins-client/ext.linereport/linereport.js
+++ plugins-client/ext.linereport/linereport.js
@@ -7,31 +7,43 @@
  
 define(function(require, exports, module) {
 
-module.exports = {
-    
-    parseOutputLines: function(output) {
-        var lines = output.split("\n");
-        var results = [];
-        for (var i = 0; i < lines.length; i++) {
-            var match = lines[i].match(/^\s*(\d+):\s*(\d+):\s*(.*)/);
-            if (!match)
-                continue;
-            var warningMatch = match[3].match(/^\swarning:?\s*(.*)/i);
-            var errorMatch = match[3].match(/^\error:?\s*(.*)/i);
-            var infoMatch = match[3].match(/^\info:?\s*(.*)/i);
-            results.push({
-                pos: { sl: parseInt(match[1], 10), sc: parseInt(match[2], 10) },
-                type: 'unused',
-                level: warningMatch ? 'warning' : infoMatch ? 'info' : 'error',
-                message: warningMatch ? warningMatch[1] :
-                         infoMatch ? infoMatch[1] :
-                         errorMatch ? errorMatch[1] :
-                         match[3]
-            });
-        }
-        return results;
-    }
+var ide = require("core/ide");
+var ext = require("core/ext");
+var parser = require("ext/linereport/linereport");
 
-};
+module.exports = ext.register("ext/linereport/linereport", {
+    name     : "linereport",
+    dev      : "Ajax.org",
+    alone    : true,
+    type     : ext.GENERAL,
+    disabled : false,
+    nodes : [],
+    
+    $commandId: 0,
+    
+    callbacks: {},
+    
+    hook: function() { // TODO
+        ide.addEventListener("socketMessage", this.onMessage.bind(this));
+    },
+    
+    onMessage: function(e) { // TODO
+    
+    },
+    
+    initReporter: function(command, callback) {
+        // For now, this is an alias of invokeReporter
+        this.invokeReporter(command, callback);
+    },
+
+    invokeReporter: function(command, callback) {
+        
+    },
+    
+    parseReporterOutput: function(output) {
+        parser.parseOutput(output);
+    }
+    
+});
 
 });
