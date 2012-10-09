cbea2c6191304e48895e533ee6a18295b419ffb9
Lennart Kats <lclkats@gmail.com>
2012-10-09 14:55:26 +0200

    Mark obj["propAccess"] as info not warning

--- plugins-client/ext.jslanguage/jshint.js
+++ plugins-client/ext.jslanguage/jshint.js
@@ -51,6 +51,8 @@ handler.analyzeSync = function(doc, ast) {
                 type = "error";
             if (reason.indexOf("Missing semicolon") !== -1)
                 type = "info";
+            if (reason.indexOf("better written in dot") !== -1)
+                type = "info";
             if (reason.indexOf("used out of scope") !== -1)
                 type = "info";
             if (reason.indexOf("conditional expression and instead saw an assignment") !== -1) {
