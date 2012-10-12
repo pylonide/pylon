d59fdcb80a07312b93b62d678083c5a92ef6f54a
Lennart Kats <lclkats@gmail.com>
2012-10-12 13:54:08 +0200

    Remove jumptodef reference

--- configs/default.js
+++ configs/default.js
@@ -141,8 +141,7 @@ var config = [
             "ext/autosave/autosave",
             "ext/revisions/revisions",
             "ext/quicksearch/quicksearch",
-            "ext/language/liveinspect",
-            "ext/jumptodef/jumptodef"
+            "ext/language/liveinspect"
             //"ext/splitview/splitview"
             //"ext/minimap/minimap"
             //"ext/acebugs/acebugs"
