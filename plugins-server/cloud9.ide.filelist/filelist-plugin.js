b9535b65b63aff0d3bbe976f9a845a6fa6b01737
mikedeboer <info@mikedeboer.nl>
2012-10-24 10:32:16 +0000

    filelist plugin now uses vfs directly instead of process manager. Included unit test

--- plugins-server/cloud9.ide.filelist/filelist-plugin.js
+++ plugins-server/cloud9.ide.filelist/filelist-plugin.js
@@ -16,86 +16,81 @@ var error = require("http-error");
 
 var name = "filelist";
 
-var ProcessManager, EventBus, IdeRoutes, Permissions, Filelist;
-
 module.exports = function setup(options, imports, register) {
-    Filelist = new FilelistLib();
+    var Filelist = new FilelistLib();
     Filelist.setEnv({
         findCmd: options.findCmd,
         platform: options.platform
     });
 
-    ProcessManager = imports["process-manager"];
-    EventBus = imports.eventbus;
-    IdeRoutes = imports["ide-routes"];
-    Permissions = imports["workspace-permissions"];
-    imports.ide.register(name, FilelistPlugin, register);
-};
+    var Vfs = imports["vfs"];
+    var IdeRoutes = imports["ide-routes"];
+    var Permissions = imports["workspace-permissions"];
 
-var FilelistPlugin = function(ide, workspace) {
-    Plugin.call(this, ide, workspace);
-    this.name = name;
-    this.processCount = 0;
-    this.ws = ide.workspace.workspaceId;
+    var FilelistPlugin = function(ide, workspace) {
+        Plugin.call(this, ide, workspace);
+        this.name = name;
+        this.processCount = 0;
+        this.ws = ide.workspace.workspaceId;
 
-    Filelist.setEnv({
-        workspaceId: this.ws,
-        basePath: ide.workspace.workspaceDir
-    });
+        Filelist.setEnv({
+            workspaceId: this.ws,
+            basePath: ide.workspace.workspaceDir
+        });
 
-    // set up some routes as well
-    var self = this;
-    IdeRoutes.use("/fs", Connect.router(function(app) {
-        app.get("/list", self.getList.bind(self));
-    }));
-};
+        // set up some routes as well
+        var self = this;
+        IdeRoutes.use("/fs", Connect.router(function(app) {
+            app.get("/list", self.getList.bind(self));
+        }));
+    };
 
-util.inherits(FilelistPlugin, Plugin);
+    util.inherits(FilelistPlugin, Plugin);
 
-(function() {
+    (function() {
 
-    this.getList = function(req, res, next) {
-        var uid = req.session.uid || req.session.anonid;
-        if (!uid)
-            return next(new error.Unauthorized());
+        this.getList = function(req, res, next) {
+            var uid = req.session.uid || req.session.anonid;
+            if (!uid)
+                return next(new error.Unauthorized());
 
-        // does this user has read-permissions...
-        Permissions.getPermissions(uid, this.ws, "fs_filelist", function(err, perms) {
-            if (err)
-                return next(err);
+            // does this user has read-permissions...
+            Permissions.getPermissions(uid, this.ws, "fs_filelist", function(err, perms) {
+                if (err)
+                    return next(err);
 
-            if (perms.fs.indexOf("r") === -1)
-                return next(new error.Forbidden("You are not allowed to view this resource"));
+                if (perms.fs.indexOf("r") === -1)
+                    return next(new error.Forbidden("You are not allowed to view this resource"));
 
-            // and kick off the download action!
-            var headersSent = false;
-            var query = Url.parse(req.url, true).query;
-            query.showHiddenFiles = !!parseInt(query.showHiddenFiles, 10);
+                // and kick off the download action!
+                var headersSent = false;
+                var query = Url.parse(req.url, true).query;
+                query.showHiddenFiles = !!parseInt(query.showHiddenFiles, 10);
 
-            Filelist.exec(query, ProcessManager, EventBus,
-                // start
-                function() {
-                    // skip it!
-                },
-                // incoming data
-                function(msg) {
-                    if (msg.stream != "stderr") {
+                Filelist.exec(query, Vfs,
+                    // incoming data
+                    function(msg) {
+                        if (!msg)
+                            return;
+
                         if (!headersSent) {
                             res.writeHead(200, { "content-type": "text/plain" });
                             headersSent = true;
                         }
-                        res.write(msg.data);
+                        res.write(msg);
+                    },
+                    // process exit
+                    function(code, stderr) {
+                        if (code === 0)
+                            res.end();
+                        else
+                            next("Process terminated with code " + code + ", " + stderr);
                     }
-                },
-                // process exit
-                function(code, stderr) {
-                    if (code === 0)
-                        res.end();
-                    else
-                        next("Process terminated with code " + code + ", " + stderr);
-                }
-            );
-        });
-    };
+                );
+            });
+        };
 
-}).call(FilelistPlugin.prototype);
+    }).call(FilelistPlugin.prototype);
+
+    imports.ide.register(name, FilelistPlugin, register);
+};
