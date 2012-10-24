a4cc595cf006809aff8f380af8970336385a57e6
mikedeboer <info@mikedeboer.nl>
2012-10-24 10:44:36 +0000

    finished filelist test and added it to the blacklist until Jenkins supports mocha tests

--- plugins-server/cloud9.ide.filelist/filelist_test.js
+++ plugins-server/cloud9.ide.filelist/filelist_test.js
@@ -50,4 +50,48 @@ describe("filelist", function() {
             }
         );
     });
+
+    it("should get filelist, without hidden files",  function(next) {
+        var out = "";
+
+        o.exec({
+                path: "",
+                showHiddenFiles: false
+            }, vfs,
+            // data
+            function(data) {
+                out += data;
+            },
+            // exit
+            function(code, stderr) {
+                Assert.equal(code, 0);
+                var files = out.split("\n").filter(function(file) { return !!file; }).sort();
+                Assert.equal(files[2], "./level1/level2/level2.rb");
+                Assert.equal(files[3], "./level1/level2/level3/level4/level4.txt");
+
+                next();
+            }
+        );
+    });
+
+    it("should not be possible to get a filelist from outside the root path ",  function(next) {
+        var out = "";
+
+        o.exec({
+                path: "/../../../../../etc",
+                showHiddenFiles: true
+            }, vfs,
+            // data
+            function(data) {
+                out += data;
+            },
+            // exit
+            function(code, stderr) {
+                Assert.equal(code, 1);
+                Assert.equal(stderr, "Invalid path");
+
+                next();
+            }
+        );
+    });
 });
