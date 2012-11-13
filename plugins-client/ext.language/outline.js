pylonide/pylon

commit 683c8f72bed6190dfe7e95343b071f9fafd5f0ee

removed ceEditor refs

plugins-client/ext.language/outline.js
@@ -42,7 +42,7 @@ module.exports = {
             hint: "search for a definition and jump to it",
             bindKey: {mac: "Command-Shift-E", win: "Ctrl-Shift-E"},
             isAvailable : function(editor) {
-                return editor && editor.ceEditor;
+                return editor && editor.path == "ext/code/code";
             },
             exec: function () {
                 _self.updateOutline(true);
@@ -159,11 +159,11 @@ module.exports = {
         }
         
         var editor = editors.currentEditor;
-        if (!editor || !editor.ceEditor)
+        if (!editor || editor.path != "ext/code/code" || !editor.amlEditor)
             return;
         
         this.fullOutline = event.data.body;
-        var ace = editor.ceEditor.$editor;
+        var ace = editor.amlEditor.$editor;
         var cursor = ace.getCursorPosition();
         this.$originalLine = cursor.row + 1;
         this.$originalColumn = cursor.column;
@@ -223,7 +223,7 @@ module.exports = {
     
     renderOutline: function(ignoreFilter) {
         var editor = editors.currentEditor;
-        if (!editor || !editor.ceEditor)
+        if (!editor || editor.path != "ext/code" || !editor.amlEditor)
             return;
             
         ext.initExtension(gotofile);
@@ -240,7 +240,7 @@ module.exports = {
             treeOutline.$removeClearMessage();
         */
 
-        var ace = editor.ceEditor.$editor;
+        var ace = editor.amlEditor.$editor;
         var selected = this.findCursorInOutline(outline, ace.getCursorPosition());
         mdlOutline.load(apf.getXml('<data>' + this.outlineJsonToXml(outline, selected, 'entries') + '</data>'));
         return selected;
@@ -302,7 +302,7 @@ module.exports = {
         if (e.keyCode === 27) { // Escape
             if (this.$originalLine) {
                 var editor = editors.currentEditor;
-                var ace = editor.ceEditor.$editor;
+                var ace = editor.amlEditor.$editor;
                 ace.gotoLine(this.$originalLine, this.$originalColumn, apf.isTrue(settings.model.queryValue("editors/code/@animatedscroll")));
                 
                 delete this.$originalLine;

plugins-client/ext.beautify/beautify.js
@@ -37,8 +37,8 @@ module.exports = ext.register("ext/beautify/beautify", {
         if (!editor)
             editor = editors.currentEditor;
 
-        if (editor.ceEditor)
-            editor = editor.ceEditor.$editor;
+        if (editor.amlEditor)
+            editor = editor.amlEditor.$editor;
 
         var sel = editor.selection;
         var session = editor.session;
@@ -129,8 +129,8 @@ module.exports = ext.register("ext/beautify/beautify", {
             msg: "Beautifying selection.",
             bindKey: {mac: "Command-Shift-B", win: "Shift-Ctrl-B"},
             isAvailable : function(editor){
-                if (editor && editor.ceEditor) {
-                    var range = editor.ceEditor.$editor.getSelectionRange();
+                if (editor && editor.amlEditor) {
+                    var range = editor.amlEditor.$editor.getSelectionRange();
                     return range.start.row != range.end.row
                       || range.start.column != range.end.column;
                 }

plugins-client/ext.clipboard/clipboard.js
@@ -148,11 +148,11 @@ module.exports = ext.register("ext/clipboard/clipboard", {
 
     $getAce : function() {
         var editor = editors.currentEditor;
-        if (!editor || !editor.ceEditor)
+        if (!editor || !editor.amlEditor)
             return;
 
-        var ceEditor = editor.ceEditor;
-        return ceEditor.$editor;
+        var amlEditor = editor.amlEditor;
+        return amlEditor.$editor;
     },
     
     enable : function(){

plugins-client/ext.code/code.js
@@ -111,6 +111,7 @@ Object.keys(SupportedModes).forEach(function(name) {
 
 module.exports = ext.register("ext/code/code", {
     name    : "Code Editor",
+    extName : "ext/code/code",
     dev     : "Ajax.org",
     type    : ext.EDITOR,
     markup  : markup,
@@ -212,7 +213,8 @@ module.exports = ext.register("ext/code/code", {
 
             apf.xmldb.setAttribute(file, "customtype", value);
             fileExtensions[ext] = value;
-        } else {
+        }
+        else {
             apf.xmldb.removeAttribute(file, "customtype", "");
 
             delete fileExtensions[ext];
@@ -221,7 +223,7 @@ module.exports = ext.register("ext/code/code", {
                     fileExtensions[ext] = mode;
                     break;
                 }
-        }
+            }
         }
 
         var mime = this.setCustomType(dotI ? ext : file, value);
@@ -232,8 +234,8 @@ module.exports = ext.register("ext/code/code", {
             mime: mime,
             customType: value
         });
-        if (self.ceEditor)
-            self.ceEditor.setAttribute("syntax", this.getSyntax(file));
+        if (this.amlEditor)
+            this.amlEditor.setAttribute("syntax", this.getSyntax(file));
     },
 
     getContentType : function(node) {
@@ -259,16 +261,16 @@ module.exports = ext.register("ext/code/code", {
     setDocument : function(doc, actiontracker, isLazy){
         var _self = this;
 
-        var ceEditor = this.amlEditor;
+        var amlEditor = this.amlEditor;
 
         if (doc.acesession) {
-            ceEditor.setProperty("value", doc.acesession);
+            amlEditor.setProperty("value", doc.acesession);
         }
         else {
             doc.isInited = doc.hasValue();
             doc.acedoc = doc.acedoc || new ProxyDocument(new Document(doc.getValue() || ""));
             var syntax = _self.getSyntax(doc.getNode());
-            var mode = ceEditor.getMode(syntax);
+            var mode = amlEditor.getMode(syntax);
             doc.acesession = new EditSession(doc.acedoc, mode);
             doc.acesession.syntax = syntax;
             doc.acedoc = doc.acesession.getDocument();
@@ -288,7 +290,6 @@ module.exports = ext.register("ext/code/code", {
 
                 doc.acesession.setValue(e.value || "");
 
-
                 if (doc.state)
                     _self.setState(doc, doc.state);
 
@@ -297,15 +298,15 @@ module.exports = ext.register("ext/code/code", {
                 if (this.$page.id != this.$page.parentNode.activepage)
                     return;
 
-                ceEditor.setAttribute("syntax", syntax);
-                ceEditor.setAttribute("value", doc.acesession);
+                amlEditor.setAttribute("syntax", syntax);
+                amlEditor.setAttribute("value", doc.acesession);
                 // force tokenize first visible rows
                 var rowCount = Math.min(50, doc.acesession.getLength());
                 doc.acesession.bgTokenizer.getTokens(0, rowCount);
             });
 
             if (!isLazy)
-                ceEditor.setProperty("value", doc.acesession || "");
+                amlEditor.setProperty("value", doc.acesession || "");
 
             doc.addEventListener("retrievevalue", function(e) {
                 if (this.editor != _self || !doc)
@@ -452,8 +453,8 @@ module.exports = ext.register("ext/code/code", {
             };
 
             command.findEditor = function(editor) {
-                if (editor && editor.ceEditor)
-                    return editor.ceEditor.$editor;
+                if (editor && editor.amlEditor)
+                    return editor.amlEditor.$editor;
                 return editor;
             };
         };
@@ -604,7 +605,7 @@ module.exports = ext.register("ext/code/code", {
             menus.addItemByPath("View/Syntax/", new apf.menu({
                 "onprop.visible" : function(e){
                     if (e.value) {
-                        if (!editors.currentEditor || !editors.currentEditor.ceEditor)
+                        if (!editors.currentEditor || !editors.currentEditor.amlEditor)
                             this.disable();
                         else {
                             this.enable();
@@ -662,7 +663,7 @@ module.exports = ext.register("ext/code/code", {
                 type    : "check",
                 checked : "[{tabEditors.activepage && tabEditors.getPage(tabEditors.activepage).$model}::@wrapmode]",
                 isAvailable : function(editor){
-                    return editor && editor.ceEditor;
+                    return editor && editor.amlEditor;
                 }
             }), 500000),
 
@@ -671,7 +672,7 @@ module.exports = ext.register("ext/code/code", {
                 type     : "check",
                 checked  : "[{require('core/settings').model}::editors/code/@wrapmodeViewport]",
                 isAvailable : function(editor){
-                    if (!editor || !editor.ceEditor)
+                    if (!editor || !editor.amlEditor)
                         return false;
 
                     var page = tabEditors.getPage();
@@ -759,18 +760,21 @@ module.exports = ext.register("ext/code/code", {
 
     init: function(amlPage) {
         var _self = this;
+        
+        if (window.__defineGetter__)
+            window.__defineGetter__("ceEditor", function() { window.location.href = "http://goo.gl/tTLGJ"; });
 
-        _self.ceEditor = _self.amlEditor = codeEditor_dontEverUseThisVariable;
+        _self.amlEditor = codeEditor_dontEverUseThisVariable;
         _self.amlEditor.show();
 
-        _self.amlEditor.$editor.$nativeCommands = _self.ceEditor.$editor.commands;
+        _self.amlEditor.$editor.$nativeCommands = _self.amlEditor.$editor.commands;
         _self.amlEditor.$editor.commands = commands;
 
         // preload common language modes
         var noop = function() {};
-        _self.ceEditor.getMode("javascript", noop);
-        _self.ceEditor.getMode("html", noop);
-        _self.ceEditor.getMode("css", noop);
+        _self.amlEditor.getMode("javascript", noop);
+        _self.amlEditor.getMode("html", noop);
+        _self.amlEditor.getMode("css", noop);
 
         ide.addEventListener("reload", function(e) {
             var doc = e.doc;
@@ -804,7 +808,7 @@ module.exports = ext.register("ext/code/code", {
                 var syntax = _self.getSyntax(doc.getNode());
                 // This event is triggered also when closing files, so session may be gone already.
                 if(doc.acesession) {
-                    doc.acesession.setMode(_self.ceEditor.getMode(syntax));
+                    doc.acesession.setMode(_self.amlEditor.getMode(syntax));
                     doc.acesession.syntax = syntax;
                 }
             });
@@ -819,28 +823,28 @@ module.exports = ext.register("ext/code/code", {
         });
 
         ide.addEventListener("animate", function(e){
-            if (!_self.ceEditor.$ext.offsetHeight)
+            if (!_self.amlEditor.$ext.offsetHeight)
                 return;
 
             var renderer, delta;
             if (e.type == "editor") {
-                renderer = _self.ceEditor.$editor.renderer;
-                renderer.onResize(true, null, null, _self.ceEditor.getHeight() + e.delta);
+                renderer = _self.amlEditor.$editor.renderer;
+                renderer.onResize(true, null, null, _self.amlEditor.getHeight() + e.delta);
             }
             else if (e.type == "splitbox") {
-                if (e.options.height !== undefined && apf.isChildOf(e.other, _self.ceEditor, true)) {
+                if (e.options.height !== undefined && apf.isChildOf(e.other, _self.amlEditor, true)) {
                     delta = e.which.getHeight() - Number(e.options.height);
                     if (delta < 0) return;
 
-                    renderer = _self.ceEditor.$editor.renderer;
-                    renderer.onResize(true, null, null, _self.ceEditor.getHeight() + delta);
+                    renderer = _self.amlEditor.$editor.renderer;
+                    renderer.onResize(true, null, null, _self.amlEditor.getHeight() + delta);
                 }
-                else if (e.options.width !== undefined && apf.isChildOf(e.other, _self.ceEditor, true)) {
+                else if (e.options.width !== undefined && apf.isChildOf(e.other, _self.amlEditor, true)) {
                     delta = e.which.getWidth() - Number(e.options.width);
                     if (delta < 0) return;
 
-                    renderer = _self.ceEditor.$editor.renderer;
-                    renderer.onResize(true, null, _self.ceEditor.getWidth() + delta);
+                    renderer = _self.amlEditor.$editor.renderer;
+                    renderer.onResize(true, null, _self.amlEditor.getWidth() + delta);
                 }
             }
         });
@@ -848,10 +852,10 @@ module.exports = ext.register("ext/code/code", {
         // display feedback while loading files
         var isOpen, bgMessage;
         var checkLoading = function(e) {
-            if (!_self.ceEditor.xmlRoot)
+            if (!_self.amlEditor.xmlRoot)
                 return;
-            var loading = _self.ceEditor.xmlRoot.hasAttribute("loading");
-            var container = _self.ceEditor.$editor.container;
+            var loading = _self.amlEditor.xmlRoot.hasAttribute("loading");
+            var container = _self.amlEditor.$editor.container;
 
             if (loading) {
                 if (!bgMessage || !bgMessage.parentNode) {
@@ -861,7 +865,7 @@ module.exports = ext.register("ext/code/code", {
                 var isDark = container.className.indexOf("ace_dark")!=-1;
                 bgMessage.className = "ace_smooth_loading" + (isDark ? " ace_dark" : "");
                 
-                bgMessage.textContent = "Loading " + _self.ceEditor.xmlRoot.getAttribute("name");
+                bgMessage.textContent = "Loading " + _self.amlEditor.xmlRoot.getAttribute("name");
                 container.style.transitionProperty = "opacity";
                 container.style.transitionDuration = "300ms";
                 container.style.pointerEvents = "none";

plugins-client/ext.codetools/codetools.js
@@ -22,14 +22,11 @@ module.exports = ext.register("ext/codetools/codetools", {
 
     register: function(plugin) {
         var _self = this;
-        if (typeof ceEditor !== "undefined")
-            return attachCE();
-
-        ide.addEventListener("init.ext/code/code", attachCE);
-        function attachCE() {
-            if (!ceEditor.$codeToolsAttached)
-                _self.attachEditorEvents(ceEditor);
-        }
+        ide.addEventListener("init.ext/code/code", function attachCE() {
+            var code = require("ext/code/code");
+            if (!code.amlEditor.$codeToolsAttached)
+                _self.attachEditorEvents(code.amlEditor);
+        });
     },
 
     attachEditorEvents: function(amlEditor) {

plugins-client/ext.debugger/breakpoints.js
@@ -97,9 +97,9 @@ module.exports = {
 
         ide.addEventListener("tab.afterswitch", function(e) {
             var page = e.nextPage;
-            if (!page || !page.$editor || !page.$editor.ceEditor)
+            if (!page || !page.$editor || !page.$editor.amlEditor)
                 return;
-            var ace = page.$editor.ceEditor.$editor;
+            var ace = page.$editor.amlEditor.$editor;
             if (!ace.$breakpointListener)
                 _self.initEditor(ace);
 
@@ -308,8 +308,8 @@ module.exports = {
     $syncOpenFiles: function() {
         // var tabFiles = ide.getAllPageModels();
         var page = tabEditors.$activepage;
-        if (page && page.$editor && page.$editor.ceEditor) {
-            var session = page.$editor.ceEditor.$editor.session;
+        if (page && page.$editor && page.$editor.amlEditor) {
+            var session = page.$editor.amlEditor.$editor.session;
             this.updateSession(session);
         }
     }

plugins-client/ext.debugger/debugger.js
@@ -21,7 +21,7 @@ var apfhook = require("./apfhook");
 
 require("ext/debugger/inspector");
 
-/*global dbInteractive txtCode dbg ceEditor
+/*global dbInteractive txtCode dbg
   dbgVariable pgDebugNav tabDebug dgVars*/
 
 module.exports = ext.register("ext/debugger/debugger", {
@@ -99,13 +99,13 @@ module.exports = ext.register("ext/debugger/debugger", {
                 if (dbg.state != "stopped")
                     return false;
                 if (event instanceof KeyboardEvent &&
-                 (!apf.activeElement || apf.activeElement != ceEditor))
+                  (!apf.activeElement || !apf.activeElement.$editor || apf.activeElement.$editor.path != "ext/code/code"))
                     return false;
                 return true;
             },
             findEditor: function(editor) {
-                if (editor && editor.ceEditor)
-                    return editor.ceEditor.$editor;
+                if (editor && editor.amlEditor)
+                    return editor.amlEditor.$editor;
                 return editor;
             }
         });

plugins-client/ext.debugger/sources.js
@@ -93,8 +93,8 @@ module.exports = {
         }
 
         function updateMarker(frame) {
-            var ceEditor = editors.currentEditor && editors.currentEditor.ceEditor;
-            var session = ceEditor && ceEditor.$editor.session;
+            var amlEditor = editors.currentEditor && editors.currentEditor.amlEditor;
+            var session = amlEditor && amlEditor.$editor.session;
             if (!session)
                 return;
 
@@ -102,7 +102,7 @@ module.exports = {
             session.$stepMarker && removeMarker(session, "step");
 
             if (frame) {
-                var path = ceEditor.xmlRoot.getAttribute("path");
+                var path = amlEditor.xmlRoot.getAttribute("path");
                 var framePath = frame.getAttribute("scriptPath");
                 var row = parseInt(frame.getAttribute("line"), 10);
                 if (frame.hasAttribute("istop")) {

commit 59999c04a4437bb6941e56447401f2df1231f155

Fix ceEditor refactoring regression
introduced in 683c8f72bed6190dfe7e95343b071f9fafd5f0ee

plugins-client/ext.language/outline.js
@@ -223,7 +223,7 @@ module.exports = {
     
     renderOutline: function(ignoreFilter) {
         var editor = editors.currentEditor;
-        if (!editor || editor.path != "ext/code" || !editor.amlEditor)
+        if (!editor || editor.path != "ext/code/code" || !editor.amlEditor)
             return;
             
         ext.initExtension(gotofile);