e2c7565e8c3735eb93f4b0d5def8fe3df05b0cae
Garen Torikian <gjtorikian@gmail.com>
2012-11-16 20:06:27 +0000

    Add JSON minifier

---
+++ build/json_minifier.js
@@ -0,0 +1,14 @@
+var args = process.argv.slice(2),
+    fileIn = args[0],
+    fileOut = args[1],
+    fs = require("fs");
+
+    fs.readFile(fileIn, "utf8", function(err, json) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        
+        console.log(fileOut)
+        fs.writeFileSync(fileOut, JSON.stringify(JSON.parse(json), null));
+    });
