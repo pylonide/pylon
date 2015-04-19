d7a1cda333b59ff4751103afa40fe27966a68b5c
Exile <exile@chamber.ee>
2015-04-19 16:37:03 +0300

    Session changes
    - express-session session-store is touch compatible, resave set to false (express-session deprecated undefined resave option)
    - saveUninitialized session option added (express-session deprecated undefined saveUninitialized option) and set in configuration
    - session key is now session name

--- plugins-server/connect.session/session-ext.js
+++ plugins-server/connect.session/session-ext.js
@@ -3,16 +3,19 @@ var assert = require("assert");
 
 module.exports = function startup(options, imports, register) {
 
-    assert(options.key, "option 'key' is required");
+    assert(options.name, "option 'name' is required");
     assert(options.secret, "option 'secret' is required");
+    assert(options.saveUninitialized, "option 'saveUninitialized' is required");
 
     var connect = imports.connect;
     var sessionStore = imports["session-store"];
 
     connect.useSession(Session({
         store: sessionStore,
-        key: options.key,
-        secret: options.secret
+        name: options.key,
+        secret: options.secret,
+        saveUninitialized: options.saveUninitialized,
+        resave: false
     }));
 
     register(null, {
